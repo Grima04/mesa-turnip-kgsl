4dec7f81e0066554347461822b1fcbe6f355674a
Karol Herbst <kherbst@redhat.com>
2017-12-23 02:25:41 +0100

    nv50/ir/nir: add skeleton getOperation for intrinsics
    v7: don't assert in default case for getSubOp
    
    Signed-off-by: Karol Herbst <kherbst@redhat.com>
    Reviewed-by: Pierre Moreau <pierre.morrow@free.fr>

--- src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
+++ src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
@@ -116,10 +116,12 @@ private:
    std::vector<DataType> getSTypes(nir_alu_instr *);
    DataType getSType(nir_src &, bool isFloat, bool isSigned);
 
+   operation getOperation(nir_intrinsic_op);
    operation getOperation(nir_op);
    operation getOperation(nir_texop);
    operation preOperationNeeded(nir_op);
 
+   int getSubOp(nir_intrinsic_op);
    int getSubOp(nir_op);
 
    CondCode getCondCode(nir_op);
@@ -458,6 +460,17 @@ Converter::getOperation(nir_texop op)
 }
 
 operation
+Converter::getOperation(nir_intrinsic_op op)
+{
+   switch (op) {
+   default:
+      ERROR("couldn't get operation for nir_intrinsic_op %u\n", op);
+      assert(false);
+      return OP_NOP;
+   }
+}
+
+operation
 Converter::preOperationNeeded(nir_op op)
 {
    switch (op) {
@@ -476,6 +489,15 @@ Converter::getSubOp(nir_op op)
    case nir_op_imul_high:
    case nir_op_umul_high:
       return NV50_IR_SUBOP_MUL_HIGH;
+   default:
+      return 0;
+   }
+}
+
+int
+Converter::getSubOp(nir_intrinsic_op op)
+{
+   switch (op) {
    default:
       return 0;
    }
