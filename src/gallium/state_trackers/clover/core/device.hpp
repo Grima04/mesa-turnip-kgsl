035e882819bcb853fff7a59c638a0ecbf89cb762
Karol Herbst <kherbst@redhat.com>
2018-05-21 12:19:42 +0200

    clover: implement CL_DEVICE_SVM_CAPABILITIES
    v2: without supporting userptrs SVM can't be implemented as it's impossible
        to ensure memory consistency with HOST_PTR buffers
    v3: fix comment style
    v4: fixes typo in comment
    
    Signed-off-by: Karol Herbst <kherbst@redhat.com>
    Reviewed-by: Pierre Moreau <dev@pmoreau.org>
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/2076>

--- src/gallium/state_trackers/clover/core/device.hpp
+++ src/gallium/state_trackers/clover/core/device.hpp
@@ -71,6 +71,7 @@ namespace clover {
       bool has_int64_atomics() const;
       bool has_unified_memory() const;
       cl_uint mem_base_addr_align() const;
+      cl_device_svm_capabilities svm_support() const;
 
       std::vector<size_t> max_block_size() const;
       cl_uint subgroup_size() const;
