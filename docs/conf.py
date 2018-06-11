8342fe8302a4462266b1097df0c9823bb166dc41
Laura Ekstrand <laura@jlekstrand.net>
2018-06-11 15:42:39 -0700

    docs: Add the favicon to the new page.
    Reviewed-by: Eric Engestrom <eric@engestrom.ch>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4630>

--- docs/conf.py
+++ docs/conf.py
@@ -95,6 +95,8 @@ highlight_language = "none"
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_favicon = "favicon.ico"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
