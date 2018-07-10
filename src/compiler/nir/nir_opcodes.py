dba4d0a319f6938263e362a44201ed1099353cac
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2018-07-10 13:17:05 +0200

    nir: fix fmin/fmax support for doubles
    Until now, it was using the floating point version of fmin/fmax,
    instead of the double version.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>
    Reviewed-by: Connor Abbott <cwabbott0@gmail.com>

--- src/compiler/nir/nir_opcodes.py
+++ src/compiler/nir/nir_opcodes.py
@@ -787,10 +787,10 @@ opcode("fdph", 1, tfloat, [3, 4], [tfloat, tfloat], False, "",
 opcode("fdph_replicated", 4, tfloat, [3, 4], [tfloat, tfloat], False, "",
        "src0.x * src1.x + src0.y * src1.y + src0.z * src1.z + src1.w")
 
-binop("fmin", tfloat, "", "fminf(src0, src1)")
+binop("fmin", tfloat, "", "fmin(src0, src1)")
 binop("imin", tint, _2src_commutative + associative, "src1 > src0 ? src0 : src1")
 binop("umin", tuint, _2src_commutative + associative, "src1 > src0 ? src0 : src1")
-binop("fmax", tfloat, "", "fmaxf(src0, src1)")
+binop("fmax", tfloat, "", "fmax(src0, src1)")
 binop("imax", tint, _2src_commutative + associative, "src1 > src0 ? src1 : src0")
 binop("umax", tuint, _2src_commutative + associative, "src1 > src0 ? src1 : src0")
 
