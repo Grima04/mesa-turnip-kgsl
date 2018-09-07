64014c8302da52480643cb7711298153cfdb0d51
Dylan Baker <dylan@pnwbakers.com>
2018-09-07 14:44:05 -0700

    Replace IROUND_POS with _mesa_roundevenf
    Which has the same behavior as long as you don't change the FPU rounding
    mode. Other code in mesa makes the same assumption so it should be safe
    to make that assumption more generally.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Kristian H. Kristensen <hoegsberg@google.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3024>

--- src/util/imports.h
+++ src/util/imports.h
@@ -120,16 +120,6 @@ static inline int64_t IROUND64(float f)
 }
 
 
-/**
- * Convert positive float to int by rounding to nearest integer.
- */
-static inline int IROUND_POS(float f)
-{
-   assert(f >= 0.0F);
-   return (int) (f + 0.5F);
-}
-
-
 /**********************************************************************
  * Functions
  */
