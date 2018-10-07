6fbb6ba290bed57481a78b9e55de1da1d1d392af
Kenneth Graunke <kenneth@whitecape.org>
2018-10-06 22:50:34 -0700

    iris: pipe to scs -> iris_pipe.h

--- src/gallium/drivers/iris/iris_state.c
+++ src/gallium/drivers/iris/iris_state.c
@@ -1290,23 +1290,6 @@ iris_bind_sampler_states(struct pipe_context *ctx,
 }
 
 /**
- * Convert an swizzle enumeration (i.e. PIPE_SWIZZLE_X) to one of the HW's
- * "Shader Channel Select" enumerations (i.e. SCS_RED).  The mappings are
- *
- * SWIZZLE_X, SWIZZLE_Y, SWIZZLE_Z, SWIZZLE_W, SWIZZLE_ZERO, SWIZZLE_ONE
- *         0          1          2          3             4            5
- *         4          5          6          7             0            1
- *   SCS_RED, SCS_GREEN,  SCS_BLUE, SCS_ALPHA,     SCS_ZERO,     SCS_ONE
- *
- * which is simply adding 4 then modding by 8 (or anding with 7).
- */
-static enum isl_channel_select
-pipe_swizzle_to_isl_channel(enum pipe_swizzle swizzle)
-{
-   return (swizzle + 4) & 7;
-}
-
-/**
  * The pipe->create_sampler_view() driver hook.
  */
 static struct pipe_sampler_view *
