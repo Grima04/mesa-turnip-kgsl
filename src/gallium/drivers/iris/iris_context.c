3f70956a4e29e350e71cdaa45b9fe4fee00296ea
Kenneth Graunke <kenneth@whitecape.org>
2018-10-19 02:12:29 -0700

    iris: try and avoid pointless compute submissions
    if apps don't use compute shaders, we don't even want to kick off the
    compute initialization batch

--- src/gallium/drivers/iris/iris_context.c
+++ src/gallium/drivers/iris/iris_context.c
@@ -42,7 +42,9 @@ iris_flush(struct pipe_context *ctx,
    struct iris_context *ice = (struct iris_context *)ctx;
 
    iris_batch_flush(&ice->render_batch);
-   iris_batch_flush(&ice->compute_batch);
+
+   if (ice->compute_batch.contains_draw)
+      iris_batch_flush(&ice->compute_batch);
 
    // XXX: bogus!!!
    if (fence)
