78fdc507a37b7174e21412cc0b2f4818dbe6b63f
Gert Wollny <gert.wollny@collabora.com>
2018-11-15 19:01:24 +0100

    i965: Add support for and expose EXT_texture_sRGB_R8
    Emulate MESA_FORMAT_R_SRGB8 by using L8_UNORM_SRGB. This is possible
    because component swizzling is handled based on the mesa format and,
    hence, the a r001 swizzling can be used to correct the components.
    
    Enables and makes pass (tested on Kabylake)
    
      dEQP-GLES31.functional.srgb_texture_decode.skip_decode.sr8.*
      dEQP-GLES31.functional.texture.filtering.cube_array.formats.sr8*
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Acked-by: Eric Engestrom <eric.engestrom@intel.com>

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -113,6 +113,7 @@ intelInitExtensions(struct gl_context *ctx)
    ctx->Extensions.EXT_texture_snorm = true;
    ctx->Extensions.EXT_texture_sRGB = true;
    ctx->Extensions.EXT_texture_sRGB_decode = true;
+   ctx->Extensions.EXT_texture_sRGB_R8 = true;
    ctx->Extensions.EXT_texture_swizzle = true;
    ctx->Extensions.EXT_texture_type_2_10_10_10_REV = true;
    ctx->Extensions.EXT_vertex_array_bgra = true;
