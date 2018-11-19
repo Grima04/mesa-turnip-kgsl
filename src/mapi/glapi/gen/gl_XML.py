cf317bf0937101001ea90bcc5bde97aea33d4677
Emil Velikov <emil.velikov@collabora.com>
2018-11-19 14:08:15 +0000

    mapi: add all _glapi_table entrypoints to static_data.py
    Currently various parts of mesa use the glapi_table differently.
    
    Some use _glapi_get_proc_offset() to get the offset, while others
    directly reference the specific offset via _gloffset_Function.
    
    Add all static entries, to ensure things don't break as we flip to the
    upstream XML + new mapi generator.
    
    Note: the offsets are also used for the alias remap table, thus we need
    to ensure we honour the correct offsets range or it will break.
    
    Currently this is done via MAX_OFFSETS constant, although a better
    solution is in the works.
    
    v2: add FramebufferTexture2DMultisampleEXT
    v3: add MAX_OFFSETS guard
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com> (v1)
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/mapi/glapi/gen/gl_XML.py
+++ src/mapi/glapi/gen/gl_XML.py
@@ -693,7 +693,7 @@ class gl_function( gl_item ):
             # Only try to set the offset when a non-alias entry-point
             # is being processed.
 
-            if name in static_data.offsets:
+            if name in static_data.offsets and static_data.offsets[name] <= static_data.MAX_OFFSETS:
                 self.offset = static_data.offsets[name]
             else:
                 self.offset = -1
