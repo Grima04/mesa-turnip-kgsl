Grima04/mesa-turnip-kgsl

commit 0d240c22141d9f5d387b8a3b6d18dfd9688b136d

freedreno/ir3: don't fetch unused tex components
Detect when a component of an (for example) texture fetch is unused and
propagate the updated wrmask back to the parent instruction.

Signed-off-by: Rob Clark <robdclark@gmail.com>

src/freedreno/ir3/ir3_depth.c
@@ -174,6 +174,33 @@ remove_unused_by_block(struct ir3_block *block)
 		if (instr->opc == OPC_END)
 			continue;
 		if (instr->flags & IR3_INSTR_UNUSED) {
+			if (instr->opc == OPC_META_FO) {
+				struct ir3_instruction *src = ssa(instr->regs[1]);
+				if (src->regs[0]->wrmask > 1) {
+					unsigned newn, lastn;
+
+					lastn = util_last_bit(src->regs[0]->wrmask);
+					src->regs[0]->wrmask &= ~(1 << instr->fo.off);
+					newn = util_last_bit(src->regs[0]->wrmask);
+
+					/* prune no-longer needed right-neighbors.  We could
+					 * probably do the same for left-neighbors (ie. tex
+					 * fetch that only need .yw components), but that
+					 * makes RA a bit more confusing than it already is
+					 */
+					if (newn < lastn) {
+						struct ir3_instruction *n = ir3_neighbor_first(instr);
+						for (unsigned i = 1; i < newn; i++) {
+							n = n->cp.right;
+						}
+						// XXX I don't think n should be null here!
+						if (n) {
+							debug_assert(n->cp.right->flags & IR3_INSTR_UNUSED);
+							n->cp.right = NULL;
+						}
+					}
+				}
+			}
 			list_delinit(&instr->node);
 			progress = true;
 		}

src/freedreno/ir3/ir3_ra.c
@@ -509,6 +509,8 @@ get_definer(struct ir3_ra_ctx *ctx, struct ir3_instruction *instr,
 		d = dd;
 	}
 
+	debug_assert(d->opc != OPC_META_FO);
+
 	id->defn = d;
 	id->sz = *sz;
 	id->off = *off;

commit c19c4bf48845be044605a97d0ea0c278bf2e1d6e

freedreno/ir3: fix crash
Fixes a crash in dEQP-GLES3.functional.shaders.fragdepth.compare.fragcoord_z

Fixes: 0d240c22141 freedreno/ir3: don't fetch unused tex components
Signed-off-by: Rob Clark <robdclark@gmail.com>

src/freedreno/ir3/ir3_depth.c
@@ -177,27 +177,21 @@ remove_unused_by_block(struct ir3_block *block)
 			if (instr->opc == OPC_META_FO) {
 				struct ir3_instruction *src = ssa(instr->regs[1]);
 				if (src->regs[0]->wrmask > 1) {
-					unsigned newn, lastn;
-
-					lastn = util_last_bit(src->regs[0]->wrmask);
 					src->regs[0]->wrmask &= ~(1 << instr->fo.off);
-					newn = util_last_bit(src->regs[0]->wrmask);
 
 					/* prune no-longer needed right-neighbors.  We could
 					 * probably do the same for left-neighbors (ie. tex
 					 * fetch that only need .yw components), but that
 					 * makes RA a bit more confusing than it already is
 					 */
-					if (newn < lastn) {
-						struct ir3_instruction *n = ir3_neighbor_first(instr);
-						for (unsigned i = 1; i < newn; i++) {
-							n = n->cp.right;
-						}
-						// XXX I don't think n should be null here!
-						if (n) {
-							debug_assert(n->cp.right->flags & IR3_INSTR_UNUSED);
-							n->cp.right = NULL;
-						}
+					struct ir3_instruction *n = instr;
+					while (n && n->cp.right)
+						n = n->cp.right;
+					while (n->flags & IR3_INSTR_UNUSED) {
+						n = n->cp.left;
+						if (!n)
+							break;
+						n->cp.right = NULL;
 					}
 				}
 			}