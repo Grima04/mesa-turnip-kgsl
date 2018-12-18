d8a11bfc0850defbd4d3dd1fdc0de0e8901bfdc4
Jason Ekstrand <jason.ekstrand@intel.com>
2018-12-18 10:30:23 -0600

    glsl_type: Add a C wrapper to get struct field offsets
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
    Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- src/compiler/nir_types.cpp
+++ src/compiler/nir_types.cpp
@@ -65,6 +65,13 @@ glsl_get_struct_field(const glsl_type *type, unsigned index)
    return type->fields.structure[index].type;
 }
 
+const int
+glsl_get_struct_field_offset(const struct glsl_type *type,
+                             unsigned index)
+{
+   return type->fields.structure[index].offset;
+}
+
 const glsl_type *
 glsl_get_function_return_type(const glsl_type *type)
 {
--- src/compiler/nir_types.h
+++ src/compiler/nir_types.h
@@ -46,6 +46,9 @@ const char *glsl_get_type_name(const struct glsl_type *type);
 const struct glsl_type *glsl_get_struct_field(const struct glsl_type *type,
                                               unsigned index);
 
+const int glsl_get_struct_field_offset(const struct glsl_type *type,
+                                       unsigned index);
+
 const struct glsl_type *glsl_get_array_element(const struct glsl_type *type);
 const struct glsl_type *glsl_without_array(const struct glsl_type *type);
 const struct glsl_type *glsl_without_array_or_matrix(const struct glsl_type *type);
