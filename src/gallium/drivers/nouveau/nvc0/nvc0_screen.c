1d10bb202558829d8bd18e211fe91e42475e214c
Ilia Mirkin <imirkin@alum.mit.edu>
2018-12-26 20:01:28 -0500

    nvc0: enable GL_NV_shader_atomic_float on pre-Maxwell
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -275,6 +275,8 @@ nvc0_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_TGSI_BALLOT:
    case PIPE_CAP_BINDLESS_TEXTURE:
       return class_3d >= NVE4_3D_CLASS;
+   case PIPE_CAP_TGSI_ATOMFADD:
+      return class_3d < GM107_3D_CLASS; /* needs additional lowering */
    case PIPE_CAP_POLYGON_MODE_FILL_RECTANGLE:
    case PIPE_CAP_TGSI_VS_LAYER_VIEWPORT:
    case PIPE_CAP_TGSI_TES_LAYER_VIEWPORT:
