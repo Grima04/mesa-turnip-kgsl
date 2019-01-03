14f265b533217a9b63c5dbe48c3b326a5cd91d3b
Gurchetan Singh <gurchetansingh@chromium.org>
2019-01-02 16:27:56 -0800

    virgl: make alignment smaller when uploading index user buffers
    Since we're just uploading to guest memory, let's just align to dword
    size.
    
    Fixes: e0f932 ("u_upload_mgr: pass alignment to u_upload_data manually")
    Reviewed-by: Gert Wollny <gert.wollny@collabora.com>

--- src/gallium/drivers/virgl/virgl_context.c
+++ src/gallium/drivers/virgl/virgl_context.c
@@ -724,7 +724,7 @@ static void virgl_draw_vbo(struct pipe_context *ctx,
            ib.offset = info.start * ib.index_size;
 
            if (ib.user_buffer) {
-                   u_upload_data(vctx->uploader, 0, info.count * ib.index_size, 256,
+                   u_upload_data(vctx->uploader, 0, info.count * ib.index_size, 4,
                                  ib.user_buffer, &ib.offset, &ib.buffer);
                    ib.user_buffer = NULL;
            }
