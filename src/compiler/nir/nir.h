b7201a468d0c1d25161f48986e26930e8909bfa8
Gert Wollny <gw.fossdev@gmail.com>
2019-02-02 18:38:17 +0100

    nir: Add posibility to not lower to source mod 'abs' for ops with three sources
    This is useful for r600 since there the abs source modifier is not supported
    for ops with three sources
    
    v2: Use correct logic to enable lowering to abs source mod (Eric Anhold)
    
    Signed-off-by: Gert Wollny <gw.fossdev@gmail.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -3205,7 +3205,8 @@ bool nir_lower_atomics_to_ssbo(nir_shader *shader, unsigned ssbo_offset);
 typedef enum  {
    nir_lower_int_source_mods = 1 << 0,
    nir_lower_float_source_mods = 1 << 1,
-   nir_lower_all_source_mods = (1 << 2) - 1
+   nir_lower_triop_abs = 1 << 2,
+   nir_lower_all_source_mods = (1 << 3) - 1
 } nir_lower_to_source_mods_flags;
 
 
