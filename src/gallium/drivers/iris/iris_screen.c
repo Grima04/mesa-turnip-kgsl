974229df46b611b741e7b6f0c0f4d0c7bcfe7c14
Kenneth Graunke <kenneth@whitecape.org>
2019-02-10 14:23:45 -0800

    iris: Add PIPE_CAP_MAX_VARYINGS

--- src/gallium/drivers/iris/iris_screen.c
+++ src/gallium/drivers/iris/iris_screen.c
@@ -241,6 +241,7 @@ iris_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_VIDEO_MEMORY:
       return 0xffffffff; // XXX: bogus
    case PIPE_CAP_MAX_SHADER_PATCH_VARYINGS:
+   case PIPE_CAP_MAX_VARYINGS:
       return 32;
    case PIPE_CAP_RESOURCE_FROM_USER_MEMORY:
       /* AMD_pinned_memory assumes the flexibility of using client memory
