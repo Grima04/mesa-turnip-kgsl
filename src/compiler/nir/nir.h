ca2b5e9069177ea603efbe250e675dc7d194ef90
Iago Toral Quiroga <itoral@igalia.com>
2019-02-12 12:55:28 +0100

    compiler/nir: add an is_conversion field to nir_op_info
    This is set to True only for numeric conversion opcodes.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -918,6 +918,9 @@ typedef struct {
    nir_alu_type input_types[NIR_MAX_VEC_COMPONENTS];
 
    nir_op_algebraic_property algebraic_properties;
+
+   /* Whether this represents a numeric conversion opcode */
+   bool is_conversion;
 } nir_op_info;
 
 extern const nir_op_info nir_op_infos[nir_num_opcodes];
