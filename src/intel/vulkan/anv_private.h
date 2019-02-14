39aee57523a02552e7eae7df5da488e535aeb1eb
Kenneth Graunke <kenneth@whitecape.org>
2019-02-14 08:55:37 -0800

    anv: Put MOCS in the correct location
    My patch to switch from struct-based MOCS to numeric MOCS accidentally
    divided all MOCS entries by 2 in the Vulkan driver.
    
    MOCS on Gen9+ is just an array index into a table.  But in the hardware
    packets, the index starts at bit 1.  So we need to shift it.
    
    Fixes: 0b44644ca68 (genxml: Consistently use a numeric "MOCS" field)
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -1455,10 +1455,10 @@ _anv_combine_address(struct anv_batch *batch, void *location,
  */
 
 /* TC=LLC/eLLC, LeCC=WB, LRUM=3, L3CC=WB */
-#define GEN9_MOCS 2
+#define GEN9_MOCS (2 << 1)
 
 /* TC=LLC/eLLC, LeCC=WB, LRUM=3, L3CC=WB */
-#define GEN9_EXTERNAL_MOCS 1
+#define GEN9_EXTERNAL_MOCS (1 << 1)
 
 /* Cannonlake MOCS defines are duplicates of Skylake MOCS defines. */
 #define GEN10_MOCS GEN9_MOCS
