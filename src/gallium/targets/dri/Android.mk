5e52184f722b70c024bec6dc0872cbf6553a1d82
Tapani Pälli <tapani.palli@intel.com>
2019-02-26 10:27:15 +0200

    android: make libbacktrace optional on USE_LIBBACKTRACE
    Otherwise with VNDK enabled we fail linking:
       src/gallium/targets/dri/Android.mk: error: gallium_dri (native:vendor)
       should not link to libbacktrace.vendor (native:vndk_private)
    
    Option makes it possible to use libbacktrace only when VNDK is not
    enabled.
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/targets/dri/Android.mk
+++ src/gallium/targets/dri/Android.mk
@@ -40,11 +40,14 @@ LOCAL_LDFLAGS := \
 	-Wl,--undefined-version
 
 LOCAL_SHARED_LIBRARIES := \
-	libbacktrace \
 	libdl \
 	libglapi \
 	libexpat \
 	libz
+
+ifeq ($(USE_LIBBACKTRACE),true)
+	LOCAL_SHARED_LIBRARIES += libbacktrace
+endif
 
 $(foreach d, $(MESA_BUILD_GALLIUM), $(eval LOCAL_CFLAGS += $(patsubst HAVE_%,-D%,$(d))))
 
