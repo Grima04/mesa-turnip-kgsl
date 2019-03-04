5d48359a2c5670d55f24e91229f59efc6368dcba
Karol Herbst <kherbst@redhat.com>
2019-03-04 19:11:12 +0100

    nir: replace magic numbers with M_PI
    we define it inside 'include/c99_math.h' so it is safe to use.
    
    Signed-off-by: Karol Herbst <kherbst@redhat.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/compiler/nir/nir_builtin_builder.h
+++ src/compiler/nir/nir_builtin_builder.h
@@ -60,7 +60,7 @@ nir_uclamp(nir_builder *b,
 static inline nir_ssa_def *
 nir_degrees(nir_builder *b, nir_ssa_def *val)
 {
-   return nir_fmul_imm(b, val, 57.2957795131);
+   return nir_fmul_imm(b, val, 180.0 / M_PI);
 }
 
 static inline nir_ssa_def *
@@ -78,7 +78,7 @@ nir_fast_normalize(nir_builder *b, nir_ssa_def *vec)
 static inline nir_ssa_def *
 nir_radians(nir_builder *b, nir_ssa_def *val)
 {
-   return nir_fmul_imm(b, val, 0.01745329251);
+   return nir_fmul_imm(b, val, M_PI / 180.0);
 }
 
 #endif /* NIR_BUILTIN_BUILDER_H */
