Grima04/mesa-turnip-kgsl

commit 45271702ec9b69f8e420940e4584e4b162188ccb

freedreno: fix ir3_cmdline build
Fixes: 7530d4abfcf glsl/freedreno/panfrost: pass gl_context to the standalone compiler
Signed-off-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/ir3/ir3_cmdline.c
@@ -51,6 +51,8 @@
 
 #include "pipe/p_context.h"
 
+#include "main/mtypes.h"
+
 static void dump_info(struct ir3_shader_variant *so, const char *str)
 {
 	uint32_t *bin;

src/gallium/drivers/freedreno/meson.build
@@ -214,6 +214,7 @@ files_libfreedreno = files(
 )
 
 freedreno_includes = [
+  inc_mesa, inc_mapi,
   inc_src, inc_include, inc_gallium, inc_gallium_aux,
   inc_freedreno, include_directories('ir3'),
 ]

commit 53083e4fbc1d06c1ef241a7c8cae67d633a09a21

freedreno/ir3: fix ir3_cmdline harder
Fixes: 45271702ec9 freedreno: fix ir3_cmdline build
Fixes: 7530d4abfcf glsl/freedreno/panfrost: pass gl_context to the standalone compiler
Signed-off-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/ir3/ir3_cmdline.c
@@ -43,6 +43,8 @@
 #include "ir3/instr-a3xx.h"
 #include "ir3/ir3.h"
 
+#include "main/mtypes.h"
+
 #include "compiler/glsl/standalone.h"
 #include "compiler/glsl/glsl_to_nir.h"
 #include "compiler/glsl/gl_nir.h"
@@ -50,8 +52,6 @@
 #include "compiler/spirv/nir_spirv.h"
 
 #include "pipe/p_context.h"
-
-#include "main/mtypes.h"
 
 static void dump_info(struct ir3_shader_variant *so, const char *str)
 {