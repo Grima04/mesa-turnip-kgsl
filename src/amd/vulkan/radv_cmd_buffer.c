e72daf3e7023246fc960cb5e375ae81313f7803b
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2019-03-08 14:51:02 +0100

    Revert "radv: execute external subpass barriers after ending subpasses"
    This changes is actually wrong because we have to sync
    before doing image layout transitions.
    
    This fixes rendering issues in Batman, Path of Exile and
    probably more titles.
    
    This reverts commit 76c17cfd8da017ebd19be33ba6cef888957a6758.
    
    Fixes: 76c17cfd8da ("radv: execute external subpass barriers after ending subpasses")
    Cc: 19.0 <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -4395,9 +4395,9 @@ void radv_CmdEndRenderPass(
 {
 	RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
 
-	radv_cmd_buffer_end_subpass(cmd_buffer);
-
 	radv_subpass_barrier(cmd_buffer, &cmd_buffer->state.pass->end_barrier);
+
+	radv_cmd_buffer_end_subpass(cmd_buffer);
 
 	vk_free(&cmd_buffer->pool->alloc, cmd_buffer->state.attachments);
 
