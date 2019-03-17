bfba0ecc1c847b4e5c5d24b89203ddcc6e672ae3
Mauro Rossi <issor.oruam@gmail.com>
2019-03-17 22:52:36 +0100

    android: nouveau: add support for nir
    Add the necessary build rules for android, to avoid building errors.
    
    Fixes: f014ae3 ("nouveau: add support for nir")
    Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>

--- src/gallium/drivers/nouveau/Android.mk
+++ src/gallium/drivers/nouveau/Android.mk
@@ -37,8 +37,13 @@ LOCAL_SRC_FILES := \
 	$(NVC0_C_SOURCES)
 
 LOCAL_C_INCLUDES := \
-	$(MESA_TOP)/include
+	$(MESA_TOP)/include \
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_nir,,)/nir \
+	$(MESA_TOP)/src/compiler/nir \
+	$(MESA_TOP)/src/mapi \
+	$(MESA_TOP)/src/mesa
 
+LOCAL_STATIC_LIBRARIES := libmesa_nir
 LOCAL_SHARED_LIBRARIES := libdrm_nouveau
 LOCAL_MODULE := libmesa_pipe_nouveau
 
