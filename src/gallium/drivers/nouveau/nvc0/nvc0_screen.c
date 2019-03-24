98934e6aa19795072a353dae6020dafadc76a1e3
Karol Herbst <kherbst@redhat.com>
2019-03-24 04:23:38 +0100

    nvc0/nir: enable bindless texture
    Signed-off-by: Karol Herbst <kherbst@redhat.com>

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -285,7 +285,7 @@ nvc0_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_TGSI_BALLOT:
       return class_3d >= NVE4_3D_CLASS;
    case PIPE_CAP_BINDLESS_TEXTURE:
-      return class_3d >= NVE4_3D_CLASS && !screen->prefer_nir;
+      return class_3d >= NVE4_3D_CLASS;
    case PIPE_CAP_TGSI_ATOMFADD:
       return class_3d < GM107_3D_CLASS; /* needs additional lowering */
    case PIPE_CAP_POLYGON_MODE_FILL_RECTANGLE:
