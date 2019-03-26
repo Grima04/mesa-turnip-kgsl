Grima04/mesa-turnip-kgsl

commit 1ea42894c70f271c6ea958a9d3cc7ba8c5948c2e

panfrost/midgard: Implement fpow
We have a native op for this, which was just found in a disassembly --
so instead of lowering, use it!

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

src/gallium/drivers/panfrost/midgard/helpers.h
@@ -228,6 +228,7 @@ static unsigned alu_opcode_props[256] = {
         [midgard_alu_op_frcp]		 = UNIT_VLUT,
         [midgard_alu_op_frsqrt]		 = UNIT_VLUT,
         [midgard_alu_op_fsqrt]		 = UNIT_VLUT,
+        [midgard_alu_op_fpow]		 = UNIT_VLUT,
         [midgard_alu_op_fexp2]		 = UNIT_VLUT,
         [midgard_alu_op_flog2]		 = UNIT_VLUT,
 

src/gallium/drivers/panfrost/midgard/midgard.h
@@ -108,6 +108,7 @@ typedef enum {
         midgard_alu_op_fcsel      = 0xC5,
         midgard_alu_op_fround     = 0xC6,
         midgard_alu_op_fatan_pt2  = 0xE8,
+        midgard_alu_op_fpow       = 0xEC,
         midgard_alu_op_frcp       = 0xF0,
         midgard_alu_op_frsqrt     = 0xF2,
         midgard_alu_op_fsqrt      = 0xF3,
@@ -462,6 +463,7 @@ static char *alu_opcode_names[256] = {
         [midgard_alu_op_frcp]       = "frcp",
         [midgard_alu_op_frsqrt]     = "frsqrt",
         [midgard_alu_op_fsqrt]      = "fsqrt",
+        [midgard_alu_op_fpow]       = "fpow",
         [midgard_alu_op_fexp2]      = "fexp2",
         [midgard_alu_op_flog2]      = "flog2",
         [midgard_alu_op_fsin]       = "fsin",

src/gallium/drivers/panfrost/midgard/midgard_compile.c
@@ -990,6 +990,7 @@ emit_alu(compiler_context *ctx, nir_alu_instr *instr)
                 ALU_CASE(frcp, frcp);
                 ALU_CASE(frsq, frsqrt);
                 ALU_CASE(fsqrt, fsqrt);
+                ALU_CASE(fpow, fpow);
                 ALU_CASE(fexp2, fexp2);
                 ALU_CASE(flog2, flog2);
 

src/gallium/drivers/panfrost/midgard/midgard_compile.h
@@ -62,7 +62,6 @@ midgard_compile_shader_nir(nir_shader *nir, midgard_program *program, bool is_bl
 static const nir_shader_compiler_options midgard_nir_options = {
         .lower_ffma = true,
         .lower_sub = true,
-        .lower_fpow = true,
         .lower_scmp = true,
         .lower_flrp32 = true,
         .lower_flrp64 = true,

commit 04a72391f35d4f25175fd1028871f2666d2ebb83

panfrost/midgard: fpow is a two-part operation
In fact, the native "fpow" instruction only does half of it; more work
is needed for the actual instruction. For now, just lower.

Fixes: 1ea42894c ("panfrost/midgard: Implement fpow")

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

src/gallium/drivers/panfrost/midgard/helpers.h
@@ -234,7 +234,7 @@ static unsigned alu_opcode_props[256] = {
         [midgard_alu_op_frcp]		 = UNIT_VLUT,
         [midgard_alu_op_frsqrt]		 = UNIT_VLUT,
         [midgard_alu_op_fsqrt]		 = UNIT_VLUT,
-        [midgard_alu_op_fpow]		 = UNIT_VLUT,
+        [midgard_alu_op_fpow_pt1]	 = UNIT_VLUT,
         [midgard_alu_op_fexp2]		 = UNIT_VLUT,
         [midgard_alu_op_flog2]		 = UNIT_VLUT,
 

src/gallium/drivers/panfrost/midgard/midgard.h
@@ -126,7 +126,7 @@ typedef enum {
         midgard_alu_op_fcsel      = 0xC5,
         midgard_alu_op_fround     = 0xC6,
         midgard_alu_op_fatan_pt2  = 0xE8,
-        midgard_alu_op_fpow       = 0xEC,
+        midgard_alu_op_fpow_pt1   = 0xEC,
         midgard_alu_op_frcp       = 0xF0,
         midgard_alu_op_frsqrt     = 0xF2,
         midgard_alu_op_fsqrt      = 0xF3,
@@ -498,7 +498,7 @@ static char *alu_opcode_names[256] = {
         [midgard_alu_op_frcp]       = "frcp",
         [midgard_alu_op_frsqrt]     = "frsqrt",
         [midgard_alu_op_fsqrt]      = "fsqrt",
-        [midgard_alu_op_fpow]       = "fpow",
+        [midgard_alu_op_fpow_pt1]   = "fpow_pt1",
         [midgard_alu_op_fexp2]      = "fexp2",
         [midgard_alu_op_flog2]      = "flog2",
         [midgard_alu_op_fsin]       = "fsin",

src/gallium/drivers/panfrost/midgard/midgard_compile.c
@@ -1016,7 +1016,6 @@ emit_alu(compiler_context *ctx, nir_alu_instr *instr)
                 ALU_CASE(frcp, frcp);
                 ALU_CASE(frsq, frsqrt);
                 ALU_CASE(fsqrt, fsqrt);
-                ALU_CASE(fpow, fpow);
                 ALU_CASE(fexp2, fexp2);
                 ALU_CASE(flog2, flog2);
 

src/gallium/drivers/panfrost/midgard/midgard_compile.h
@@ -73,6 +73,7 @@ static const nir_shader_compiler_options midgard_nir_options = {
         .lower_fdiv = true,
         .lower_idiv = true,
         .lower_isign = true,
+        .lower_fpow = true,
 
         .vertex_id_zero_based = true,
         .lower_extract_byte = true,