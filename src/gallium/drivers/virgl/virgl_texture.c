7394ef4a7293e7ec6322faf18e7ddeef9c2c0183
Erik Faye-Lund <erik.faye-lund@collabora.com>
2019-04-10 12:22:34 +0200

    virgl: do not warn about display-target binding
    We never want to display a transfer-temp surface, so let's ignore that
    flag when calculating the new binding flags.
    
    Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

--- src/gallium/drivers/virgl/virgl_texture.c
+++ src/gallium/drivers/virgl/virgl_texture.c
@@ -64,7 +64,7 @@ static void virgl_copy_region_with_blit(struct pipe_context *pipe,
 static unsigned temp_bind(unsigned orig)
 {
    unsigned warn = ~(PIPE_BIND_RENDER_TARGET | PIPE_BIND_DEPTH_STENCIL |
-                     PIPE_BIND_SAMPLER_VIEW);
+                     PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_DISPLAY_TARGET);
    if (orig & warn)
       fprintf(stderr, "Waring, possibly unhandled bind: %x\n", orig & warn);
    return orig & (PIPE_BIND_DEPTH_STENCIL | PIPE_BIND_RENDER_TARGET);
