dfd79079daf08cba0e1d5defc18dedeffb0c6327
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2019-04-18 17:39:36 +0100

    anv: fix uninitialized pthread cond clock domain
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: 843775bab78a6b ("anv: Rework fences")
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -2051,7 +2051,7 @@ VkResult anv_CreateDevice(
       result = vk_error(VK_ERROR_INITIALIZATION_FAILED);
       goto fail_mutex;
    }
-   if (pthread_cond_init(&device->queue_submit, NULL) != 0) {
+   if (pthread_cond_init(&device->queue_submit, &condattr) != 0) {
       pthread_condattr_destroy(&condattr);
       result = vk_error(VK_ERROR_INITIALIZATION_FAILED);
       goto fail_mutex;
