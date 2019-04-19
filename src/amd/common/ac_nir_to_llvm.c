Grima04/mesa-turnip-kgsl

commit 78c551aca1c785470e3c0480e33a072b0b5f8928

ac/nir: use new LLVM 8 intrinsics for SSBO atomics except cmpswap
Use the raw version (ie. IDXEN=0) because vindex is unused.
Use the old intrinsic for compare&swap because the new one
hangs the GPU for some reasons.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/common/ac_nir_to_llvm.c
@@ -1648,17 +1648,6 @@ static LLVMValueRef visit_atomic_ssbo(struct ac_nir_context *ctx,
 	LLVMValueRef params[6];
 	int arg_count = 0;
 
-	if (instr->intrinsic == nir_intrinsic_ssbo_atomic_comp_swap) {
-		params[arg_count++] = ac_llvm_extract_elem(&ctx->ac, get_src(ctx, instr->src[3]), 0);
-	}
-	params[arg_count++] = ac_llvm_extract_elem(&ctx->ac, get_src(ctx, instr->src[2]), 0);
-	params[arg_count++] = ctx->abi->load_ssbo(ctx->abi,
-						 get_src(ctx, instr->src[0]),
-						 true);
-	params[arg_count++] = ctx->ac.i32_0; /* vindex */
-	params[arg_count++] = get_src(ctx, instr->src[1]);      /* voffset */
-	params[arg_count++] = ctx->ac.i1false;  /* slc */
-
 	switch (instr->intrinsic) {
 	case nir_intrinsic_ssbo_atomic_add:
 		op = "add";
@@ -1694,11 +1683,27 @@ static LLVMValueRef visit_atomic_ssbo(struct ac_nir_context *ctx,
 		abort();
 	}
 
-	if (HAVE_LLVM >= 0x900 &&
+	if (instr->intrinsic == nir_intrinsic_ssbo_atomic_comp_swap) {
+		params[arg_count++] = ac_llvm_extract_elem(&ctx->ac, get_src(ctx, instr->src[3]), 0);
+	}
+	params[arg_count++] = ac_llvm_extract_elem(&ctx->ac, get_src(ctx, instr->src[2]), 0);
+	params[arg_count++] = ctx->abi->load_ssbo(ctx->abi,
+						  get_src(ctx, instr->src[0]),
+						  true);
+
+	if (HAVE_LLVM >= 0x800 &&
 	    instr->intrinsic != nir_intrinsic_ssbo_atomic_comp_swap) {
+		params[arg_count++] = get_src(ctx, instr->src[1]); /* voffset */
+		params[arg_count++] = ctx->ac.i32_0; /* soffset */
+		params[arg_count++] = ctx->ac.i32_0; /* slc */
+
 		snprintf(name, sizeof(name),
-			 "llvm.amdgcn.buffer.atomic.%s.i32", op);
+			 "llvm.amdgcn.raw.buffer.atomic.%s.i32", op);
 	} else {
+		params[arg_count++] = ctx->ac.i32_0; /* vindex */
+		params[arg_count++] = get_src(ctx, instr->src[1]); /* voffset */
+		params[arg_count++] = ctx->ac.i1false; /* slc */
+
 		snprintf(name, sizeof(name),
 			 "llvm.amdgcn.buffer.atomic.%s", op);
 	}

commit 895e10d2dbc9b0efcb97f8232948f780a32c1524

ac/nir: only use the new raw/struct SSBO atomic intrinsics with LLVM 9+
They are buggy with older LLVM version, see r358579.

Fixes: 78c551aca1c ("ac/nir: use new LLVM 8 intrinsics for SSBO atomics except cmpswap")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

src/amd/common/ac_nir_to_llvm.c
@@ -1692,8 +1692,11 @@ static LLVMValueRef visit_atomic_ssbo(struct ac_nir_context *ctx,
 						  get_src(ctx, instr->src[0]),
 						  true);
 
-	if (HAVE_LLVM >= 0x800 &&
+	if (HAVE_LLVM >= 0x900 &&
 	    instr->intrinsic != nir_intrinsic_ssbo_atomic_comp_swap) {
+		/* XXX: The new raw/struct atomic intrinsics are buggy with
+		 * LLVM 8, see r358579.
+		 */
 		params[arg_count++] = get_src(ctx, instr->src[1]); /* voffset */
 		params[arg_count++] = ctx->ac.i32_0; /* soffset */
 		params[arg_count++] = ctx->ac.i32_0; /* slc */