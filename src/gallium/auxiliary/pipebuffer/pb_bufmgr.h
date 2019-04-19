f5c08d981885de4f46b251f6440f1f5587bd878d
Eric Anholt <eric@anholt.net>
2019-04-19 15:02:22 -0700

    gallium: Remove the pool pipebuffer manager.
    Noticed while trying to decide if pipebuffer was of any use to me, and
    found that nothing has used it in the last 10 years at least.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
    Reviewed-by: Kristian Høgsberg <hoegsberg@gmail.com>

--- src/gallium/auxiliary/pipebuffer/pb_bufmgr.h
+++ src/gallium/auxiliary/pipebuffer/pb_bufmgr.h
@@ -33,13 +33,11 @@
  * "buffer factory" would probably a more accurate description.
  * 
  * You can chain buffer managers so that you can have a finer grained memory
- * management and pooling.
+ * management.
  * 
  * For example, for a simple batch buffer manager you would chain:
  * - the native buffer manager, which provides DMA memory from the graphics
  * memory space;
- * - the pool buffer manager, which keep around a pool of equally sized buffers
- * to avoid latency associated with the native buffer manager; 
  * - the fenced buffer manager, which will delay buffer destruction until the 
  * the moment the card finishing processing it. 
  * 
@@ -96,21 +94,6 @@ struct pb_manager
  */
 struct pb_manager *
 pb_malloc_bufmgr_create(void);
-
-
-/** 
- * Static buffer pool sub-allocator.
- * 
- * Manages the allocation of equally sized buffers. It does so by allocating
- * a single big buffer and divide it equally sized buffers. 
- * 
- * It is meant to manage the allocation of batch buffer pools.
- */
-struct pb_manager *
-pool_bufmgr_create(struct pb_manager *provider, 
-                   pb_size n, pb_size size,
-                   const struct pb_desc *desc);
-
 
 /** 
  * Static sub-allocator based the old memory manager.
