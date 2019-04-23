a3c202de0a963c0562796cf75e3a9b3eedf1afad
Julien Isorce <jisorce@oblong.com>
2019-04-23 14:26:33 -0700

    gallium: add resource_get_info to pipe_screen
    Generic plumbing.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=110443
    Signed-off-by: Julien Isorce <jisorce@oblong.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/auxiliary/driver_ddebug/dd_screen.c
+++ src/gallium/auxiliary/driver_ddebug/dd_screen.c
@@ -311,6 +311,17 @@ dd_screen_resource_get_handle(struct pipe_screen *_screen,
    return screen->resource_get_handle(screen, pipe, resource, handle, usage);
 }
 
+static void
+dd_screen_resource_get_info(struct pipe_screen *_screen,
+                            struct pipe_resource *resource,
+                            unsigned *stride,
+                            unsigned *offset)
+{
+   struct pipe_screen *screen = dd_screen(_screen)->screen;
+
+   screen->resource_get_info(screen, resource, stride, offset);
+}
+
 static bool
 dd_screen_check_resource_capability(struct pipe_screen *_screen,
                                     struct pipe_resource *resource,
@@ -554,6 +565,7 @@ ddebug_screen_create(struct pipe_screen *screen)
    SCR_INIT(resource_from_user_memory);
    SCR_INIT(check_resource_capability);
    dscreen->base.resource_get_handle = dd_screen_resource_get_handle;
+   SCR_INIT(resource_get_info);
    SCR_INIT(resource_changed);
    dscreen->base.resource_destroy = dd_screen_resource_destroy;
    SCR_INIT(flush_frontbuffer);
