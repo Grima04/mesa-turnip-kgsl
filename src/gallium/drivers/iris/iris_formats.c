b15f5cfd20c8b079918dd19bed32de7cc9617100
Rafael Antognolli <rafael.antognolli@intel.com>
2019-04-29 16:02:58 -0700

    iris: Do not advertise multisampled image load/store.
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/gallium/drivers/iris/iris_formats.c
+++ src/gallium/drivers/iris/iris_formats.c
@@ -454,6 +454,11 @@ iris_is_format_supported(struct pipe_screen *pscreen,
    }
 
    if (usage & PIPE_BIND_SHADER_IMAGE) {
+      /* Dataport doesn't support compression, and we can't resolve an MCS
+       * compressed surface.
+       */
+      supported &= sample_count == 1;
+
       // XXX: allow untyped reads
       supported &= isl_format_supports_typed_reads(devinfo, format) &&
                    isl_format_supports_typed_writes(devinfo, format);
