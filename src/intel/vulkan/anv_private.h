629806b55bccd7f3e5b7b753820c4442fdb30bbe
Nanley Chery <nanley.g.chery@intel.com>
2019-04-30 14:49:10 -0700

    anv: Fix some depth buffer sampling cases on ICL+
    Don't attempt sampling with HiZ if the sampler lacks support for it. On
    ICL, the HW docs state that sampling with HiZ is not supported and that
    instances of AUX_HIZ in the RENDER_SURFACE_STATE object will be
    interpreted as AUX_NONE.
    
    Cc: <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Anuj Phogat <anuj.phogat@gmail.com>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -3187,7 +3187,13 @@ anv_can_sample_with_hiz(const struct gen_device_info * const devinfo,
    if (!(image->aspects & VK_IMAGE_ASPECT_DEPTH_BIT))
       return false;
 
-   if (devinfo->gen < 8)
+   /* Allow this feature on BDW even though it is disabled in the BDW devinfo
+    * struct. There's documentation which suggests that this feature actually
+    * reduces performance on BDW, but it has only been observed to help so
+    * far. Sampling fast-cleared blocks on BDW must also be handled with care
+    * (see depth_stencil_attachment_compute_aux_usage() for more info).
+    */
+   if (devinfo->gen != 8 && !devinfo->has_sample_with_hiz)
       return false;
 
    return image->samples == 1;
