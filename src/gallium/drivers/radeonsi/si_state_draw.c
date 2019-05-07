00707922d40dfc6b8f439ddea15157e30b6975fe
Nicolai Hähnle <nicolai.haehnle@amd.com>
2019-05-08 00:54:46 +0200

    radeonsi/gfx10: implement si_emit_derived_tess_state
    Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/gallium/drivers/radeonsi/si_state_draw.c
+++ src/gallium/drivers/radeonsi/si_state_draw.c
@@ -258,8 +258,12 @@ static void si_emit_derived_tess_state(struct si_context *sctx,
 	assert(ls_current->config.lds_size == 0);
 
 	if (sctx->chip_class >= GFX9) {
-		unsigned hs_rsrc2 = ls_current->config.rsrc2 |
-				    S_00B42C_LDS_SIZE_GFX9(lds_size);
+		unsigned hs_rsrc2 = ls_current->config.rsrc2;
+
+		if (sctx->chip_class >= GFX10)
+			hs_rsrc2 |= S_00B42C_LDS_SIZE_GFX10(lds_size);
+		else
+			hs_rsrc2 |= S_00B42C_LDS_SIZE_GFX9(lds_size);
 
 		radeon_set_sh_reg(cs, R_00B42C_SPI_SHADER_PGM_RSRC2_HS, hs_rsrc2);
 
