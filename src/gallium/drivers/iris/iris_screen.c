25afbb04c222aeadfb8caa2b906187feb7a3e2fe
Kenneth Graunke <kenneth@whitecape.org>
2019-05-14 20:31:46 -0700

    iris: Advertise coherent framebuffer fetches
    This lets us advertise GL_EXT_shader_framebuffer_fetch and
    GL_KHR_blend_equation_advanced_coherent support.

--- src/gallium/drivers/iris/iris_screen.c
+++ src/gallium/drivers/iris/iris_screen.c
@@ -193,7 +193,9 @@ iris_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_SURFACE_REINTERPRET_BLOCKS:
       return true;
    case PIPE_CAP_FBFETCH:
+      /* TODO: Support non-coherent FB fetch on Broadwell */
       return devinfo->gen >= 9 ? BRW_MAX_DRAW_BUFFERS : 0;
+   case PIPE_CAP_FBFETCH_COHERENT:
    case PIPE_CAP_CONSERVATIVE_RASTER_INNER_COVERAGE:
    case PIPE_CAP_POST_DEPTH_COVERAGE:
    case PIPE_CAP_SHADER_STENCIL_EXPORT:
