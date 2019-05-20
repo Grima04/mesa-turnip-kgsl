c71ca65405e677ac65d176cd4d6138923df6cd56
Brian Paul <brianp@vmware.com>
2019-05-20 06:24:06 -0600

    svga: clamp max_const_buffers to SVGA_MAX_CONST_BUFS
    In case the device reports 15 (or more) buffers.
    
    Reviewed-by: Charmaine Lee <charmainel@vmware.com>

--- src/gallium/drivers/svga/svga_screen.c
+++ src/gallium/drivers/svga/svga_screen.c
@@ -1079,7 +1079,8 @@ svga_screen_create(struct svga_winsys_screen *sws)
       /* Maximum number of constant buffers */
       svgascreen->max_const_buffers =
          get_uint_cap(sws, SVGA3D_DEVCAP_DX_MAX_CONSTANT_BUFFERS, 1);
-      assert(svgascreen->max_const_buffers <= SVGA_MAX_CONST_BUFS);
+      svgascreen->max_const_buffers = MIN2(svgascreen->max_const_buffers,
+                                           SVGA_MAX_CONST_BUFS);
 
       screen->is_format_supported = svga_is_dx_format_supported;
    }
