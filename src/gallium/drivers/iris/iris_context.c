0f1b68ebeeee196ced42d8ac66d231338b507bad
Kenneth Graunke <kenneth@whitecape.org>
2019-05-29 14:48:41 -0700

    iris: Re-emit Surface State Base Address when context is lost.
    When we hit a GPU hang, we failed to reset Surface State Base Address
    right away, and would keep hanging until we filled up the binder.  Then
    we'd finally get it right after a lot of repeated stumbles.  Update it
    right away so we hopefully hang fewer times before succeeding.

--- src/gallium/drivers/iris/iris_context.c
+++ src/gallium/drivers/iris/iris_context.c
@@ -99,6 +99,7 @@ iris_lost_context_state(struct iris_batch *batch)
 
    ice->state.dirty = ~0ull;
    memset(ice->state.last_grid, 0, sizeof(ice->state.last_grid));
+   batch->last_surface_base_address = ~0ull;
 }
 
 static enum pipe_reset_status
