0905189a25edabaea8de5482b9d5f531a92aa1cf
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2019-05-30 09:58:01 +0200

    radv: enable VK_EXT_sample_locations
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-By: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -1369,17 +1369,9 @@ void radv_GetPhysicalDeviceProperties2(
 		case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SAMPLE_LOCATIONS_PROPERTIES_EXT: {
 			VkPhysicalDeviceSampleLocationsPropertiesEXT *properties =
 				(VkPhysicalDeviceSampleLocationsPropertiesEXT *)ext;
-			/* TODO: The ext is currently disabled because the
-			 * driver needs to handle sample locations during
-			 * layout transitions for depth/stencil surfaces and
-			 * HTILE.
-			 */
-			properties->sampleLocationSampleCounts = VK_SAMPLE_COUNT_1_BIT;
-			/*
 			properties->sampleLocationSampleCounts = VK_SAMPLE_COUNT_2_BIT |
 								 VK_SAMPLE_COUNT_4_BIT |
 								 VK_SAMPLE_COUNT_8_BIT;
-			*/
 			properties->maxSampleLocationGridSize = (VkExtent2D){ 2 , 2 };
 			properties->sampleLocationCoordinateRange[0] = 0.0f;
 			properties->sampleLocationCoordinateRange[1] = 0.9375f;
