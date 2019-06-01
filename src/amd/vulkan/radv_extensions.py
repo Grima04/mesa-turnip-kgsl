e8a256eb543b845e18b6a5eeacaeb2eaadefacd0
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2019-06-01 21:45:39 +0200

    radv: Add VK_KHR_pipeline_executable_properties in disabled state.
    So we can add the functions.
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>

--- src/amd/vulkan/radv_extensions.py
+++ src/amd/vulkan/radv_extensions.py
@@ -80,6 +80,7 @@ EXTENSIONS = [
     Extension('VK_KHR_maintenance1',                      1, True),
     Extension('VK_KHR_maintenance2',                      1, True),
     Extension('VK_KHR_maintenance3',                      1, True),
+    Extension('VK_KHR_pipeline_executable_properties',    1, False),
     Extension('VK_KHR_push_descriptor',                   1, True),
     Extension('VK_KHR_relaxed_block_layout',              1, True),
     Extension('VK_KHR_sampler_mirror_clamp_to_edge',      1, True),
