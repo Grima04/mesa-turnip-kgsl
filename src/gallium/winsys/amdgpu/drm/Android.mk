cf389ba89507b20dee1ac640b3a496aa86f82ac1
Mauro Rossi <issor.oruam@gmail.com>
2019-06-08 15:15:09 +0200

    android: winsys/amdgpu,radv: fix generated amdgfxregs.h header dependecies
    Fix android building errors in winsys/amdgpu and radv
    due to 'amdgfxregs.h' not found.
    
    Changelog:
    amd/common - generated $(intermediated)/common path is added to exports
    winsys/amdgpu - libmesa_amd_common static dependency is added
    radv - correct generated $(intermediated)/common path is added to includes
    
    Fixes: f480b8a ("amd/common: use generated register header")
    Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

--- src/gallium/winsys/amdgpu/drm/Android.mk
+++ src/gallium/winsys/amdgpu/drm/Android.mk
@@ -32,7 +32,7 @@ LOCAL_SRC_FILES := $(C_SOURCES)
 
 LOCAL_CFLAGS := $(AMDGPU_CFLAGS)
 
-LOCAL_STATIC_LIBRARIES := libmesa_amdgpu_addrlib
+LOCAL_STATIC_LIBRARIES := libmesa_amdgpu_addrlib libmesa_amd_common
 
 LOCAL_SHARED_LIBRARIES := libdrm_amdgpu
 LOCAL_MODULE := libmesa_winsys_amdgpu
