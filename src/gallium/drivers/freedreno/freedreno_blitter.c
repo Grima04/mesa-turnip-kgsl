ce12ac8c2bc4114ffa9386a473fcf24df079fb1e
Rob Clark <robdclark@chromium.org>
2019-06-12 11:24:55 -0700

    freedreno/blitter: remove dead code
    The src/dst format is overriden from the pipe_blit_info, so this just
    logic just serves to confuse the reader.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- src/gallium/drivers/freedreno/freedreno_blitter.c
+++ src/gallium/drivers/freedreno/freedreno_blitter.c
@@ -41,10 +41,6 @@ default_dst_texture(struct pipe_surface *dst_templ, struct pipe_resource *dst,
 		unsigned dstlevel, unsigned dstz)
 {
 	memset(dst_templ, 0, sizeof(*dst_templ));
-	if (dst->target == PIPE_BUFFER)
-		dst_templ->format = PIPE_FORMAT_R8_UINT;
-	else
-		dst_templ->format = util_format_linear(dst->format);
 	dst_templ->u.tex.level = dstlevel;
 	dst_templ->u.tex.first_layer = dstz;
 	dst_templ->u.tex.last_layer = dstz;
@@ -67,9 +63,6 @@ default_src_texture(struct pipe_sampler_view *src_templ,
 
 	if (src->target  == PIPE_BUFFER) {
 		src_templ->target = PIPE_TEXTURE_1D;
-		src_templ->format = PIPE_FORMAT_R8_UINT;
-	} else {
-		src_templ->format = util_format_linear(src->format);
 	}
 	src_templ->u.tex.first_level = srclevel;
 	src_templ->u.tex.last_level = srclevel;
