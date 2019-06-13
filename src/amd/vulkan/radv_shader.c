miscco/mesa

radv: enable AMD_shader_ballot with RADV_PERFTEST_SHADER_BALLOT ('shader_ballot')

src/amd/vulkan/radv_shader.c
@@ -292,6 +292,7 @@ radv_shader_compile_to_nir(struct radv_device *device,
 				.multiview = true,
 				.physical_storage_buffer_address = true,
 				.runtime_descriptor_array = true,
+				.shader_ballot = device->instance->perftest_flags & RADV_PERFTEST_SHADER_BALLOT,
 				.shader_viewport_index_layer = true,
 				.stencil_export = true,
 				.storage_8bit = !(device->instance->perftest_flags & RADV_PERFTEST_ACO),

Grima04/mesa-turnip-kgsl

Reviewed-by: Connor Abbott <cwabbott0@gmail.com>

src/amd/vulkan/radv_shader.c
@@ -246,7 +246,7 @@ radv_shader_compile_to_nir(struct radv_device *device,
 			.lower_ubo_ssbo_access_to_offsets = true,
 			.caps = {
 				.amd_gcn_shader = true,
-				.amd_shader_ballot = false,
+				.amd_shader_ballot = device->instance->perftest_flags & RADV_PERFTEST_SHADER_BALLOT,
 				.amd_trinary_minmax = true,
 				.derivative_group = true,
 				.descriptor_array_dynamic_indexing = true,