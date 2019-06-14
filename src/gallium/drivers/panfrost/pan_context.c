443f9ae0ad19fbb13dfde19f406c4e2892af55c0
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2019-06-14 12:48:06 -0700

    panfrost: Remove forced flush on clears
    This worked around a bug in oooold versions of Panfrost. Nowadays, its
    presence is, at best, *creating* bugs. Let's wack it.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

--- src/gallium/drivers/panfrost/pan_context.c
+++ src/gallium/drivers/panfrost/pan_context.c
@@ -1391,10 +1391,6 @@ panfrost_submit_frame(struct panfrost_context *ctx, bool flush_immediate,
         /* Edge case if screen is cleared and nothing else */
         bool has_draws = ctx->draw_count > 0;
 
-        /* Workaround a bizarre lockup (a hardware errata?) */
-        if (!has_draws)
-                flush_immediate = true;
-
 #ifndef DRY_RUN
         
         bool is_scanout = panfrost_is_scanout(ctx);
