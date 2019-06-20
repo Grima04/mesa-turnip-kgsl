78cdf9a99f07679f95dacd9ae6712278a2e40202
Marek Olšák <marek.olsak@amd.com>
2019-06-19 20:42:18 -0400

    amd/addrlib: add gfx10 support
    Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/addrlib/src/chip/gfx10/gfx10_gb_reg.h
+++ src/amd/addrlib/src/chip/gfx10/gfx10_gb_reg.h
@@ -24,11 +24,11 @@
  * of the Software.
  */
 
-#if !defined (__GFX9_GB_REG_H__)
-#define __GFX9_GB_REG_H__
+#if !defined (__GFX10_GB_REG_H__)
+#define __GFX10_GB_REG_H__
 
 /*
-*    gfx9_gb_reg.h
+*    gfx10_gb_reg.h
 *
 *    Register Spec Release:  1.0
 *
@@ -45,45 +45,27 @@
 #define BIGENDIAN_CPU
 #endif
 
-union GB_ADDR_CONFIG {
-    struct {
-#if        defined(LITTLEENDIAN_CPU)
+union GB_ADDR_CONFIG
+{
+    struct
+    {
+#if defined(LITTLEENDIAN_CPU)
         unsigned int                       NUM_PIPES : 3;
         unsigned int            PIPE_INTERLEAVE_SIZE : 3;
         unsigned int            MAX_COMPRESSED_FRAGS : 2;
-        unsigned int            BANK_INTERLEAVE_SIZE : 3;
-        unsigned int                                 : 1;
-        unsigned int                       NUM_BANKS : 3;
-        unsigned int                                 : 1;
-        unsigned int         SHADER_ENGINE_TILE_SIZE : 3;
-        unsigned int              NUM_SHADER_ENGINES : 2;
-        unsigned int                        NUM_GPUS : 3;
-        unsigned int             MULTI_GPU_TILE_SIZE : 2;
-        unsigned int                   NUM_RB_PER_SE : 2;
-        unsigned int                        ROW_SIZE : 2;
-        unsigned int                 NUM_LOWER_PIPES : 1;
-        unsigned int                       SE_ENABLE : 1;
-#elif        defined(BIGENDIAN_CPU)
-        unsigned int                       SE_ENABLE : 1;
-        unsigned int                 NUM_LOWER_PIPES : 1;
-        unsigned int                        ROW_SIZE : 2;
-        unsigned int                   NUM_RB_PER_SE : 2;
-        unsigned int             MULTI_GPU_TILE_SIZE : 2;
-        unsigned int                        NUM_GPUS : 3;
-        unsigned int              NUM_SHADER_ENGINES : 2;
-        unsigned int         SHADER_ENGINE_TILE_SIZE : 3;
-        unsigned int                                 : 1;
-        unsigned int                       NUM_BANKS : 3;
-        unsigned int                                 : 1;
-        unsigned int            BANK_INTERLEAVE_SIZE : 3;
+        unsigned int                       NUM_PKRS  : 3;
+        unsigned int                                 : 21;
+#elif defined(BIGENDIAN_CPU)
+        unsigned int                                 : 21;
+        unsigned int                       NUM_PKRS  : 3;
         unsigned int            MAX_COMPRESSED_FRAGS : 2;
         unsigned int            PIPE_INTERLEAVE_SIZE : 3;
         unsigned int                       NUM_PIPES : 3;
 #endif
     } bitfields, bits;
     unsigned int    u32All;
-    signed int    i32All;
-    float    f32All;
+    int             i32All;
+    float           f32All;
 };
 
 #endif
