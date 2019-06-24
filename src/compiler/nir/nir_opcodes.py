miscco/mesa

nir: introduce lowering of bitfield_insert to bfm and a new opcode bitfield_select.
bitfield_select is defined as:
bitfield_select(mask, base, insert) = (mask & base) | (~mask & insert)
matching the behavior of AMD's BFI instruction.

src/compiler/nir/nir_opcodes.py
@@ -871,6 +871,9 @@ if (mask == 0) {
 }
 """)
 
+
+triop("bitfield_select", tuint32, "", "(src0 & src1) | (~src0 & src2)")
+
 # SM5 ubfe/ibfe assembly: only the 5 least significant bits of offset and bits are used.
 opcode("ubfe", 0, tuint32,
        [0, 0, 0], [tuint32, tuint32, tuint32], False, "", """

Grima04/mesa-turnip-kgsl

Reviewed-by: Connor Abbott <cwabbott0@gmail.com>

src/compiler/nir/nir_opcodes.py
@@ -871,6 +871,9 @@ if (mask == 0) {
 }
 """)
 
+
+triop("bitfield_select", tuint, "", "(src0 & src1) | (~src0 & src2)")
+
 # SM5 ubfe/ibfe assembly: only the 5 least significant bits of offset and bits are used.
 opcode("ubfe", 0, tuint32,
        [0, 0, 0], [tuint32, tuint32, tuint32], False, "", """