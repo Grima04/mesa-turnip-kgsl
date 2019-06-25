anholt/mesa

commit 3d198926a487cefc0316c2c4d1ebb20ff8ebf535

freedreno: use fd_bc_alloc_batch instead of fd_batch_create.
Following the commit 2385d7b066 and 8e798e28f7, for resource dependancy
tracking.

Fixes: dEQP-GLES31.functional.image_load_store.early_fragment_tests.no_early_fragment_tests_depth_fbo
with FD_MESA_DEBUG=inorder

Signed-off-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/a5xx/fd5_blitter.c
@@ -459,7 +459,7 @@ fd5_blitter_blit(struct fd_context *ctx, const struct pipe_blit_info *info)
 		return;
 	}
 
-	batch = fd_batch_create(ctx, true);
+	batch = fd_bc_alloc_batch(&ctx->screen->batch_cache, ctx, true);
 
 	fd5_emit_restore(batch, batch->draw);
 	fd5_emit_lrz_flush(batch->draw);

src/gallium/drivers/freedreno/freedreno_batch.c
@@ -358,7 +358,7 @@ fd_batch_flush(struct fd_batch *batch, bool sync, bool force)
 			 */
 			new_batch = NULL;
 		} else {
-			new_batch = fd_batch_create(ctx, false);
+			new_batch = fd_bc_alloc_batch(&ctx->screen->batch_cache, ctx, false);
 			util_copy_framebuffer_state(&new_batch->framebuffer, &batch->framebuffer);
 		}
 

Grima04/mesa-turnip-kgsl

commit 927fb50727e6e6652c0e4ce300e098843ad12013

freedreno/a5xx: fix batch leak in fd5 blitter path
Fixes: 3d198926a48 freedreno: use fd_bc_alloc_batch instead of fd_batch_create.
Signed-off-by: Rob Clark <robdclark@chromium.org>

src/gallium/drivers/freedreno/a5xx/fd5_blitter.c
@@ -482,6 +482,7 @@ fd5_blitter_blit(struct fd_context *ctx, const struct pipe_blit_info *info)
 	batch->needs_flush = true;
 
 	fd_batch_flush(batch, false, false);
+	fd_batch_reference(&batch, NULL);
 
 	return true;
 }