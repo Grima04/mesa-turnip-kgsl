yuq/mesa-lima

commit b5e04e9217bad938970c41fd56edea9c9876825a

radv: Support allocating variable size descriptor sets.
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_descriptor_set.c
@@ -392,6 +392,7 @@ static VkResult
 radv_descriptor_set_create(struct radv_device *device,
 			   struct radv_descriptor_pool *pool,
 			   const struct radv_descriptor_set_layout *layout,
+			   const uint32_t *variable_count,
 			   struct radv_descriptor_set **out_set)
 {
 	struct radv_descriptor_set *set;
@@ -420,9 +421,9 @@ radv_descriptor_set_create(struct radv_device *device,
 	}
 
 	set->layout = layout;
-	if (layout->size) {
-		uint32_t layout_size = align_u32(layout->size, 32);
-		set->size = layout->size;
+	uint32_t layout_size = align_u32(layout->size, 32);
+	if (layout_size) {
+		set->size = layout_size;
 
 		if (!pool->host_memory_base && pool->entry_count == pool->max_entry_count) {
 			vk_free2(&device->alloc, NULL, set);
@@ -648,14 +649,26 @@ VkResult radv_AllocateDescriptorSets(
 	uint32_t i;
 	struct radv_descriptor_set *set = NULL;
 
+	const VkDescriptorSetVariableDescriptorCountAllocateInfoEXT *variable_counts =
+		vk_find_struct_const(pAllocateInfo->pNext, DESCRIPTOR_SET_VARIABLE_DESCRIPTOR_COUNT_ALLOCATE_INFO_EXT);
+	const uint32_t zero = 0;
+
 	/* allocate a set of buffers for each shader to contain descriptors */
 	for (i = 0; i < pAllocateInfo->descriptorSetCount; i++) {
 		RADV_FROM_HANDLE(radv_descriptor_set_layout, layout,
 				 pAllocateInfo->pSetLayouts[i]);
 
+		const uint32_t *variable_count = NULL;
+		if (variable_counts) {
+			if (i < variable_counts->descriptorSetCount)
+				variable_count = variable_counts->pDescriptorCounts + i;
+			else
+				variable_count = &zero;
+		}
+
 		assert(!(layout->flags & VK_DESCRIPTOR_SET_LAYOUT_CREATE_PUSH_DESCRIPTOR_BIT_KHR));
 
-		result = radv_descriptor_set_create(device, pool, layout, &set);
+		result = radv_descriptor_set_create(device, pool, layout, variable_count, &set);
 		if (result != VK_SUCCESS)
 			break;
 

Grima04/mesa-turnip-kgsl

commit d7e6541cc720a7d11d678adbeec7143b099127b0

radv: Only allocate supplied number of descriptors when variable.
Fixes: b5e04e9217b "radv: Support allocating variable size descriptor sets."
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=111019
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_descriptor_set.c
@@ -503,7 +503,13 @@ radv_descriptor_set_create(struct radv_device *device,
 	}
 
 	set->layout = layout;
-	uint32_t layout_size = align_u32(layout->size, 32);
+	uint32_t layout_size = layout->size;
+	if (variable_count) {
+		assert(layout->has_variable_descriptors);
+		layout_size = layout->binding[layout->binding_count - 1].offset +
+		              *variable_count * layout->binding[layout->binding_count - 1].size;
+	}
+	layout_size = align_u32(layout_size, 32);
 	if (layout_size) {
 		set->size = layout_size;
 