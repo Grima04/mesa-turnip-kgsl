Grima04/mesa-turnip-kgsl

commit d7e6541cc720a7d11d678adbeec7143b099127b0

radv: Only allocate supplied number of descriptors when variable.
Fixes: b5e04e9217b "radv: Support allocating variable size descriptor sets."
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=111019
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_descriptor_set.c
@@ -503,7 +503,13 @@ radv_descriptor_set_create(struct radv_device *device,
 	}
 
 	set->layout = layout;
-	uint32_t layout_size = align_u32(layout->size, 32);
+	uint32_t layout_size = layout->size;
+	if (variable_count) {
+		assert(layout->has_variable_descriptors);
+		layout_size = layout->binding[layout->binding_count - 1].offset +
+		              *variable_count * layout->binding[layout->binding_count - 1].size;
+	}
+	layout_size = align_u32(layout_size, 32);
 	if (layout_size) {
 		set->size = layout_size;
 

commit 8a053254b81497cb6b0708fe86986e461ee43771

radv: Fix interactions between variable descriptor count and inline uniform blocks.
Fixes: d7e6541cc72 "radv: Only allocate supplied number of descriptors when variable."
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_descriptor_set.c
@@ -506,8 +506,12 @@ radv_descriptor_set_create(struct radv_device *device,
 	uint32_t layout_size = layout->size;
 	if (variable_count) {
 		assert(layout->has_variable_descriptors);
+		uint32_t stride = layout->binding[layout->binding_count - 1].size;
+		if (layout->binding[layout->binding_count - 1].type == VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK_EXT)
+			stride = 1;
+
 		layout_size = layout->binding[layout->binding_count - 1].offset +
-		              *variable_count * layout->binding[layout->binding_count - 1].size;
+		              *variable_count * stride;
 	}
 	layout_size = align_u32(layout_size, 32);
 	if (layout_size) {