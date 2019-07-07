Grima04/mesa-turnip-kgsl

commit 38305e6c94ea31e0f775cbc693fea267023e3bc4

anv: replace hard-coded platform list with vk.xml parse
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

src/intel/vulkan/anv_extensions_gen.py
@@ -31,6 +31,8 @@ from mako.template import Template
 
 from anv_extensions import *
 
+platform_defines = []
+
 def _init_exts_from_xml(xml):
     """ Walk the Vulkan XML and fill out extra extension information. """
 
@@ -39,6 +41,9 @@ def _init_exts_from_xml(xml):
     ext_name_map = {}
     for ext in EXTENSIONS:
         ext_name_map[ext.name] = ext
+
+    for platform in xml.findall('./platforms/platform'):
+        platform_defines.append(platform.attrib['protect'])
 
     for ext_elem in xml.findall('.extensions/extension'):
         ext_name = ext_elem.attrib['name']
@@ -103,12 +108,12 @@ _TEMPLATE_C = Template(COPYRIGHT + """
 #include "vk_util.h"
 
 /* Convert the VK_USE_PLATFORM_* defines to booleans */
-%for platform in ['ANDROID_KHR', 'WAYLAND_KHR', 'XCB_KHR', 'XLIB_KHR', 'DISPLAY_KHR', 'XLIB_XRANDR_EXT']:
-#ifdef VK_USE_PLATFORM_${platform}
-#   undef VK_USE_PLATFORM_${platform}
-#   define VK_USE_PLATFORM_${platform} true
+%for platform_define in platform_defines:
+#ifdef ${platform_define}
+#   undef ${platform_define}
+#   define ${platform_define} true
 #else
-#   define VK_USE_PLATFORM_${platform} false
+#   define ${platform_define} false
 #endif
 %endfor
 
@@ -204,6 +209,7 @@ if __name__ == '__main__':
         'MAX_API_VERSION': MAX_API_VERSION,
         'instance_extensions': [e for e in EXTENSIONS if e.type == 'instance'],
         'device_extensions': [e for e in EXTENSIONS if e.type == 'device'],
+        'platform_defines': platform_defines,
     }
 
     if args.out_h:

commit 5493ec3c199b3190dc5d247d9e2a0eccd5066cf2

anv: manually add KHR_display to the list of platforms
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 38305e6c94ea31 ("anv: replace hard-coded platform list with vk.xml parse")
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=111078
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>

src/intel/vulkan/anv_extensions_gen.py
@@ -42,6 +42,8 @@ def _init_exts_from_xml(xml):
     for ext in EXTENSIONS:
         ext_name_map[ext.name] = ext
 
+    # KHR_display is missing from the list.
+    platform_defines.append('VK_USE_PLATFORM_DISPLAY_KHR')
     for platform in xml.findall('./platforms/platform'):
         platform_defines.append(platform.attrib['protect'])
 