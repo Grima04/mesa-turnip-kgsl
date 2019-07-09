Grima04/mesa-turnip-kgsl

commit 204e4da9b4768033b81ec69d448eca9ba1c872fe

radv: Use correct gs_out with tessellation.
We should use the primitives output by the TES in that case.

There is always a separate TES if there is no GS.

Reviewed-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_pipeline.c
@@ -4156,6 +4156,9 @@ radv_pipeline_init(struct radv_pipeline *pipeline,
 	if (radv_pipeline_has_gs(pipeline)) {
 		gs_out = si_conv_gl_prim_to_gs_out(pipeline->shaders[MESA_SHADER_GEOMETRY]->info.gs.output_prim);
 		pipeline->graphics.can_use_guardband = gs_out == V_028A6C_OUTPRIM_TYPE_TRISTRIP;
+	} else if (radv_pipeline_has_tess(pipeline)) {
+		gs_out = si_conv_gl_prim_to_gs_out(pipeline->shaders[MESA_SHADER_TESS_EVAL]->info.tes.primitive_mode);
+		pipeline->graphics.can_use_guardband = gs_out == V_028A6C_OUTPRIM_TYPE_TRISTRIP;
 	} else {
 		gs_out = si_conv_prim_to_gs_out(pCreateInfo->pInputAssemblyState->topology);
 	}

commit 64cd972ffb479d950d8d28a3894ce274abb44c78

radv/gfx10: Use correct gs_out for tess point_mode.
Fixes: 204e4da9b47 "radv: Use correct gs_out with tessellation."
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_pipeline.c
@@ -4197,7 +4197,10 @@ radv_pipeline_init(struct radv_pipeline *pipeline,
 		gs_out = si_conv_gl_prim_to_gs_out(pipeline->shaders[MESA_SHADER_GEOMETRY]->info.gs.output_prim);
 		pipeline->graphics.can_use_guardband = gs_out == V_028A6C_OUTPRIM_TYPE_TRISTRIP;
 	} else if (radv_pipeline_has_tess(pipeline)) {
-		gs_out = si_conv_gl_prim_to_gs_out(pipeline->shaders[MESA_SHADER_TESS_EVAL]->info.tes.primitive_mode);
+		if (pipeline->shaders[MESA_SHADER_TESS_EVAL]->info.tes.point_mode)
+			gs_out = V_028A6C_OUTPRIM_TYPE_POINTLIST;
+		else
+			gs_out = si_conv_gl_prim_to_gs_out(pipeline->shaders[MESA_SHADER_TESS_EVAL]->info.tes.primitive_mode);
 		pipeline->graphics.can_use_guardband = gs_out == V_028A6C_OUTPRIM_TYPE_TRISTRIP;
 	} else {
 		gs_out = si_conv_prim_to_gs_out(pCreateInfo->pInputAssemblyState->topology);