7ebdf5be1501647b6b0518809c91ea8a5f3c6681
Erik Faye-Lund <erik.faye-lund@collabora.com>
2019-07-16 11:02:16 +0200

    zink: disable timestamp-queries
    We don't implement the get_timestamp context-method, so this is just
    going to crash if anyone tries to use it. Let's implement it later.
    
    Acked-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -165,8 +165,10 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_CONSTANT_BUFFER_OFFSET_ALIGNMENT:
       return screen->props.limits.minUniformBufferOffsetAlignment;
 
+#if 0 /* TODO: Enable me */
    case PIPE_CAP_QUERY_TIMESTAMP:
       return 1;
+#endif
 
    case PIPE_CAP_MIN_MAP_BUFFER_ALIGNMENT:
       return screen->props.limits.minMemoryMapAlignment;
