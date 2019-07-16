65e2cf98d5162b8926ab888f8f120b3574d44b43
Erik Faye-Lund <erik.faye-lund@collabora.com>
2019-07-16 17:02:56 +0200

    zink: initialize nr_samples for pipe_surface
    Acked-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/zink/zink_surface.c
+++ src/gallium/drivers/zink/zink_surface.c
@@ -48,6 +48,7 @@ zink_create_surface(struct pipe_context *pctx,
    surface->base.format = templ->format;
    surface->base.width = u_minify(pres->width0, level);
    surface->base.height = u_minify(pres->height0, level);
+   surface->base.nr_samples = templ->nr_samples;
    surface->base.u.tex.level = level;
    surface->base.u.tex.first_layer = templ->u.tex.first_layer;
    surface->base.u.tex.last_layer = templ->u.tex.last_layer;
