32344dc5811021409f04db50f461582c05aa6a80
Rafael Antognolli <rafael.antognolli@intel.com>
2019-07-19 15:30:56 -0700

    intel/gen_decoder: Decode SLICE_HASH_TABLE.

--- src/intel/common/gen_batch_decoder.c
+++ src/intel/common/gen_batch_decoder.c
@@ -753,6 +753,13 @@ decode_3dstate_scissor_state_pointers(struct gen_batch_decode_ctx *ctx,
 }
 
 static void
+decode_3dstate_slice_table_state_pointers(struct gen_batch_decode_ctx *ctx,
+                                          const uint32_t *p)
+{
+   decode_dynamic_state_pointers(ctx, "SLICE_HASH_TABLE", p, 1);
+}
+
+static void
 decode_load_register_imm(struct gen_batch_decode_ctx *ctx, const uint32_t *p)
 {
    struct gen_group *reg = gen_spec_find_register(ctx->spec, p[1]);
@@ -801,6 +808,7 @@ struct custom_decoder {
    { "3DSTATE_BLEND_STATE_POINTERS", decode_3dstate_blend_state_pointers },
    { "3DSTATE_CC_STATE_POINTERS", decode_3dstate_cc_state_pointers },
    { "3DSTATE_SCISSOR_STATE_POINTERS", decode_3dstate_scissor_state_pointers },
+   { "3DSTATE_SLICE_TABLE_STATE_POINTERS", decode_3dstate_slice_table_state_pointers },
    { "MI_LOAD_REGISTER_IMM", decode_load_register_imm }
 };
 
