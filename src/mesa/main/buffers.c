Grima04/mesa-turnip-kgsl

commit 35294f2eca8b49193f1bcb8c62edbcc1537a9cc3

mesa: fix pbuffers because internally they are front buffers
This fixes the egl_ext_device_base piglit test, which uses EGL pbuffers.

Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

src/mesa/main/buffers.c
@@ -93,32 +93,48 @@ supported_buffer_bitmask(const struct gl_context *ctx,
 static GLbitfield
 draw_buffer_enum_to_bitmask(const struct gl_context *ctx, GLenum buffer)
 {
+   /* If the front buffer is the only buffer, GL_BACK and all other flags
+    * that include BACK select the front buffer for drawing. There are
+    * several reasons we want to do this.
+    *
+    * 1) OpenGL ES 3.0 requires it:
+    *
+    *   Page 181 (page 192 of the PDF) in section 4.2.1 of the OpenGL
+    *   ES 3.0.1 specification says:
+    *
+    *     "When draw buffer zero is BACK, color values are written
+    *     into the sole buffer for single-buffered contexts, or into
+    *     the back buffer for double-buffered contexts."
+    *
+    *   We also do this for GLES 1 and 2 because those APIs have no
+    *   concept of selecting the front and back buffer anyway and it's
+    *   convenient to be able to maintain the magic behaviour of
+    *   GL_BACK in that case.
+    *
+    * 2) Pbuffers are back buffers from the application point of view,
+    *    but they are front buffers from the Mesa point of view,
+    *    because they are always single buffered.
+    */
+   if (!ctx->DrawBuffer->Visual.doubleBufferMode) {
+      switch (buffer) {
+      case GL_BACK:
+         buffer = GL_FRONT;
+         break;
+      case GL_BACK_RIGHT:
+         buffer = GL_FRONT_RIGHT;
+         break;
+      case GL_BACK_LEFT:
+         buffer = GL_FRONT_LEFT;
+         break;
+      }
+   }
+
    switch (buffer) {
       case GL_NONE:
          return 0;
       case GL_FRONT:
          return BUFFER_BIT_FRONT_LEFT | BUFFER_BIT_FRONT_RIGHT;
       case GL_BACK:
-         if (_mesa_is_gles(ctx)) {
-            /* Page 181 (page 192 of the PDF) in section 4.2.1 of the OpenGL
-             * ES 3.0.1 specification says:
-             *
-             *     "When draw buffer zero is BACK, color values are written
-             *     into the sole buffer for single-buffered contexts, or into
-             *     the back buffer for double-buffered contexts."
-             *
-             * Since there is no stereo rendering in ES 3.0, only return the
-             * LEFT bits.  This also satisfies the "n must be 1" requirement.
-             *
-             * We also do this for GLES 1 and 2 because those APIs have no
-             * concept of selecting the front and back buffer anyway and it's
-             * convenient to be able to maintain the magic behaviour of
-             * GL_BACK in that case.
-             */
-            if (ctx->DrawBuffer->Visual.doubleBufferMode)
-               return BUFFER_BIT_BACK_LEFT;
-            return BUFFER_BIT_FRONT_LEFT;
-         }
          return BUFFER_BIT_BACK_LEFT | BUFFER_BIT_BACK_RIGHT;
       case GL_RIGHT:
          return BUFFER_BIT_FRONT_RIGHT | BUFFER_BIT_BACK_RIGHT;

commit 78164a3a6c278adcbb93e21b64a575c3f8598327

mesa: Fix ReadBuffers with pbuffers
pbuffers are internally single-buffered.  Marek fixed DrawBuffers to
handle this case, but we need to fix ReadBuffers too.  Otherwise,
pretty much every conformance test fails because glReadPixels breaks.

v2: Refactor the switch into a helper (suggested by Eric Anholt)

Fixes: 35294f2eca8 ("mesa: fix pbuffers because internally they are front buffers")
Acked-by: Eric Engestrom <eric.engestrom@intel.com> (v1)
Reviewed-by: Eric Anholt <eric@anholt.net>

src/mesa/main/buffers.c
@@ -84,14 +84,8 @@ supported_buffer_bitmask(const struct gl_context *ctx,
    return mask;
 }
 
-
-/**
- * Helper routine used by glDrawBuffer and glDrawBuffersARB.
- * Given a GLenum naming one or more color buffers (such as
- * GL_FRONT_AND_BACK), return the corresponding bitmask of BUFFER_BIT_* flags.
- */
-static GLbitfield
-draw_buffer_enum_to_bitmask(const struct gl_context *ctx, GLenum buffer)
+static GLenum
+back_to_front_if_single_buffered(const struct gl_context *ctx, GLenum buffer)
 {
    /* If the front buffer is the only buffer, GL_BACK and all other flags
     * that include BACK select the front buffer for drawing. There are
@@ -128,6 +122,19 @@ draw_buffer_enum_to_bitmask(const struct gl_context *ctx, GLenum buffer)
          break;
       }
    }
+
+   return buffer;
+}
+
+/**
+ * Helper routine used by glDrawBuffer and glDrawBuffersARB.
+ * Given a GLenum naming one or more color buffers (such as
+ * GL_FRONT_AND_BACK), return the corresponding bitmask of BUFFER_BIT_* flags.
+ */
+static GLbitfield
+draw_buffer_enum_to_bitmask(const struct gl_context *ctx, GLenum buffer)
+{
+   buffer = back_to_front_if_single_buffered(ctx, buffer);
 
    switch (buffer) {
       case GL_NONE:
@@ -192,20 +199,12 @@ draw_buffer_enum_to_bitmask(const struct gl_context *ctx, GLenum buffer)
 static gl_buffer_index
 read_buffer_enum_to_index(const struct gl_context *ctx, GLenum buffer)
 {
+   buffer = back_to_front_if_single_buffered(ctx, buffer);
+
    switch (buffer) {
       case GL_FRONT:
          return BUFFER_FRONT_LEFT;
       case GL_BACK:
-         if (_mesa_is_gles(ctx)) {
-            /* In draw_buffer_enum_to_bitmask, when GLES contexts draw to
-             * GL_BACK with a single-buffered configuration, we actually end
-             * up drawing to the sole front buffer in our internal
-             * representation.  For consistency, we must read from that
-             * front left buffer too.
-             */
-            if (!ctx->DrawBuffer->Visual.doubleBufferMode)
-               return BUFFER_FRONT_LEFT;
-         }
          return BUFFER_BACK_LEFT;
       case GL_RIGHT:
          return BUFFER_FRONT_RIGHT;