98e59f0a0add4c042cd4f3c7b62b1436065471ee
Jonathan Marek <jonathan@marek.ca>
2019-07-30 02:48:54 -0400

    etnaviv: update headers from rnndb
    Update to etna_viv commit f38ba2d.
    
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/drivers/etnaviv/hw/state_3d.xml.h
+++ src/gallium/drivers/etnaviv/hw/state_3d.xml.h
@@ -8,14 +8,14 @@ http://0x04.net/cgit/index.cgi/rules-ng-ng
 git clone git://0x04.net/rules-ng-ng
 
 The rules-ng-ng source files this header was generated from are:
-- state.xml     (  26087 bytes, from 2019-06-10 23:29:22)
+- state.xml     (  26087 bytes, from 2019-07-31 15:09:17)
 - common.xml    (  35468 bytes, from 2019-06-10 23:29:22)
-- common_3d.xml (  14984 bytes, from 2019-07-01 19:47:22)
+- common_3d.xml (  14988 bytes, from 2019-07-15 12:48:57)
 - state_hi.xml  (  30232 bytes, from 2019-06-10 23:29:22)
 - copyright.xml (   1597 bytes, from 2019-06-10 23:29:22)
 - state_2d.xml  (  51552 bytes, from 2019-06-10 23:29:22)
-- state_3d.xml  (  79878 bytes, from 2019-07-01 19:47:36)
-- state_blt.xml (  13381 bytes, from 2019-07-01 19:46:55)
+- state_3d.xml  (  80519 bytes, from 2019-07-31 15:09:17)
+- state_blt.xml (  13381 bytes, from 2019-07-04 11:45:35)
 - state_vg.xml  (   5975 bytes, from 2019-06-10 23:29:22)
 
 Copyright (C) 2012-2019 by the following authors:
@@ -45,14 +45,6 @@ DEALINGS IN THE SOFTWARE.
 */
 
 
-#define COMPARE_FUNC_NEVER					0x00000000
-#define COMPARE_FUNC_LESS					0x00000001
-#define COMPARE_FUNC_EQUAL					0x00000002
-#define COMPARE_FUNC_LEQUAL					0x00000003
-#define COMPARE_FUNC_GREATER					0x00000004
-#define COMPARE_FUNC_NOTEQUAL					0x00000005
-#define COMPARE_FUNC_GEQUAL					0x00000006
-#define COMPARE_FUNC_ALWAYS					0x00000007
 #define STENCIL_OP_KEEP						0x00000000
 #define STENCIL_OP_ZERO						0x00000001
 #define STENCIL_OP_REPLACE					0x00000002
