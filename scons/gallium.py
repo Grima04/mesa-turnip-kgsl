Grima04/mesa-turnip-kgsl

commit 597bddad47e831474a33cde054c40bbd2cbc0600

scons: Test for random_r()
Suggested-by: Eric Engestrom <eric.engestrom@intel.com>

scons/gallium.py
@@ -368,6 +368,9 @@ def generate(env):
         if check_functions(env, ['strtod_l', 'strtof_l']):
             cppdefines += ['HAVE_STRTOD_L']
 
+        if check_functions(env, 'random_r'):
+            cppdefines += ['HAVE_RANDOM_R']
+
         if check_functions(env, ['timespec_get']):
             cppdefines += ['HAVE_TIMESPEC_GET']
 

commit 412e1b51fee9bc8bbb6ade21687cd02a65a62bb3

scons: Fix random_r check.
Fixes: 597bddad47e8 ("scons: Test for random_r()")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Matt Turner <mattst88@gmail.com>

scons/gallium.py
@@ -369,7 +369,7 @@ def generate(env):
         if check_functions(env, ['strtod_l', 'strtof_l']):
             cppdefines += ['HAVE_STRTOD_L']
 
-        if check_functions(env, 'random_r'):
+        if check_functions(env, ['random_r']):
             cppdefines += ['HAVE_RANDOM_R']
 
         if check_functions(env, ['timespec_get']):