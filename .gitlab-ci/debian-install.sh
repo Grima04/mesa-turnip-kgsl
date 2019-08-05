Grima04/mesa-turnip-kgsl

commit 85dace1c0b7c1839d12171c2c9d63f8e4a002f35

gitlab-ci: remove software-properties-common
Currently we use the python package to manage repositories. At the same
time we also do that by hand - since it's a trivial echo to a file.

Stay consistent, remove the package and manage things manually.

Acked-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

.gitlab-ci/debian-install.sh
@@ -16,12 +16,11 @@ apt-get install -y \
       curl \
       wget \
       unzip \
-      gnupg \
-      software-properties-common
+      gnupg
 
 curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-add-apt-repository "deb https://apt.llvm.org/stretch/ llvm-toolchain-stretch-7 main"
-add-apt-repository "deb https://apt.llvm.org/stretch/ llvm-toolchain-stretch-8 main"
+echo "deb [trusted=yes] https://apt.llvm.org/stretch/ llvm-toolchain-stretch-7 main" >/etc/apt/sources.list.d/llvm7.list
+echo "deb [trusted=yes] https://apt.llvm.org/stretch/ llvm-toolchain-stretch-8 main" >/etc/apt/sources.list.d/llvm8.list
 
 sed -i -e 's/http:\/\/deb/https:\/\/deb/g' /etc/apt/sources.list
 echo 'deb https://deb.debian.org/debian stretch-backports main' >/etc/apt/sources.list.d/backports.list
@@ -46,8 +45,8 @@ apt-get install -y -t stretch-backports \
       clang-8
 
 # Install remaining packages from Debian buster to get newer versions
-add-apt-repository "deb https://deb.debian.org/debian/ buster main"
-add-apt-repository "deb https://deb.debian.org/debian/ buster-updates main"
+echo "deb https://deb.debian.org/debian/ buster main" >/etc/apt/sources.list.d/buster.list
+echo "deb https://deb.debian.org/debian/ buster-updates main" >/etc/apt/sources.list.d/buster-updates.list
 apt-get update
 apt-get install -y \
       bzip2 \

commit c251e2e66257a7023c8e07469bbdee3061d299a0

gitlab-ci: don't remove a package we don't install anymore
Fixes: 85dace1c0b7c1839d121 ("gitlab-ci: remove software-properties-common")
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

.gitlab-ci/debian-install.sh
@@ -219,6 +219,5 @@ apt-get purge -y \
       libtool \
       curl \
       unzip \
-      gnupg \
-      software-properties-common
+      gnupg
 apt-get autoremove -y --purge