d17ff2f7f1864c81c1e00d04baf20f953c6d276a
Eric Anholt <eric@anholt.net>
2019-08-15 15:38:00 -0700

    gallium: Fix big-endian addressing of non-bitmask array formats.
    The formats affected are:
    
    - LA x (16_FLOAT, 32_FLOAT, 32_UINT, 32_SINT)
    - R8G8B8 x (UNORM, SNORM, SRGB, USCALED, SSCALED, UINT, SINT)
    - RG/RGB/RGBA x (64_FLOAT, 32_FLOAT, 16_FLOAT, 32_UNORM, 32_SNORM,
                     32_USCALED, 32_SSCALED, 32_FIXED, 32_UINT, 32_SINT)
    - RGB/RGBA x (16_UNORM, 16_SNORM, 16_USCALED, 16_SSCALED,
                  16_UINT, 16_SINT)
    - RGBx16 x (UNORM, SNORM, FLOAT, UINT, SINT)
    - RGBx32 x (FLOAT, UINT, SINT)
    - RA x (16_FLOAT, 32_FLOAT, 32_UINT, 32_SINT)
    
    The updated st_formats.c unit test checks that the formats affected by
    this change are all array formats in the equivalent Mesa format (if
    any).  Mesa's array format definition is clear: the value stored is an
    array (increasing memory address) of values of the channel's type.
    It's also the only thing that makes sense for the RGB types, or very
    large types like RGBA64_FLOAT (A should not move to the low address
    because the cpu is BE).
    
    Acked-by: Roland Scheidegger <sroland@vmware.com>
    Acked-by: Adam Jackson <ajax@redhat.com>
    Tested-by: Matt Turner <mattst88@gmail.com> (unit tests on BE)
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/mesa/state_tracker/tests/st_format.c
+++ src/mesa/state_tracker/tests/st_format.c
@@ -87,6 +87,29 @@ int main(int argc, char **argv)
          continue;
 
       if (mf != MESA_FORMAT_NONE) {
+         const struct util_format_description *desc = util_format_description(i);
+
+         /* Make sure that gallium and Mesa agree on whether the format is an
+          * array format.
+          */
+         if (desc->nr_channels > 1) {
+            bool mesa_array = (_mesa_get_format_layout(mf) ==
+                               MESA_FORMAT_LAYOUT_ARRAY);
+            bool gallium_array = desc->is_array && !desc->is_bitmask;
+            /* We should probably be checking equality here, but we have some
+             * UINT and SINT types that are array formats in Mesa but not in
+             * gallium.
+             */
+            if (gallium_array && !mesa_array) {
+               fprintf(stderr, "%s is %sarray, %s is %sarray\n",
+                       util_format_short_name(i),
+                       gallium_array ? "" : "not ",
+                       _mesa_get_format_name(mf),
+                       mesa_array ? "" : "not ");
+               return 1;
+            }
+         }
+
          enum pipe_format pf =
             st_mesa_format_to_pipe_format(st, mf);
          if (pf != i) {
