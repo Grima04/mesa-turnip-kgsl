Grima04/mesa-turnip-kgsl

commit ea337c8b7e9e987e7415f56e30f5707976b78e74

radv/gfx10: fix VS input VGPRs with the legacy path
For some reasons, InstanceID is VGPR3 although StepRate0 is set to 1.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/vulkan/radv_shader.c
@@ -765,7 +765,7 @@ static void radv_postprocess_config(const struct radv_physical_device *pdevice,
 			if (info->vs.export_prim_id) {
 				vgpr_comp_cnt = 2;
 			} else if (info->info.vs.needs_instance_id) {
-				vgpr_comp_cnt = 1;
+				vgpr_comp_cnt = pdevice->rad_info.chip_class >= GFX10 ? 3 : 1;
 			} else {
 				vgpr_comp_cnt = 0;
 			}
@@ -837,7 +837,11 @@ static void radv_postprocess_config(const struct radv_physical_device *pdevice,
 
 		if (es_type == MESA_SHADER_VERTEX) {
 			/* VGPR0-3: (VertexID, InstanceID / StepRate0, ...) */
-			es_vgpr_comp_cnt = info->info.vs.needs_instance_id ? 1 : 0;
+			if (info->info.vs.needs_instance_id) {
+				es_vgpr_comp_cnt = pdevice->rad_info.chip_class >= GFX10 ? 3 : 1;
+			} else {
+				es_vgpr_comp_cnt = 0;
+			}
 		} else if (es_type == MESA_SHADER_TESS_EVAL) {
 			es_vgpr_comp_cnt = info->info.uses_prim_id ? 3 : 2;
 		} else {

src/amd/vulkan/radv_nir_to_llvm.c
@@ -852,9 +852,15 @@ declare_vs_input_vgprs(struct radv_shader_context *ctx, struct arg_info *args)
 			}
 		} else {
 			if (ctx->ac.chip_class >= GFX10) {
-				add_arg(args, ARG_VGPR, ctx->ac.i32, NULL); /* user vgpr */
-				add_arg(args, ARG_VGPR, ctx->ac.i32, NULL); /* user vgpr */
-				add_arg(args, ARG_VGPR, ctx->ac.i32, &ctx->abi.instance_id);
+				if (ctx->options->key.vs_common_out.as_ngg) {
+					add_arg(args, ARG_VGPR, ctx->ac.i32, NULL); /* user vgpr */
+					add_arg(args, ARG_VGPR, ctx->ac.i32, NULL); /* user vgpr */
+					add_arg(args, ARG_VGPR, ctx->ac.i32, &ctx->abi.instance_id);
+				} else {
+					add_arg(args, ARG_VGPR, ctx->ac.i32, NULL); /* unused */
+					add_arg(args, ARG_VGPR, ctx->ac.i32, &ctx->vs_prim_id);
+					add_arg(args, ARG_VGPR, ctx->ac.i32, &ctx->abi.instance_id);
+				}
 			} else {
 				add_arg(args, ARG_VGPR, ctx->ac.i32, &ctx->abi.instance_id);
 				add_arg(args, ARG_VGPR, ctx->ac.i32, &ctx->vs_prim_id);

commit 2e763f7c87cefbb0c2c8d692e8e1584f89b4c747

radv: Use correct vgpr_comp_cnt for VS if both prim_id and instance_id are needed.
Should take the max of the 2.

Fixes: ea337c8b7e9 "radv/gfx10: fix VS input VGPRs with the legacy path"
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_shader.c
@@ -809,10 +809,12 @@ static void radv_postprocess_config(const struct radv_physical_device *pdevice,
 			 * If PrimID is disabled. InstanceID / StepRate1 is loaded instead.
 			 * StepRate0 is set to 1. so that VGPR3 doesn't have to be loaded.
 			 */
-			if (info->vs.export_prim_id) {
+			if (info->info.vs.needs_instance_id && pdevice->rad_info.chip_class >= GFX10) {
+				vgpr_comp_cnt = 3;
+			} else if (info->vs.export_prim_id) {
 				vgpr_comp_cnt = 2;
 			} else if (info->info.vs.needs_instance_id) {
-				vgpr_comp_cnt = pdevice->rad_info.chip_class >= GFX10 ? 3 : 1;
+				vgpr_comp_cnt = 1;
 			} else {
 				vgpr_comp_cnt = 0;
 			}