0aa308f4200ad88c9b9ac0fd3e2ad30bde74edb9
Nanley Chery <nanley.g.chery@intel.com>
2019-08-21 10:57:29 -0700

    intel: Fix and use HIZ_CCS write through mode
    Write through to the CCS if the surface is used as a texture and can be
    sampled by the HW with CCS.
    
    Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/intel/isl/isl_emit_depth_stencil.c
+++ src/intel/isl/isl_emit_depth_stencil.c
@@ -192,6 +192,12 @@ isl_genX(emit_depth_stencil_hiz_s)(const struct isl_device *dev, void *batch,
       hiz.SurfaceBaseAddress = info->hiz_address;
       hiz.MOCS = info->mocs;
       hiz.SurfacePitch = info->hiz_surf->row_pitch_B - 1;
+#if GEN_GEN >= 12
+      hiz.HierarchicalDepthBufferWriteThruEnable =
+         isl_surf_supports_hiz_ccs_wt(dev->info, info->depth_surf,
+                                      info->hiz_usage);
+#endif
+
 #if GEN_GEN >= 8
       /* From the SKL PRM Vol2a:
        *
