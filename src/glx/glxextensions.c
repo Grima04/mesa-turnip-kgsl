163fc11f271c8c68e5da2d8704e8696c576b9dca
Adam Jackson <ajax@redhat.com>
2019-08-22 15:15:28 -0400

    glx: Unset the direct_support bit for GLX_EXT_import_context
    GLX_EXT_import_context operates only on indirect contexts, a direct
    context cannot possibly support it. Without this change the extension
    will appear in the combined GLX extension string even if it is missing
    from the server string, indicating a lack of required server support.

--- src/glx/glxextensions.c
+++ src/glx/glxextensions.c
@@ -146,7 +146,7 @@ static const struct extension_info known_glx_extensions[] = {
    { GLX(EXT_create_context_es_profile), VER(0,0), Y, N, N, N },
    { GLX(EXT_fbconfig_packed_float),   VER(0,0), Y, Y, N, N },
    { GLX(EXT_framebuffer_sRGB),        VER(0,0), Y, Y, N, N },
-   { GLX(EXT_import_context),          VER(0,0), Y, Y, N, N },
+   { GLX(EXT_import_context),          VER(0,0), Y, N, N, N },
    { GLX(EXT_texture_from_pixmap),     VER(0,0), Y, N, N, N },
    { GLX(EXT_visual_info),             VER(0,0), Y, Y, N, N },
    { GLX(EXT_visual_rating),           VER(0,0), Y, Y, N, N },
