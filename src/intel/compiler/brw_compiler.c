b418269d7dd576a7c9afd728bf8a883b4da98b30
Ian Romanick <ian.d.romanick@intel.com>
2019-08-26 13:33:06 -0700

    intel/compiler: Request bitfield_reverse lowering on pre-Gen7 hardware
    See the previous commit for the explanation of the Fixes tag.
    
    Hurts 21 shaders in shader-db.  All of the hurt shaders are in Unreal
    Engine 4 tech demos.
    
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Fixes: 7afa26d4e39 ("nir: Add lowering for nir_op_bitfield_reverse.")

--- src/intel/compiler/brw_compiler.c
+++ src/intel/compiler/brw_compiler.c
@@ -187,6 +187,7 @@ brw_compiler_create(void *mem_ctx, const struct gen_device_info *devinfo)
       nir_options->lower_flrp32 = devinfo->gen < 6 || devinfo->gen >= 11;
 
       nir_options->lower_rotate = devinfo->gen < 11;
+      nir_options->lower_bitfield_reverse = devinfo->gen < 7;
 
       nir_options->lower_int64_options = int64_options;
       nir_options->lower_doubles_options = fp64_options;
