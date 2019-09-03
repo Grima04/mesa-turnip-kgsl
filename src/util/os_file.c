Grima04/mesa-turnip-kgsl

commit 955c63d3643f30d7db0c5d16e06a5eda4f62f889

util/os_file: resize buffer to what was actually needed
Fixes: 316964709e21286c2af5 "util: add os_read_file() helper"
Reported-by: Jason Ekstrand <jason@jlekstrand.net>
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/util/os_file.c
@@ -92,6 +92,17 @@ os_read_file(const char *filename)
    if (actually_read > 0)
       offset += actually_read;
 
+   /* Final resize to actual size */
+   len = offset + 1;
+   char *newbuf = realloc(buf, len);
+   if (!newbuf) {
+      free(buf);
+      close(fd);
+      errno = -ENOMEM;
+      return NULL;
+   }
+   buf = newbuf;
+
    buf[offset] = '\0';
 
    return buf;

commit 1667360f7d920a35191dc67b7ee120eef95e8788

util/os_file: fix double-close()
Fixes: 955c63d3643f30d7db0c ("util/os_file: resize buffer to what was actually needed")
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

src/util/os_file.c
@@ -120,7 +120,6 @@ os_read_file(const char *filename)
    char *newbuf = realloc(buf, len);
    if (!newbuf) {
       free(buf);
-      close(fd);
       errno = -ENOMEM;
       return NULL;
    }