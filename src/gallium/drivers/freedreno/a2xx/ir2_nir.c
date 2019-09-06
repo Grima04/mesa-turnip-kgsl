Grima04/mesa-turnip-kgsl

commit 43dbd7d603a6b2cc91e62d3ecbffe57bebdda2d4

freedreno: a2xx: fix order of NIR opts
int_to_float needs to come after bool_to_float, and lower_to_source_mods
needs to come after both, since they don't deal wih source mods.

Signed-off-by: Jonathan Marek <jonathan@marek.ca>
Reviewed-by: Rob Clark <robdclark@chromium.org>

src/gallium/drivers/freedreno/a2xx/ir2_nir.c
@@ -1064,13 +1064,13 @@ ir2_nir_compile(struct ir2_context *ctx, bool binning)
 	/* postprocess */
 	OPT_V(ctx->nir, nir_opt_algebraic_late);
 
-	OPT_V(ctx->nir, nir_lower_to_source_mods, nir_lower_all_source_mods);
 	OPT_V(ctx->nir, nir_copy_prop);
 	OPT_V(ctx->nir, nir_opt_dce);
 	OPT_V(ctx->nir, nir_opt_move_comparisons);
 
-	OPT_V(ctx->nir, nir_lower_bool_to_float);
 	OPT_V(ctx->nir, nir_lower_int_to_float);
+	OPT_V(ctx->nir, nir_lower_bool_to_float);
+	OPT_V(ctx->nir, nir_lower_to_source_mods, nir_lower_all_source_mods);
 
 	/* lower to scalar instructions that can only be scalar on a2xx */
 	OPT_V(ctx->nir, ir2_nir_lower_scalar);

commit 3516a90ab4842a6610dc31514809d490bc4add87

freedreno/a2xx: ir2: fix lowering of instructions after float lowering
Some instructions generated by int/bool float lowering need to be lowered
by opt_algebraic.

Fixes: 43dbd7d6

Signed-off-by: Jonathan Marek <jonathan@marek.ca>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Eric Anholt <eric@anholt.net>

src/gallium/drivers/freedreno/a2xx/ir2_nir.c
@@ -1097,15 +1097,14 @@ ir2_nir_compile(struct ir2_context *ctx, bool binning)
 	if (binning)
 		cleanup_binning(ctx);
 
-	/* postprocess */
-	OPT_V(ctx->nir, nir_opt_algebraic_late);
-
 	OPT_V(ctx->nir, nir_copy_prop);
 	OPT_V(ctx->nir, nir_opt_dce);
 	OPT_V(ctx->nir, nir_opt_move, nir_move_comparisons);
 
 	OPT_V(ctx->nir, nir_lower_int_to_float);
 	OPT_V(ctx->nir, nir_lower_bool_to_float);
+	while(OPT(ctx->nir, nir_opt_algebraic));
+	OPT_V(ctx->nir, nir_opt_algebraic_late);
 	OPT_V(ctx->nir, nir_lower_to_source_mods, nir_lower_all_source_mods);
 
 	OPT_V(ctx->nir, nir_lower_alu_to_scalar, ir2_alu_to_scalar_filter_cb, NULL);