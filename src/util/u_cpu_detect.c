7d5ad8e77e4e085d30debc832d401d1cd78fb8c3
Jan Beich <jbeich@FreeBSD.org>
2019-09-16 11:26:24 +0000

    util: skip NEON detection if built with -mfpu=neon
    Helps platforms where runtime detection isn't implemented.
    
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Signed-off-by: Jan Beich <jbeich@FreeBSD.org>

--- src/util/u_cpu_detect.c
+++ src/util/u_cpu_detect.c
@@ -360,7 +360,9 @@ check_os_arm_support(void)
     * used. Because of this we cannot use PIPE_OS_ANDROID here, but rather
     * have a separate macro that only gets enabled from respective Android.mk.
     */
-#if defined(HAS_ANDROID_CPUFEATURES)
+#if defined(__ARM_NEON) || defined(__ARM_NEON__)
+   util_cpu_caps.has_neon = 1;
+#elif defined(HAS_ANDROID_CPUFEATURES)
    AndroidCpuFamily cpu_family = android_getCpuFamily();
    uint64_t cpu_features = android_getCpuFeatures();
 
