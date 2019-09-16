Grima04/mesa-turnip-kgsl

commit 04dc6074cf7f651b720868e0ba24362b585d1b31

driconfig: add a new engine name/version parameter
Vulkan applications can register with the following structure :

typedef struct VkApplicationInfo {
    VkStructureType    sType;
    const void*        pNext;
    const char*        pApplicationName;
    uint32_t           applicationVersion;
    const char*        pEngineName;
    uint32_t           engineVersion;
    uint32_t           apiVersion;
} VkApplicationInfo;

This enables the Vulkan implementations to apply workarounds based off
matching this description.

Here we add a new parameter for matching the driconfig options with
the following :

    <device driver="anv">
        <application engine_name_match="MyOwnEngine.*" engine_versions="10:12,40:42">
            <option name="blaaah" value="true" />
        </application>
    </device>

v2: switch engine name match to use regexps

v3: Verify that the regexec returns REG_NOMATCH for match failure (Eric)

v4: Add missing bit that went to the following commit (Eric)

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Cc: 19.2 <mesa-stable@lists.freedesktop.org>

src/util/xmlconfig.c
@@ -42,6 +42,7 @@
 #include <errno.h>
 #include <dirent.h>
 #include <fnmatch.h>
+#include <regex.h>
 #include "xmlconfig.h"
 #include "u_process.h"
 
@@ -699,6 +700,8 @@ struct OptConfData {
     int screenNum;
     const char *driverName, *execName;
     const char *kernelDriverName;
+    const char *engineName;
+    uint32_t engineVersion;
     uint32_t ignoringDevice;
     uint32_t ignoringApp;
     uint32_t inDriConf;
@@ -709,12 +712,13 @@ struct OptConfData {
 
 /** \brief Elements in configuration files. */
 enum OptConfElem {
-    OC_APPLICATION = 0, OC_DEVICE, OC_DRICONF, OC_OPTION, OC_COUNT
+    OC_APPLICATION = 0, OC_DEVICE, OC_DRICONF, OC_ENGINE, OC_OPTION, OC_COUNT
 };
 static const XML_Char *OptConfElems[] = {
     [OC_APPLICATION]  = "application",
     [OC_DEVICE] = "device",
     [OC_DRICONF] = "driconf",
+    [OC_ENGINE]  = "engine",
     [OC_OPTION] = "option",
 };
 
@@ -743,6 +747,20 @@ parseDeviceAttr(struct OptConfData *data, const XML_Char **attr)
     }
 }
 
+static bool
+valueInRanges(const driOptionInfo *info, uint32_t value)
+{
+    uint32_t i;
+
+    for (i = 0; i < info->nRanges; i++) {
+       if (info->ranges[i].start._int <= value &&
+           info->ranges[i].end._int >= value)
+           return true;
+    }
+
+    return false;
+}
+
 /** \brief Parse attributes of an application element. */
 static void
 parseAppAttr(struct OptConfData *data, const XML_Char **attr)
@@ -756,6 +774,40 @@ parseAppAttr(struct OptConfData *data, const XML_Char **attr)
     }
     if (exec && strcmp (exec, data->execName))
         data->ignoringApp = data->inApp;
+}
+
+/** \brief Parse attributes of an application element. */
+static void
+parseEngineAttr(struct OptConfData *data, const XML_Char **attr)
+{
+    uint32_t i;
+    const XML_Char *engine_name_match = NULL, *engine_versions = NULL;
+    driOptionInfo version_ranges = {
+       .type = DRI_INT,
+    };
+    for (i = 0; attr[i]; i += 2) {
+        if (!strcmp (attr[i], "name")) /* not needed here */;
+        else if (!strcmp (attr[i], "engine_name_match")) engine_name_match = attr[i+1];
+        else if (!strcmp (attr[i], "engine_versions")) engine_versions = attr[i+1];
+        else XML_WARNING("unknown application attribute: %s.", attr[i]);
+    }
+    if (engine_name_match) {
+       regex_t re;
+
+       if (regcomp (&re, engine_name_match, REG_EXTENDED|REG_NOSUB) != 0) {
+          if (regexec (&re, data->engineName, 0, NULL, 0) == REG_NOMATCH)
+             data->ignoringApp = data->inApp;
+          regfree (&re);
+       } else
+          XML_WARNING ("Invalid engine_name_match=\"%s\".", engine_name_match);
+    }
+    if (engine_versions) {
+       if (parseRanges (&version_ranges, engine_versions) &&
+           !valueInRanges (&version_ranges, data->engineVersion))
+          data->ignoringApp = data->inApp;
+    }
+
+    free(version_ranges.ranges);
 }
 
 /** \brief Parse attributes of an option element. */
@@ -815,10 +867,19 @@ optConfStartElem(void *userData, const XML_Char *name,
         if (!data->inDevice)
             XML_WARNING1 ("<application> should be inside <device>.");
         if (data->inApp)
-            XML_WARNING1 ("nested <application> elements.");
+            XML_WARNING1 ("nested <application> or <engine> elements.");
         data->inApp++;
         if (!data->ignoringDevice && !data->ignoringApp)
             parseAppAttr (data, attr);
+        break;
+      case OC_ENGINE:
+        if (!data->inDevice)
+            XML_WARNING1 ("<engine> should be inside <device>.");
+        if (data->inApp)
+            XML_WARNING1 ("nested <application> or <engine> elements.");
+        data->inApp++;
+        if (!data->ignoringDevice && !data->ignoringApp)
+            parseEngineAttr (data, attr);
         break;
       case OC_OPTION:
         if (!data->inApp)
@@ -849,6 +910,7 @@ optConfEndElem(void *userData, const XML_Char *name)
             data->ignoringDevice = 0;
         break;
       case OC_APPLICATION:
+      case OC_ENGINE:
         if (data->inApp-- == data->ignoringApp)
             data->ignoringApp = 0;
         break;
@@ -996,7 +1058,8 @@ parseConfigDir(struct OptConfData *data, const char *dirname)
 void
 driParseConfigFiles(driOptionCache *cache, const driOptionCache *info,
                     int screenNum, const char *driverName,
-                    const char *kernelDriverName)
+                    const char *kernelDriverName,
+                    const char *engineName, uint32_t engineVersion)
 {
     char *home;
     struct OptConfData userData;
@@ -1007,6 +1070,8 @@ driParseConfigFiles(driOptionCache *cache, const driOptionCache *info,
     userData.screenNum = screenNum;
     userData.driverName = driverName;
     userData.kernelDriverName = kernelDriverName;
+    userData.engineName = engineName ? engineName : "";
+    userData.engineVersion = engineVersion;
     userData.execName = util_get_process_name();
 
     parseConfigDir(&userData, DATADIR "/drirc.d");

src/amd/vulkan/radv_device.c
@@ -567,7 +567,9 @@ static void  radv_init_dri_options(struct radv_instance *instance)
 	driParseOptionInfo(&instance->available_dri_options, radv_dri_options_xml);
 	driParseConfigFiles(&instance->dri_options,
 	                    &instance->available_dri_options,
-	                    0, "radv", NULL);
+	                    0, "radv", NULL,
+	                    instance->engineName,
+	                    instance->engineVersion);
 }
 
 VkResult radv_CreateInstance(

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -99,7 +99,7 @@ pipe_loader_load_options(struct pipe_loader_device *dev)
 
    driParseOptionInfo(&dev->option_info, xml_options);
    driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
-                       dev->driver_name, NULL);
+                       dev->driver_name, NULL, NULL, 0);
 }
 
 char *

src/gallium/targets/d3dadapter9/drm.c
@@ -250,7 +250,8 @@ drm_create_adapter( int fd,
     ctx->base.throttling = ctx->base.throttling_value > 0;
 
     driParseOptionInfo(&defaultInitOptions, __driConfigOptionsNine);
-    driParseConfigFiles(&userInitOptions, &defaultInitOptions, 0, "nine", NULL);
+    driParseConfigFiles(&userInitOptions, &defaultInitOptions, 0,
+                        "nine", NULL, NULL, 0);
     if (driCheckOption(&userInitOptions, "throttle_value", DRI_INT)) {
         throttling_value_user = driQueryOptioni(&userInitOptions, "throttle_value");
         if (throttling_value_user == -1)

src/intel/vulkan/anv_device.c
@@ -780,7 +780,9 @@ VkResult anv_CreateInstance(
 
    driParseOptionInfo(&instance->available_dri_options, anv_dri_options_xml);
    driParseConfigFiles(&instance->dri_options, &instance->available_dri_options,
-                       0, "anv", NULL);
+                       0, "anv", NULL,
+                       instance->app_info.engine_name,
+                       instance->app_info.engine_version);
 
    *pInstance = anv_instance_to_handle(instance);
 

src/loader/loader.c
@@ -185,7 +185,7 @@ static char *loader_get_dri_config_driver(int fd)
 
    driParseOptionInfo(&defaultInitOptions, __driConfigOptionsLoader);
    driParseConfigFiles(&userInitOptions, &defaultInitOptions, 0,
-                       "loader", kernel_driver);
+                       "loader", kernel_driver, NULL, 0);
    if (driCheckOption(&userInitOptions, "dri_driver", DRI_STRING)) {
       char *opt = driQueryOptionstr(&userInitOptions, "dri_driver");
       /* not an empty string */
@@ -206,7 +206,8 @@ static char *loader_get_dri_config_device_id(void)
    char *prime = NULL;
 
    driParseOptionInfo(&defaultInitOptions, __driConfigOptionsLoader);
-   driParseConfigFiles(&userInitOptions, &defaultInitOptions, 0, "loader", NULL);
+   driParseConfigFiles(&userInitOptions, &defaultInitOptions, 0,
+                       "loader", NULL, NULL, 0);
    if (driCheckOption(&userInitOptions, "device_id", DRI_STRING))
       prime = strdup(driQueryOptionstr(&userInitOptions, "device_id"));
    driDestroyOptionCache(&userInitOptions);

src/mesa/drivers/dri/common/dri_util.c
@@ -148,7 +148,8 @@ driCreateNewScreen2(int scrn, int fd,
 
     /* Option parsing before ->InitScreen(), as some options apply there. */
     driParseOptionInfo(&psp->optionInfo, __dri2ConfigOptions);
-    driParseConfigFiles(&psp->optionCache, &psp->optionInfo, psp->myNum, "dri2", NULL);
+    driParseConfigFiles(&psp->optionCache, &psp->optionInfo, psp->myNum,
+                        "dri2", NULL, NULL, 0);
 
     *driver_configs = psp->driver->InitScreen(psp);
     if (*driver_configs == NULL) {

src/mesa/drivers/dri/i915/intel_context.c
@@ -446,7 +446,7 @@ intelInitContext(struct intel_context *intel,
 	  0, sizeof(ctx->TextureFormatSupported));
 
    driParseConfigFiles(&intel->optionCache, &intelScreen->optionCache,
-                       sPriv->myNum, "i915", NULL);
+                       sPriv->myNum, "i915", NULL, NULL, 0);
    intel->maxBatchSize = 4096;
 
    /* Estimate the size of the mappable aperture into the GTT.  There's an

commit 10206ba17b3f685035418b2ab99b10ea261600e1

util/xmlconfig: fix regexp compile failure check
This is embarrasing...

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 04dc6074cf ("driconfig: add a new engine name/version parameter")
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>

src/util/xmlconfig.c
@@ -794,7 +794,7 @@ parseEngineAttr(struct OptConfData *data, const XML_Char **attr)
     if (engine_name_match) {
        regex_t re;
 
-       if (regcomp (&re, engine_name_match, REG_EXTENDED|REG_NOSUB) != 0) {
+       if (regcomp (&re, engine_name_match, REG_EXTENDED|REG_NOSUB) == 0) {
           if (regexec (&re, data->engineName, 0, NULL, 0) == REG_NOMATCH)
              data->ignoringApp = data->inApp;
           regfree (&re);