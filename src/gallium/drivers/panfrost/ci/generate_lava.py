7b01f725dd2984d1e6a994330a9dd012957d8abb
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2019-09-18 16:03:36 +0200

    gitlab-ci: Run dEQP on devices with Panfrost
    Include Panfrost's gitlab.ci.yml file from Mesa's main .gitlab-ci.yml so
    we test on devices with Panfrost.
    
    This uses LAVA to schedule jobs in the devices and will be the base for
    testing Etnaviv, Lima, etc.
    
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>

--- src/gallium/drivers/panfrost/ci/generate_lava.py
+++ src/gallium/drivers/panfrost/ci/generate_lava.py
@@ -9,6 +9,7 @@ parser.add_argument("--base-artifacts-url")
 parser.add_argument("--arch")
 parser.add_argument("--device-type")
 parser.add_argument("--kernel-image-name")
+parser.add_argument("--gpu-version")
 args = parser.parse_args()
 
 env = Environment(loader = FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
@@ -19,5 +20,6 @@ values['base_artifacts_url'] = args.base_artifacts_url
 values['arch'] = args.arch
 values['device_type'] = args.device_type
 values['kernel_image_name'] = args.kernel_image_name
+values['gpu_version'] = args.gpu_version
 
 print(template.render(values))
