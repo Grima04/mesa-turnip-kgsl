anholt/mesa

commit 533fead4236459c3f04700ff130ffaee1503cb69

bin/get-pick-list.sh: tweak the commit sha matching pattern
Currently we match on:
 - any arbitrary length of,
 - any a-z A-Z and 0-9 characters

At the same time, a commit sha consists of lowercase hexadecimal
numbers. Any sha shorter than 8 characters is ambiguous - in some cases
even 11+ are required.

So change the pattern to a-f0-9 and adjust the length to 8-40.

As we're here we could use a single grep, instead of the grep/sed combo.

Cc: mesa-stable@lists.freedesktop.org
Reviewed-by: Juan A. Suarez <jasuarez@igalia.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

bin/get-pick-list.sh
@@ -25,7 +25,7 @@ is_fixes_nomination()
 {
 	fixes=`git show --pretty=medium -s $1 | tr -d "\n" | \
 		sed -e 's/fixes:[[:space:]]*/\nfixes:/Ig' | \
-		grep "fixes:" | sed -e 's/\(fixes:[a-zA-Z0-9]*\).*$/\1/'`
+		grep -Eo 'fixes:[a-f0-9]{8,40}'`
 
 	fixes_count=`echo "$fixes" | wc -l`
 	if [ $fixes_count -eq 0 ] ; then

Grima04/mesa-turnip-kgsl

commit b3c25e6f9953b6c7d196b47c6ba1987f681edf7d

bin/get-pick-list.sh: sha1 commits can be smaller than 8 chars
The script only handles commits with "Fixes: <sha1>" where <sha1> is
equal or great than 8 chars. But <sha1> can be smaller, like 7 chars.

This commit relax the restriction to handle <sha1> 4 or more chars.

Fixes: 533fead4236 ("bin/get-pick-list.sh: tweak the commit sha matching pattern")

Acked-by: Eric Engestrom <eric.engestrom@intel.com>
Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>

bin/get-pick-list.sh
@@ -32,7 +32,7 @@ is_sha_nomination()
 {
 	fixes=`git show --pretty=medium -s $1 | tr -d "\n" | \
 		sed -e 's/'"$2"'/\nfixes:/Ig' | \
-		grep -Eo 'fixes:[a-f0-9]{8,40}'`
+		grep -Eo 'fixes:[a-f0-9]{4,40}'`
 
 	fixes_count=`echo "$fixes" | grep "fixes:" | wc -l`
 	if test $fixes_count -eq 0; then