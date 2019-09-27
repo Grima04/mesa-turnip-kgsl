6965a02e09de2d31e5fa951ffa2ae401b2197fb1
Francisco Jerez <currojerez@riseup.net>
2019-09-27 14:50:00 -0700

    intel/ir: Represent physical edge of unconditional CONTINUE instruction.
    This edge doesn't exist in the original scalar program, but it
    represents a potential control flow path the EU will take in cases
    where control flow isn't uniform across channels of the same SIMD
    thread.
    
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- src/intel/compiler/brw_cfg.cpp
+++ src/intel/compiler/brw_cfg.cpp
@@ -334,6 +334,8 @@ cfg_t::cfg_t(exec_list *instructions)
 	 next = new_block();
 	 if (inst->predicate)
             cur->add_successor(mem_ctx, next, bblock_link_logical);
+         else
+            cur->add_successor(mem_ctx, next, bblock_link_physical);
 
 	 set_next_block(&cur, next, ip);
 	 break;
