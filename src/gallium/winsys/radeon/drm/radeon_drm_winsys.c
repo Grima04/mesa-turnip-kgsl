3b2b83924e3185ba635739667aea849ad6869efb
Marek Olšák <marek.olsak@amd.com>
2019-09-30 15:14:11 -0400

    winsys/radeon: initialize SIMD properties in radeon_info
    This was missed when I added them.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/1839
    Fixes: 0692ae34e93 ("ac: move ac_get_num_physical_sgprs into radeon_info")
    
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

--- src/gallium/winsys/radeon/drm/radeon_drm_winsys.c
+++ src/gallium/winsys/radeon/drm/radeon_drm_winsys.c
@@ -589,6 +589,9 @@ static bool do_winsys_init(struct radeon_drm_winsys *ws)
     ws->info.max_alignment = 1024*1024;
     ws->info.has_graphics = true;
     ws->info.cpdma_prefetch_writes_memory = true;
+    ws->info.max_wave64_per_simd = 10;
+    ws->info.num_physical_sgprs_per_simd = 512;
+    ws->info.num_physical_wave64_vgprs_per_simd = 256;
 
     ws->check_vm = strstr(debug_get_option("R600_DEBUG", ""), "check_vm") != NULL ||
                    strstr(debug_get_option("AMD_DEBUG", ""), "check_vm") != NULL;
