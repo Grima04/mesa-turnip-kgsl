4fee7b30c0ecc56d7659ecad1d8b140ab253f0db
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2019-10-15 21:36:48 -0400

    panfrost: Disable frame throttling
    The new frame throttling implemention interacts unfortunately with
    pipelining, leading to fence fds leaking like crazy and ultimately apps
    crashing quickly.
    
    With this patch, apps still crash but not as quickly. We need to either
    figure out the real cause or revert the core changes.
    
    Nevertheless, we don't want frame throttling in the first place, so.
    
    Fixes: a65e29ccb26 ("gallium: simplify throttle implementation")
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

--- src/gallium/drivers/panfrost/pan_screen.c
+++ src/gallium/drivers/panfrost/pan_screen.c
@@ -101,6 +101,9 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
         case PIPE_CAP_MAX_RENDER_TARGETS:
                 return is_deqp ? 4 : 1;
 
+        /* Throttling frames breaks pipelining */
+        case PIPE_CAP_THROTTLE:
+                return 0;
 
         case PIPE_CAP_OCCLUSION_QUERY:
                 return 1;
