flightlessmango/mesa

iris/gen12: Write GFX_AUX_TABLE base address register
Rework:
 * Move last_aux_map_state to iris_batch. (Nanley, Ken)

Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

src/gallium/drivers/iris/iris_batch.h
@@ -126,6 +126,8 @@ struct iris_batch {
 
    /** Have we emitted any draw calls to this batch? */
    bool contains_draw;
+
+   uint64_t last_aux_map_state;
 };
 
 void iris_init_batch(struct iris_batch *batch,

Grima04/mesa-turnip-kgsl

Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/gallium/drivers/iris/iris_batch.h
@@ -126,6 +126,8 @@ struct iris_batch {
 
    /** Have we emitted any draw calls to this batch? */
    bool contains_draw;
+
+   uint32_t last_aux_map_state;
 };
 
 void iris_init_batch(struct iris_batch *batch,