ef48d57b77daf6e17f55f90fa1fcede1b02018a9
Erik Faye-Lund <erik.faye-lund@collabora.com>
2019-11-01 14:13:15 +0100

    zink: add X32_S8X24 format
    Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Reviewed-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9244>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -955,6 +955,9 @@ zink_get_format(struct zink_screen *screen, enum pipe_format format)
 {
    VkFormat ret = zink_pipe_format_to_vk_format(emulate_x8(format));
 
+   if (format == PIPE_FORMAT_X32_S8X24_UINT)
+      return VK_FORMAT_D32_SFLOAT_S8_UINT;
+
    if (format == PIPE_FORMAT_X24S8_UINT)
       /* valid when using aspects to extract stencil,
        * fails format test because it's emulated */
