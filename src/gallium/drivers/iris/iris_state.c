Grima04/mesa-turnip-kgsl

commit 2ca0d913ea82c8025e801b33fc4a7800d4f7a9a7

iris: Fix framebuffer layer count
cso_fb->layers is only valid for no-attachment framebuffers.  Use the
helper function to get the real value, then stash it so we don't have
to call the helper function on the old value for comparison, or at draw
time for Force Zero RTA Index setting.

This fixes Force Zero RTA Index being set even when attempting layered
rendering.

src/gallium/drivers/iris/iris_state.c
@@ -2088,6 +2088,7 @@ iris_set_framebuffer_state(struct pipe_context *ctx,
    struct iris_resource *stencil_res;
 
    unsigned samples = util_framebuffer_get_num_samples(state);
+   unsigned layers = util_framebuffer_get_num_layers(state);
 
    if (cso->samples != samples) {
       ice->state.dirty |= IRIS_DIRTY_MULTISAMPLE;
@@ -2097,7 +2098,7 @@ iris_set_framebuffer_state(struct pipe_context *ctx,
       ice->state.dirty |= IRIS_DIRTY_BLEND_STATE;
    }
 
-   if ((cso->layers == 0) != (state->layers == 0)) {
+   if ((cso->layers == 0) != (layers == 0)) {
       ice->state.dirty |= IRIS_DIRTY_CLIP;
    }
 
@@ -2107,6 +2108,7 @@ iris_set_framebuffer_state(struct pipe_context *ctx,
 
    util_copy_framebuffer_state(cso, state);
    cso->samples = samples;
+   cso->layers = layers;
 
    struct iris_depth_buffer_state *cso_z = &ice->state.genx->depth_buffer;
 

commit fc7b7480867d6049ca12f87d9b6ab0d9ad55d59f

iris: Fix "Force Zero RTA Index Enable" setting again
In 2ca0d913ea8, we began updating cso_fb->layers to the actual layer
count, rather than 0.  This fixed cases where we were setting "Force
Zero RTA Index Enable" even when doing layered rendering.  Sadly, it
also broke the check entirely: cso_fb->layers is now 1 for non-layered
cases, but the Force Zero RTA Index check was still comparing for 0.

Fixes: 2ca0d913ea8 ("iris: Fix framebuffer layer count")

src/gallium/drivers/iris/iris_state.c
@@ -5509,7 +5509,7 @@ iris_upload_dirty_render_state(struct iris_context *ice,
              BRW_BARYCENTRIC_NONPERSPECTIVE_BITS)
             cl.NonPerspectiveBarycentricEnable = true;
 
-         cl.ForceZeroRTAIndexEnable = cso_fb->layers == 0;
+         cl.ForceZeroRTAIndexEnable = cso_fb->layers <= 1;
          cl.MaximumVPIndex = ice->state.num_viewports - 1;
       }
       iris_emit_merge(batch, cso_rast->clip, dynamic_clip,