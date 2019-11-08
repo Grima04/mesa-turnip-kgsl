084431ce45b3421ef224a16652559713151b122a
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2019-11-08 16:04:47 +0000

    gallium/swr: Enable some ARB_gpu_shader5 extensions Enable / add to features.txt: - Enhanced textureGather. - Geometry shader instancing. - Geometry shader multiple streams.
    Reviewed-by: Jan Zielinski <jan.zielinski@intel.com>

--- src/gallium/drivers/swr/swr_screen.cpp
+++ src/gallium/drivers/swr/swr_screen.cpp
@@ -274,6 +274,7 @@ swr_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_DOUBLES:
    case PIPE_CAP_TEXTURE_QUERY_LOD:
    case PIPE_CAP_COPY_BETWEEN_COMPRESSED_AND_PLAIN_FORMATS:
+   case PIPE_CAP_TGSI_TG4_COMPONENT_IN_SWIZZLE:
       return 1;
 
    /* MSAA support
