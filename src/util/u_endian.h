Grima04/mesa-turnip-kgsl

commit 9020f519d2434be33edd59b9205d6647db4472bb

util/u_endian: Add error checks
As suggested by Eric Engestrom and Michel Dänzer.

src/util/u_endian.h
@@ -80,4 +80,10 @@
 
 #endif
 
+#if !defined(UTIL_ARCH_LITTLE_ENDIAN) || !defined(UTIL_ARCH_BIG_ENDIAN)
+# error "UTIL_ARCH_LITTLE_ENDIAN and/or UTIL_ARCH_BIG_ENDIAN were unset."
+#elif UTIL_ARCH_LITTLE_ENDIAN == UTIL_ARCH_BIG_ENDIAN
+# error "UTIL_ARCH_LITTLE_ENDIAN and UTIL_ARCH_BIG_ENDIAN must not both be 1 or 0."
+#endif
+
 #endif

commit b12911c88e89c693a526db74f9399ee72dff77e7

util/android: fix android build errors
Fixes: 9020f519 ("util/u_endian: Add error checks")
Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2078
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Kristian H. Kristensen <hoegsberg@google.com>

src/util/u_endian.h
@@ -73,7 +73,7 @@
 # define UTIL_ARCH_BIG_ENDIAN 1
 #endif
 
-#elif defined(_WIN32)
+#elif defined(_WIN32) || defined(ANDROID)
 
 #define UTIL_ARCH_LITTLE_ENDIAN 1
 #define UTIL_ARCH_BIG_ENDIAN 0