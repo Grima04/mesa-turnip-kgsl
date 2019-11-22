59b489f44be8ae64a701568f2ae5247933e94fe7
Eric Anholt <eric@anholt.net>
2019-11-22 15:16:27 -0800

    ci: Use a tag from the parallel-deqp-runner repo.
    If the repo continues development, we don't want to accidentally pick
    up potentially breaking changes on our next container rebuild.
    
    Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>

--- .gitlab-ci/build-cts-runner.sh
+++ .gitlab-ci/build-cts-runner.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-git clone https://gitlab.freedesktop.org/mesa/parallel-deqp-runner.git --depth 1
+git clone https://gitlab.freedesktop.org/mesa/parallel-deqp-runner.git --depth 1 -b mesa-ci-2019-11-22
 cd parallel-deqp-runner
 meson build/
 ninja -C build -j4 install
