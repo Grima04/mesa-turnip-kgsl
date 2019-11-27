98d7125b36bfde75f35310a81eb5b8a632d961f4
Jonathan Marek <jonathan@marek.ca>
2019-11-27 10:45:41 -0500

    freedreno/perfcntrs/fdperf: add missing a20x compatible
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>
    Reviewed-by: Rob Clark <robdclark@chromium.org>

--- src/freedreno/perfcntrs/fdperf.c
+++ src/freedreno/perfcntrs/fdperf.c
@@ -282,6 +282,7 @@ find_device_fn(const char *fpath, const struct stat *sb, int typeflag, struct FT
 		char *str = readfile(fpath, &sz);
 		if ((strcmp(str, "qcom,adreno-3xx") == 0) ||
 				(strcmp(str, "qcom,kgsl-3d0") == 0) ||
+				(strstr(str, "amd,imageon") == str) ||
 				(strstr(str, "qcom,adreno") == str)) {
 			int dlen = strlen(fpath) - strlen("/compatible");
 			dev.dtnode = malloc(dlen + 1);
