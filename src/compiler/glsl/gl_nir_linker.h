0e60ea1d67b2b1398be44cc8ae2ad069bc5980bd
Timothy Arceri <tarceri@itsqueeze.com>
2019-12-11 16:12:54 +1100

    glsl: add gl_nir_link_check_atomic_counter_resources()
    This is pretty much a copy of link_check_atomic_counter_resources()
    updated to work with the NIR linker.
    
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>

--- src/compiler/glsl/gl_nir_linker.h
+++ src/compiler/glsl/gl_nir_linker.h
@@ -53,6 +53,9 @@ void nir_build_program_resource_list(struct gl_context *ctx,
 void gl_nir_link_assign_atomic_counter_resources(struct gl_context *ctx,
                                                  struct gl_shader_program *prog);
 
+void gl_nir_link_check_atomic_counter_resources(struct gl_context *ctx,
+                                                struct gl_shader_program *prog);
+
 void gl_nir_link_assign_xfb_resources(struct gl_context *ctx,
                                       struct gl_shader_program *prog);
 
