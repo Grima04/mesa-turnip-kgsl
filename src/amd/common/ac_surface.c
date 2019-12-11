e197fb1c2fccf4719630d91a7c7f76308d88132b
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2019-12-11 16:04:58 +0100

    amd/common: Fix tcCompatible degradation on Stoney.
    addrlib sometimes returns smaller sizes for tcCompat as it does
    not seem to take into account the depth+stencil matching config
    gymnastics with tcCompat.
    
    This fixes
    dEQP-VK.pipeline.render_to_image.core.2d_array.huge.height.r8g8b8a8_unorm_d32_sfloat_s8_uint
    
    CC: <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3054>

--- src/amd/common/ac_surface.c
+++ src/amd/common/ac_surface.c
@@ -344,7 +344,7 @@ static int gfx6_compute_level(ADDR_HANDLE addrlib,
 	    surf_level->mode == RADEON_SURF_MODE_2D &&
 	    level == 0 &&
 	    !(surf->flags & RADEON_SURF_NO_HTILE)) {
-		AddrHtileIn->flags.tcCompatible = AddrSurfInfoIn->flags.tcCompatible;
+		AddrHtileIn->flags.tcCompatible = AddrSurfInfoOut->tcCompatible;
 		AddrHtileIn->pitch = AddrSurfInfoOut->pitch;
 		AddrHtileIn->height = AddrSurfInfoOut->height;
 		AddrHtileIn->numSlices = AddrSurfInfoOut->depth;
