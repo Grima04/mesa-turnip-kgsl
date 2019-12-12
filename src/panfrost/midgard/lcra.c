Grima04/mesa-turnip-kgsl

commit 12e393bacf02e45bded02375aabc8329d42bb085

panfrost: add lcra_free() to free lcra state
Signed-off-by: Urja Rannikko <urjaman@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/panfrost/midgard/lcra.c
@@ -67,6 +67,22 @@ lcra_alloc_equations(
 }
 
 void
+lcra_free(struct lcra_state *l)
+{
+        free(l->alignment);
+        free(l->linear);
+        free(l->modulus);
+        free(l->class);
+        free(l->class_start);
+        free(l->class_disjoint);
+        free(l->class_size);
+        free(l->spill_cost);
+        free(l->solutions);
+
+        free(l);
+}
+
+void
 lcra_set_alignment(struct lcra_state *l, unsigned node, unsigned align_log2)
 {
         l->alignment[node] = align_log2 + 1;

src/panfrost/midgard/lcra.h
@@ -85,6 +85,8 @@ lcra_alloc_equations(
                 unsigned min_alignment, unsigned max_alignment,
                 unsigned bound, unsigned class_count);
 
+void
+lcra_free(struct lcra_state *l);
 
 void
 lcra_set_disjoint_class(struct lcra_state *l, unsigned c1, unsigned c2);

src/panfrost/midgard/midgard_schedule.c
@@ -1427,7 +1427,11 @@ schedule_program(compiler_context *ctx)
                 mir_squeeze_index(ctx);
                 mir_invalidate_liveness(ctx);
 
-                l = NULL;
+                if (l) {
+                        lcra_free(l);
+                        l = NULL;
+                }
+
                 l = allocate_registers(ctx, &spilled);
         } while(spilled && ((iter_count--) > 0));
 
@@ -1442,4 +1446,6 @@ schedule_program(compiler_context *ctx)
         ctx->tls_size = spill_count * 16;
 
         install_registers(ctx, l);
+
+        lcra_free(l);
 }

commit 4308d752811310689bbe71cba35b4357fa7efb0f

pan/midgard: Don't try to free NULL in LCRA
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Fixes: 12e393bacf0 ("panfrost: add lcra_free() to free lcra state")

src/panfrost/midgard/lcra.c
@@ -69,6 +69,9 @@ lcra_alloc_equations(
 void
 lcra_free(struct lcra_state *l)
 {
+        if (!l)
+                return;
+
         free(l->alignment);
         free(l->linear);
         free(l->modulus);