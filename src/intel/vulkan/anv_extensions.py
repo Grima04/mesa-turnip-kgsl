a649bbffee579b306a2d12f252d0f3230b2402b5
Iván Briano <ivan.briano@intel.com>
2019-12-12 16:09:00 -0800

    anv: Export VK_KHR_buffer_device_address only when really supported
    Fixes: 1b6991ba1d8 ("anv: Implement VK_KHR_buffer_device_address")
    
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3071>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3071>

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -67,7 +67,7 @@ EXTENSIONS = [
     Extension('VK_KHR_8bit_storage',                      1, 'device->info.gen >= 8'),
     Extension('VK_KHR_16bit_storage',                     1, 'device->info.gen >= 8'),
     Extension('VK_KHR_bind_memory2',                      1, True),
-    Extension('VK_KHR_buffer_device_address',             1, True),
+    Extension('VK_KHR_buffer_device_address',             1, 'device->has_a64_buffer_access'),
     Extension('VK_KHR_create_renderpass2',                1, True),
     Extension('VK_KHR_dedicated_allocation',              1, True),
     Extension('VK_KHR_depth_stencil_resolve',             1, True),
