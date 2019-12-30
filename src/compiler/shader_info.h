7558b5da13685f701cbe7a9ca804e95e01e38e63
Eric Anholt <eric@anholt.net>
2019-12-30 12:01:25 -0800

    compiler: Add a note about how num_ssbos works in the program info.
    These numbers are always confusing, and it's particularly so for this
    field where it has a different meaning in different info structs.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3240>

--- src/compiler/shader_info.h
+++ src/compiler/shader_info.h
@@ -104,7 +104,11 @@ typedef struct shader_info {
    uint8_t num_ubos;
    /* Number of atomic buffers used by this shader */
    uint8_t num_abos;
-   /* Number of shader storage buffers used by this shader */
+   /* Number of shader storage buffers (max .driver_location + 1) used by this
+    * shader.  In the case of nir_lower_atomics_to_ssbo being used, this will
+    * be the number of actual SSBOs in gl_program->info, and the lowered SSBOs
+    * and atomic counters in nir_shader->info.
+    */
    uint8_t num_ssbos;
    /* Number of images used by this shader */
    uint8_t num_images;
