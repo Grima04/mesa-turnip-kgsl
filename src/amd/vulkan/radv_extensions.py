4e3c81517bafe73015e4af4bdce0eae0cab7751c
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2019-12-30 15:27:21 +0100

    radv: Disable VK_EXT_sample_locations on GFX10.
    Workaround for https://gitlab.freedesktop.org/mesa/mesa/issues/2163
    
    CC: <mesa-stable@lists.freedesktop.org>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3236>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3236>

--- src/amd/vulkan/radv_extensions.py
+++ src/amd/vulkan/radv_extensions.py
@@ -133,7 +133,8 @@ EXTENSIONS = [
     Extension('VK_EXT_pipeline_creation_feedback',        1, True),
     Extension('VK_EXT_post_depth_coverage',               1, 'device->rad_info.chip_class >= GFX10'),
     Extension('VK_EXT_queue_family_foreign',              1, True),
-    Extension('VK_EXT_sample_locations',                  1, True),
+    # Disable sample locations on GFX10 until the CTS failures have been resolved.
+    Extension('VK_EXT_sample_locations',                  1, 'device->rad_info.chip_class < GFX10'),
     Extension('VK_EXT_sampler_filter_minmax',             1, 'device->rad_info.chip_class >= GFX7'),
     Extension('VK_EXT_scalar_block_layout',               1, 'device->rad_info.chip_class >= GFX7'),
     Extension('VK_EXT_shader_demote_to_helper_invocation',1, 'device->use_aco'),
