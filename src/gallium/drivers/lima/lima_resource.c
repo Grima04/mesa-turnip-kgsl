Grima04/mesa-turnip-kgsl

commit 01a451b04d2da9981ea8f16f9d0d6a49fcc6c2df

lima: handle DRM_FORMAT_MOD_INVALID in resource_from_handle()
Assume that resource is tiled if we get DRM_FORMAT_MOD_INVALID
in resource_from_handle() and we don't have RO.

Fixes: 8c12f4e5f24f ("lima: enable tiling")
Reviewed-by: Qiang Yu <yuq825@gmail.com>
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

src/gallium/drivers/lima/lima_resource.c
@@ -329,9 +329,13 @@ lima_resource_from_handle(struct pipe_screen *pscreen,
    case DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED:
       res->tiled = true;
       break;
+   case DRM_FORMAT_MOD_INVALID:
+      res->tiled = screen->ro == NULL;
+      break;
    default:
       fprintf(stderr, "Attempted to import unsupported modifier 0x%llx\n",
                   (long long)handle->modifier);
+      goto err_out;
    }
 
    return pres;

commit a533d1d4c607543fc1e3ca427eba50e9602f6fe2

lima: use linear layout for shared buffers if modifier is not specified
Use linear layout for shared buffers if modifier is not specified
and use linear layout when importing buffers with invalid modifier.

Fixes: 01a451b04d2d ("lima: handle DRM_FORMAT_MOD_INVALID in resource_from_handle()")
Reviewed-by: Eric Anholt <eric@anholt.net>
Reviewed-by: Erico Nunes <nunes.erico@gmail.com>
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

src/gallium/drivers/lima/lima_resource.c
@@ -193,6 +193,10 @@ _lima_resource_create_with_modifiers(struct pipe_screen *pscreen,
    if (templat->bind & (PIPE_BIND_LINEAR | PIPE_BIND_SCANOUT))
       should_tile = false;
 
+   /* If there's no user modifiers and buffer is shared we use linear */
+   if (!has_user_modifiers && (templat->bind & PIPE_BIND_SHARED))
+      should_tile = false;
+
    if (drm_find_modifier(DRM_FORMAT_MOD_LINEAR, modifiers, count))
       should_tile = false;
 
@@ -330,7 +334,10 @@ lima_resource_from_handle(struct pipe_screen *pscreen,
       res->tiled = true;
       break;
    case DRM_FORMAT_MOD_INVALID:
-      res->tiled = screen->ro == NULL;
+      /* Modifier wasn't specified and it's shared buffer. We create these
+       * as linear, so disable tiling.
+       */
+      res->tiled = false;
       break;
    default:
       fprintf(stderr, "Attempted to import unsupported modifier 0x%llx\n",