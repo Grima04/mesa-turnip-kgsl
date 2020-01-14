b046f550866790d3cfbc64add060f413f06d8f03
Rhys Perry <pendingchaos02@gmail.com>
2020-01-14 11:42:11 +0000

    aco: use nir_move_copies
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/2421>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/2421>

--- src/amd/compiler/aco_instruction_selection_setup.cpp
+++ src/amd/compiler/aco_instruction_selection_setup.cpp
@@ -888,7 +888,8 @@ setup_nir(isel_context *ctx, nir_shader *nir)
    nir_lower_load_const_to_scalar(nir);
    nir_opt_shrink_load(nir);
    nir_move_options move_opts = (nir_move_options)(
-      nir_move_const_undef | nir_move_load_ubo | nir_move_load_input | nir_move_comparisons);
+      nir_move_const_undef | nir_move_load_ubo | nir_move_load_input |
+      nir_move_comparisons | nir_move_copies);
    nir_opt_sink(nir, move_opts);
    nir_opt_move(nir, move_opts);
    nir_convert_to_lcssa(nir, true, false);
