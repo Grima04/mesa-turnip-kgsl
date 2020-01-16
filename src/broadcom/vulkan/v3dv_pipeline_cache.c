6cac991d3f9a93686da85256a0d407d80f2b1e4c
Alejandro Piñeiro <apinheiro@igalia.com>
2020-01-16 13:22:57 +0100

    v3dv: stubs for Create/DestroyPipelineCache
    Good enough to avoid vkrunner crashing.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6766>

---
+++ src/broadcom/vulkan/v3dv_pipeline_cache.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2019 Raspberry Pi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "v3dv_private.h"
+
+VkResult
+v3dv_CreatePipelineCache(VkDevice _device,
+    const VkPipelineCacheCreateInfo *pCreateInfo,
+    const VkAllocationCallbacks *pAllocator,
+    VkPipelineCache *pPipelineCache)
+{
+   /* FIXME: stub */
+   return VK_SUCCESS;
+}
+
+void
+v3dv_DestroyPipelineCache(VkDevice _device,
+                          VkPipelineCache _cache,
+                          const VkAllocationCallbacks *pAllocator)
+{
+   /* FIXME: stub */
+}
