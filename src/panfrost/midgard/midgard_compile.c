7d52b3a18bb4263608faf9cd44cfd6d9b1aee474
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-01-23 15:37:35 -0500

    pan/midgard: Remove pack_color define
    Unused at the moment.
    
    ../src/panfrost/midgard/midgard_compile.c:124:29: warning: ‘m_pack_colour’ defined but not used [-Wunused-function]
      124 |  static midgard_instruction m_##name(unsigned ssa, unsigned address) { \
          |                             ^~
    ../src/panfrost/midgard/midgard_compile.c:145:22: note: in expansion of macro ‘M_LOAD_STORE’
      145 | #define M_LOAD(name) M_LOAD_STORE(name, false)
          |                      ^~~~~~~~~~~~
    ../src/panfrost/midgard/midgard_compile.c:213:1: note: in expansion of macro ‘M_LOAD’
      213 | M_LOAD(pack_colour);
          | ^~~~~~
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3543>

--- src/panfrost/midgard/midgard_compile.c
+++ src/panfrost/midgard/midgard_compile.c
@@ -210,7 +210,6 @@ M_LOAD(ld_color_buffer_32u);
 M_STORE(st_vary_32);
 M_LOAD(ld_cubemap_coords);
 M_LOAD(ld_compute_id);
-M_LOAD(pack_colour);
 
 static midgard_instruction
 v_branch(bool conditional, bool invert)
