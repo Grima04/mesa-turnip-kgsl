Grima04/mesa-turnip-kgsl

commit ef5266ebd50e7fa65c56bdb623e12ca8c233b470

util/os_socket: Add socket related functions.
v3:
 - Add os_socket.c/h into Makefile.sources (Lionel)
 - Add empty non-linux implementation to public functions.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

src/util/os_socket.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2019 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ *
+ * Socket operations helpers
+ */
+
+#ifndef _OS_SOCKET_H_
+#define _OS_SOCKET_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int os_socket_accept(int s);
+
+int os_socket_listen_abstract(const char *path, int count);
+
+ssize_t os_socket_recv(int socket, void *buffer, size_t length, int flags);
+ssize_t os_socket_send(int socket, const void *buffer, size_t length, int flags);
+
+void os_socket_block(int s, bool block);
+void os_socket_close(int s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OS_SOCKET_H_ */

src/util/Makefile.sources
@@ -57,6 +57,8 @@ MESA_UTIL_FILES := \
 	os_time.h \
 	os_file.c \
 	os_file.h \
+	os_socket.c \
+	os_socket.h \
 	os_misc.c \
 	os_misc.h \
 	u_process.c \

src/util/meson.build
@@ -62,6 +62,8 @@ files_mesa_util = files(
   'os_file.c',
   'os_misc.c',
   'os_misc.h',
+  'os_socket.c',
+  'os_socket.h',
   'u_process.c',
   'u_process.h',
   'sha1/sha1.c',

src/util/os_socket.c
@@ -1,0 +1,122 @@
+/*
+ * Copyright 2019 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <errno.h>
+
+#include "os_socket.h"
+
+#if defined(__linux__)
+
+#include <fcntl.h>
+#include <poll.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+int
+os_socket_listen_abstract(const char *path, int count)
+{
+   int s = socket(AF_UNIX, SOCK_STREAM, 0);
+   if (s < 0)
+      return -1;
+
+   struct sockaddr_un addr;
+   memset(&addr, 0, sizeof(addr));
+   addr.sun_family = AF_UNIX;
+   strncpy(addr.sun_path + 1, path, sizeof(addr.sun_path) - 2);
+
+   /* Create an abstract socket */
+   int ret = bind(s, (struct sockaddr*)&addr,
+                  offsetof(struct sockaddr_un, sun_path) +
+                  strlen(path) + 1);
+   if (ret < 0)
+      return -1;
+
+   listen(s, count);
+
+   return s;
+}
+
+int
+os_socket_accept(int s)
+{
+   return accept(s, NULL, NULL);
+}
+
+ssize_t
+os_socket_recv(int socket, void *buffer, size_t length, int flags)
+{
+   return recv(socket, buffer, length, flags);
+}
+
+ssize_t
+os_socket_send(int socket, const void *buffer, size_t length, int flags)
+{
+   return send(socket, buffer, length, flags);
+}
+
+void
+os_socket_block(int s, bool block)
+{
+   int old = fcntl(s, F_GETFL, 0);
+   if (old == -1)
+      return;
+
+   /* TODO obey block */
+   if (block)
+      fcntl(s, F_SETFL, old & ~O_NONBLOCK);
+   else
+      fcntl(s, F_SETFL, old | O_NONBLOCK);
+}
+
+void
+os_socket_close(int s)
+{
+   close(s);
+}
+
+#else
+
+int
+os_socket_listen_abstract(const char *path, int count)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+int
+os_socket_accept(int s)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+ssize_t
+os_socket_recv(int socket, void *buffer, size_t length, int flags)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+ssize_t
+os_socket_send(int socket, const void *buffer, size_t length, int flags)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+void
+os_socket_block(int s, bool block)
+{
+}
+
+void
+os_socket_close(int s)
+{
+}
+
+#endif

commit e62c3cf350a8b169e6401d5f1e1f17388cdc4b77

util/os_socket: Include unistd.h to fix build error
Fixes

In file included from ../src/util/os_socket.c:8:
../src/util/os_socket.h:26:1: error: unknown type name ‘ssize_t’; did you mean ‘size_t’?
 ssize_t os_socket_recv(int socket, void *buffer, size_t length, int flags);

seen with gcc version 8.3.0 (Buildroot 2019.11) and uClibc 1.0.32.

Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Fixes: ef5266ebd50e7fa65c56 ("util/os_socket: Add socket related functions.")
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3659>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3659>

src/util/os_socket.h
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 #ifdef _MSC_VER
 #include <BaseTsd.h>
 typedef SSIZE_T ssize_t;