26ab38f1440625b85adc235140574901e60562a1
Kristian H. Kristensen <hoegsberg@google.com>
2020-02-06 14:49:57 -0800

    ci: Drop turnip opt-in option
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3742>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3742>

--- .gitlab-ci/meson-build.sh
+++ .gitlab-ci/meson-build.sh
@@ -54,7 +54,6 @@ meson _build --native-file=native.file \
       ${GALLIUM_ST} \
       -D gallium-drivers=${GALLIUM_DRIVERS:-[]} \
       -D vulkan-drivers=${VULKAN_DRIVERS:-[]} \
-      -D I-love-half-baked-turnips=true \
       ${EXTRA_OPTION}
 cd _build
 meson configure
