21a2ce71b132739b001442a4f9829de345311c35
Qiang Yu <yuq825@gmail.com>
2020-02-08 19:24:34 +0800

    lima: move flush code to lima_submit.c
    Just code move, no content change.
    
    Reviewed-by: Vasily Khoruzhick <anarsoul@gmail.com>
    Signed-off-by: Qiang Yu <yuq825@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3755>

--- src/gallium/drivers/lima/lima_submit.h
+++ src/gallium/drivers/lima/lima_submit.h
@@ -33,8 +33,6 @@ struct lima_bo;
 
 bool lima_submit_add_bo(struct lima_submit *submit, int pipe,
                         struct lima_bo *bo, uint32_t flags);
-bool lima_submit_start(struct lima_submit *submit, int pipe, void *frame, uint32_t size);
-bool lima_submit_wait(struct lima_submit *submit, int pipe, uint64_t timeout_ns);
 bool lima_submit_has_bo(struct lima_submit *submit, struct lima_bo *bo, bool all);
 
 bool lima_submit_init(struct lima_context *ctx);
