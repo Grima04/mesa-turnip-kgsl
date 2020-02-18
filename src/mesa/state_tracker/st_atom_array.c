Grima04/mesa-turnip-kgsl

commit 11db8e0e00a72884ba9fda953b549dd65119dc73

st/mesa: optimize st_update_array with ALWAYSINLINE
The time spent in st_update_array is reduced by 5-10%.

Reviewed-by: Mathias Fröhlich <mathias.froehlich@web.de>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3766>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3766>

src/mesa/state_tracker/st_atom_array.c
@@ -364,7 +364,10 @@ static void init_velement_lowered(const struct st_vertex_program *vp,
    }
 }
 
-void
+/* ALWAYS_INLINE helps the compiler realize that most of the parameters are
+ * on the stack.
+ */
+void ALWAYS_INLINE
 st_setup_arrays(struct st_context *st,
                 const struct st_vertex_program *vp,
                 const struct st_common_variant *vp_variant,
@@ -427,7 +430,10 @@ st_setup_arrays(struct st_context *st,
    *has_user_vertex_buffers = uses_user_vertex_buffers;
 }
 
-void
+/* ALWAYS_INLINE helps the compiler realize that most of the parameters are
+ * on the stack.
+ */
+void ALWAYS_INLINE
 st_setup_current(struct st_context *st,
                  const struct st_vertex_program *vp,
                  const struct st_common_variant *vp_variant,

commit 2491a2ddeb53f3d44b4633f967bfa5b023946bce

st/mesa: try to fix MSVC build failure due to ALWAYS_INLINE
Fixes: 11db8e0e00a72884ba9f ("st/mesa: optimize st_update_array with ALWAYSINLINE")
Tested-by: Eric Engestrom <eric@engestrom.ch>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3861>

src/mesa/state_tracker/st_atom_array.c
@@ -125,7 +125,10 @@ init_velement(const struct st_vertex_program *vp,
 /* ALWAYS_INLINE helps the compiler realize that most of the parameters are
  * on the stack.
  */
-void ALWAYS_INLINE
+void
+#ifndef _MSC_VER /* MSVC doesn't like inlining public functions */
+ALWAYS_INLINE
+#endif
 st_setup_arrays(struct st_context *st,
                 const struct st_vertex_program *vp,
                 const struct st_common_variant *vp_variant,