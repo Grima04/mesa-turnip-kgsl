Grima04/mesa-turnip-kgsl

commit f76cbc7901f7d500f5a4f74aedfd29970d1efd00

util: Add os_same_file_description helper
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3202>

src/util/os_file.c
@@ -34,7 +34,9 @@ os_file_create_unique(const char *filename, int filemode)
 #if defined(__linux__)
 
 #include <fcntl.h>
+#include <linux/kcmp.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <unistd.h>
 
 
@@ -130,13 +132,34 @@ os_read_file(const char *filename)
    return buf;
 }
 
+bool
+os_same_file_description(int fd1, int fd2)
+{
+   pid_t pid = getpid();
+
+   return syscall(SYS_kcmp, pid, pid, KCMP_FILE, fd1, fd2) == 0;
+}
+
 #else
+
+#include "u_debug.h"
 
 char *
 os_read_file(const char *filename)
 {
    errno = -ENOSYS;
    return NULL;
+}
+
+bool
+os_same_file_description(int fd1, int fd2)
+{
+   if (fd1 == fd2)
+      return true;
+
+   debug_warn_once("Can't tell if different file descriptors reference the same"
+                   " file description, false negatives might cause trouble!\n");
+   return false;
 }
 
 #endif

src/util/os_file.h
@@ -8,6 +8,7 @@
 #ifndef _OS_FILE_H_
 #define _OS_FILE_H_
 
+#include <stdbool.h>
 #include <stdio.h>
 
 #ifdef  __cplusplus
@@ -29,6 +30,13 @@ os_file_create_unique(const char *filename, int filemode);
  */
 char *
 os_read_file(const char *filename);
+
+/*
+ * Returns true if the two file descriptors passed in can be determined to
+ * reference the same file description, false otherwise
+ */
+bool
+os_same_file_description(int fd1, int fd2);
 
 #ifdef __cplusplus
 }

commit f7bfb10c69dfe48a91e35523cb5ee641bdbf6988

util: remove the dependency on kcmp.h
Fixes: f76cbc7901f7 "util: Add os_same_file_description helper"

Acked-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3860>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3860>

src/util/os_file.c
@@ -34,11 +34,12 @@ os_file_create_unique(const char *filename, int filemode)
 #if defined(__linux__)
 
 #include <fcntl.h>
-#include <linux/kcmp.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
+/* copied from <linux/kcmp.h> */
+#define KCMP_FILE 0
 
 static ssize_t
 readN(int fd, char *buf, size_t len)