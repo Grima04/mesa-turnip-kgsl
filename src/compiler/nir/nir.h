ce87da71e93d9eea7e9a2667e3273cab9c97667f
Daniel Schürmann <daniel@schuermann.dev>
2020-03-04 16:55:13 +0100

    nir: add pass to lower discard() to demote()
    This pass is intended to work around game bugs, only!
    It also lowers nir_intrinsic_load_helper_invocation to
    nir_intrinsic_is_helper_invocation for consistency.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4047>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -4197,6 +4197,8 @@ typedef enum {
 bool nir_lower_interpolation(nir_shader *shader,
                              nir_lower_interpolation_options options);
 
+bool nir_lower_discard_to_demote(nir_shader *shader);
+
 bool nir_normalize_cubemap_coords(nir_shader *shader);
 
 void nir_live_ssa_defs_impl(nir_function_impl *impl);
