5c7ee8a9746b1ae7d852b1ae3e5408378547c156
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-03-05 10:28:13 -0500

    pan/bi: Improve block printing
    Skip predecessor printing if there are none and match a missing brace,
    also fixup the spacing.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4097>

--- src/panfrost/bifrost/bi_print.c
+++ src/panfrost/bifrost/bi_print.c
@@ -399,21 +399,25 @@ bi_print_block(bi_block *block, FILE *fp)
                         bi_print_instruction(ins, fp);
         }
 
+        fprintf(fp, "}");
+
         if (block->successors[0]) {
                 fprintf(fp, " -> ");
 
                 for (unsigned i = 0; i < ARRAY_SIZE(block->successors); ++i) {
                         if (block->successors[i])
-                                fprintf(fp, "block%u", block->successors[i]->name);
+                                fprintf(fp, "block%u ", block->successors[i]->name);
                 }
         }
 
-        fprintf(fp, " from ");
+        if (block->predecessors->entries) {
+                fprintf(fp, " from");
 
-        bi_foreach_predecessor(block, pred)
-                fprintf(fp, "block%u ", pred->name);
+                bi_foreach_predecessor(block, pred)
+                        fprintf(fp, " block%u", pred->name);
+        }
 
-        fprintf(fp, "\n");
+        fprintf(fp, "\n\n");
 }
 
 void
