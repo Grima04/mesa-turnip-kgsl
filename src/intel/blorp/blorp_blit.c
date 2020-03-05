69a0150e4e8c3a9c46375dbfb49d3ada9b47b7f1
Jason Ekstrand <jason@jlekstrand.net>
2020-03-05 11:47:27 -0600

    intel/blorp: Allow STC_CCS in blit sources
    Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4056>

--- src/intel/blorp/blorp_blit.c
+++ src/intel/blorp/blorp_blit.c
@@ -2677,7 +2677,8 @@ blorp_copy(struct blorp_batch *batch,
           params.src.aux_usage == ISL_AUX_USAGE_HIZ_CCS_WT ||
           params.src.aux_usage == ISL_AUX_USAGE_MCS ||
           params.src.aux_usage == ISL_AUX_USAGE_MCS_CCS ||
-          params.src.aux_usage == ISL_AUX_USAGE_CCS_E);
+          params.src.aux_usage == ISL_AUX_USAGE_CCS_E ||
+          params.src.aux_usage == ISL_AUX_USAGE_STC_CCS);
 
    if (isl_aux_usage_has_hiz(params.src.aux_usage)) {
       /* In order to use HiZ, we have to use the real format for the source.
