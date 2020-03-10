9c53a3bb223592471d38a4efbfca8d58dd47e432
Tapani Pälli <tapani.palli@intel.com>
2020-03-10 09:21:09 +0200

    iris: toggle on PIPE_CAP_MIXED_COLOR_DEPTH_BITS
    This enables additional EGL configs where we have depth/stencil buffer
    with different number of bits per pixel than color buffer has. This
    enables some Android games to work that require such config.
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4127>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4127>

--- src/gallium/drivers/iris/iris_screen.c
+++ src/gallium/drivers/iris/iris_screen.c
@@ -199,6 +199,7 @@ iris_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_GL_SPIRV_VARIABLE_POINTERS:
    case PIPE_CAP_DEMOTE_TO_HELPER_INVOCATION:
    case PIPE_CAP_NATIVE_FENCE_FD:
+   case PIPE_CAP_MIXED_COLOR_DEPTH_BITS:
       return true;
    case PIPE_CAP_FBFETCH:
       return BRW_MAX_DRAW_BUFFERS;
