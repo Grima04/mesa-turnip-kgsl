8997757c6abfe657a259bc5c681628e70792b67a
Eric Anholt <eric@anholt.net>
2020-03-11 11:11:19 -0700

    ci: Enable ccaching of CMake builds as well.
    They ignore $PATH for unknown reasons, so you have to force the ccache
    wrapping yourself.
    
    Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4099>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4099>

--- .gitlab-ci/container/container_pre_build.sh
+++ .gitlab-ci/container/container_pre_build.sh
@@ -5,4 +5,9 @@ export CCACHE_COMPRESS=true
 export CCACHE_DIR=/cache/mesa/ccache
 export PATH=/usr/lib/ccache:$PATH
 
+# CMake ignores $PATH, so we have to force CC/GCC to the ccache versions.
+# Watch out, you can't have spaces in here because the renderdoc build fails.
+export CC="/usr/lib/ccache/gcc"
+export CXX="/usr/lib/ccache/g++"
+
 ccache --show-stats
