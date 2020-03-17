32eecf58791ea3c5367e5ff4a542ae25fc3ce61d
Michel Dänzer <mdaenzer@redhat.com>
2020-03-17 09:34:51 +0100

    gitlab-ci: Don't use buster-backports packages by default for x86_build
    The backports repository can be temporarily inconsistent between
    architectures, which can break the docker image build.
    
    Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4209>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4209>

--- .gitlab-ci/container/x86_build.sh
+++ .gitlab-ci/container/x86_build.sh
@@ -24,14 +24,6 @@ sed -i -e 's/http:\/\/deb/https:\/\/deb/g' /etc/apt/sources.list
 echo 'deb https://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
 
 apt-get update
-
-# Use newer packages from backports by default
-cat >/etc/apt/preferences <<EOF
-Package: *
-Pin: release a=buster-backports
-Pin-Priority: 500
-EOF
-
 apt-get dist-upgrade -y
 
 apt-get install -y --no-remove \
