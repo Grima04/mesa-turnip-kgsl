256d5ca80af0b742d4b2bf156180a22b1976fb80
Marek Olšák <marek.olsak@amd.com>
2020-03-22 00:47:23 -0400

    mesa: don't set unnecessary program flags in _mesa_update_state
    _NEW_PROGRAM is already set.
    _NEW_FRAG_CLAMP is not used by the fixed-func fragment shader.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4314>

--- src/mesa/main/state.c
+++ src/mesa/main/state.c
@@ -403,8 +403,7 @@ _mesa_update_state_locked( struct gl_context *ctx )
       if (ctx->FragmentProgram._MaintainTexEnvProgram) {
          prog_flags |= (_NEW_BUFFERS | _NEW_TEXTURE_OBJECT | _NEW_FOG |
                         _NEW_VARYING_VP_INPUTS | _NEW_LIGHT | _NEW_POINT |
-                        _NEW_RENDERMODE | _NEW_PROGRAM | _NEW_FRAG_CLAMP |
-                        _NEW_COLOR | _NEW_TEXTURE_STATE);
+                        _NEW_RENDERMODE | _NEW_COLOR | _NEW_TEXTURE_STATE);
       }
       if (ctx->VertexProgram._MaintainTnlProgram) {
          prog_flags |= (_NEW_VARYING_VP_INPUTS | _NEW_TEXTURE_OBJECT |
