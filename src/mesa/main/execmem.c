Grima04/mesa-turnip-kgsl

commit e5339fe4a47c242693962c9f90bbab8b74935cba

Move compiler.h and imports.h/c from src/mesa/main into src/util
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4324>

src/mesa/main/execmem.c
@@ -32,7 +32,8 @@
 
 
 #include <stdio.h>
-#include "imports.h"
+#include "util/imports.h"
+#include "main/glheader.h"
 #include "execmem.h"
 #include "c11/threads.h"
 

commit 7df75203052fa8a8f2fbd603a4c7553752c57b0f

mesa: Change _mesa_exec_malloc argument type.
Fix build error.

In file included from ../src/mesa/x86/rtasm/x86sse.c:7:0:
../src/mesa/main/execmem.h:31:19: error: unknown type name ‘GLuint’; did you mean ‘uint’?
 _mesa_exec_malloc(GLuint size);
                   ^~~~~~
                   uint

Suggested-by: Marek Olšák <marek.olsak@amd.com>
Fixes: e5339fe4a47c ("Move compiler.h and imports.h/c from src/mesa/main into src/util")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4361>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4361>

src/mesa/main/execmem.c
@@ -90,7 +90,7 @@ init_heap(void)
 
 
 void *
-_mesa_exec_malloc(GLuint size)
+_mesa_exec_malloc(unsigned size)
 {
    struct mem_block *block = NULL;
    void *addr = NULL;
@@ -140,7 +140,7 @@ _mesa_exec_free(void *addr)
  */
 
 void *
-_mesa_exec_malloc(GLuint size)
+_mesa_exec_malloc(unsigned size)
 {
    return malloc( size );
 }

src/mesa/main/execmem.h
@@ -28,7 +28,7 @@
 
 
 extern void *
-_mesa_exec_malloc(GLuint size);
+_mesa_exec_malloc(unsigned size);
 
 extern void
 _mesa_exec_free(void *addr);