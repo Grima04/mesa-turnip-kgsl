ae7da1a01706835120bd59ea069e49cf325feaa3
Rob Clark <robdclark@chromium.org>
2020-03-30 15:52:30 -0700

    util: move ALIGN/ROUND_DOWN_TO to u_math.h
    These are less mesa specific than the rest of macros.h, and would be
    nice to use outside of mesa.  Prep for next patch.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4381>

--- src/mesa/main/macros.h
+++ src/mesa/main/macros.h
@@ -691,52 +691,6 @@ minify(unsigned value, unsigned levels)
     return MAX2(1, value >> levels);
 }
 
-/**
- * Align a value up to an alignment value
- *
- * If \c value is not already aligned to the requested alignment value, it
- * will be rounded up.
- *
- * \param value  Value to be rounded
- * \param alignment  Alignment value to be used.  This must be a power of two.
- *
- * \sa ROUND_DOWN_TO()
- */
-static inline uintptr_t
-ALIGN(uintptr_t value, int32_t alignment)
-{
-   assert((alignment > 0) && _mesa_is_pow_two(alignment));
-   return (((value) + (alignment) - 1) & ~((alignment) - 1));
-}
-
-/**
- * Like ALIGN(), but works with a non-power-of-two alignment.
- */
-static inline uintptr_t
-ALIGN_NPOT(uintptr_t value, int32_t alignment)
-{
-   assert(alignment > 0);
-   return (value + alignment - 1) / alignment * alignment;
-}
-
-/**
- * Align a value down to an alignment value
- *
- * If \c value is not already aligned to the requested alignment value, it
- * will be rounded down.
- *
- * \param value  Value to be rounded
- * \param alignment  Alignment value to be used.  This must be a power of two.
- *
- * \sa ALIGN()
- */
-static inline uintptr_t
-ROUND_DOWN_TO(uintptr_t value, int32_t alignment)
-{
-   assert((alignment > 0) && _mesa_is_pow_two(alignment));
-   return ((value) & ~(alignment - 1));
-}
-
 
 /** Cross product of two 3-element vectors */
 static inline void
