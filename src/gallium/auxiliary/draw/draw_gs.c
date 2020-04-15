Grima04/mesa-turnip-kgsl

commit bf12bc2dd7a28844103bb30a07be0440e60c5864

draw: add nir info gathering and building support
Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/draw/draw_gs.c
@@ -35,7 +35,7 @@
 
 #include "tgsi/tgsi_parse.h"
 #include "tgsi/tgsi_exec.h"
-
+#include "nir/nir_to_tgsi_info.h"
 #include "pipe/p_shader_tokens.h"
 
 #include "util/u_math.h"
@@ -793,13 +793,17 @@ draw_create_geometry_shader(struct draw_context *draw,
 
    gs->draw = draw;
    gs->state = *state;
-   gs->state.tokens = tgsi_dup_tokens(state->tokens);
-   if (!gs->state.tokens) {
-      FREE(gs);
-      return NULL;
-   }
 
-   tgsi_scan_shader(state->tokens, &gs->info);
+   if (state->type == PIPE_SHADER_IR_TGSI) {
+      gs->state.tokens = tgsi_dup_tokens(state->tokens);
+      if (!gs->state.tokens) {
+         FREE(gs);
+         return NULL;
+      }
+
+      tgsi_scan_shader(state->tokens, &gs->info);
+   } else
+      nir_tgsi_scan_shader(state->ir.nir, &gs->info, false);
 
    /* setup the defaults */
    gs->max_out_prims = 0;

src/gallium/auxiliary/draw/draw_vs_llvm.c
@@ -37,7 +37,7 @@
 
 #include "tgsi/tgsi_parse.h"
 #include "tgsi/tgsi_scan.h"
-
+#include "nir/nir_to_tgsi_info.h"
 static void
 vs_llvm_prepare(struct draw_vertex_shader *shader,
                 struct draw_context *draw)
@@ -90,14 +90,20 @@ draw_create_vs_llvm(struct draw_context *draw,
    if (!vs)
       return NULL;
 
-   /* we make a private copy of the tokens */
-   vs->base.state.tokens = tgsi_dup_tokens(state->tokens);
-   if (!vs->base.state.tokens) {
-      FREE(vs);
-      return NULL;
-   }
+   /* due to some bugs in the feedback state tracker we have to check
+      for ir.nir & PIPE_SHADER_IR_NIR here. */
+   if (state->ir.nir && state->type == PIPE_SHADER_IR_NIR)
+      nir_tgsi_scan_shader(state->ir.nir, &vs->base.info, false);
+   else {
+      /* we make a private copy of the tokens */
+      vs->base.state.tokens = tgsi_dup_tokens(state->tokens);
+      if (!vs->base.state.tokens) {
+         FREE(vs);
+         return NULL;
+      }
 
-   tgsi_scan_shader(state->tokens, &vs->base.info);
+      tgsi_scan_shader(state->tokens, &vs->base.info);
+   }
 
    vs->variant_key_size = 
       draw_llvm_variant_key_size(
@@ -106,6 +112,8 @@ draw_create_vs_llvm(struct draw_context *draw,
               vs->base.info.file_max[TGSI_FILE_SAMPLER_VIEW]+1),
          vs->base.info.file_max[TGSI_FILE_IMAGE]+1);
 
+   vs->base.state.type = state->type;
+   vs->base.state.ir.nir = state->ir.nir;
    vs->base.state.stream_output = state->stream_output;
    vs->base.draw = draw;
    vs->base.prepare = vs_llvm_prepare;

src/gallium/auxiliary/draw/draw_fs.c
@@ -32,6 +32,7 @@
 #include "util/u_prim.h"
 
 #include "tgsi/tgsi_parse.h"
+#include "nir/nir_to_tgsi_info.h"
 
 #include "draw_fs.h"
 #include "draw_private.h"
@@ -47,7 +48,10 @@ draw_create_fragment_shader(struct draw_context *draw,
    dfs = CALLOC_STRUCT(draw_fragment_shader);
    if (dfs) {
       dfs->base = *shader;
-      tgsi_scan_shader(shader->tokens, &dfs->info);
+      if (shader->type == PIPE_SHADER_IR_TGSI)
+         tgsi_scan_shader(shader->tokens, &dfs->info);
+      else
+         nir_tgsi_scan_shader(shader->ir.nir, &dfs->info, false);
    }
 
    return dfs;

src/gallium/auxiliary/draw/draw_llvm.c
@@ -43,6 +43,7 @@
 #include "gallivm/lp_bld_flow.h"
 #include "gallivm/lp_bld_debug.h"
 #include "gallivm/lp_bld_tgsi.h"
+#include "gallivm/lp_bld_nir.h"
 #include "gallivm/lp_bld_printf.h"
 #include "gallivm/lp_bld_intr.h"
 #include "gallivm/lp_bld_init.h"
@@ -646,7 +647,10 @@ draw_llvm_create_variant(struct draw_llvm *llvm,
    memcpy(&variant->key, key, shader->variant_key_size);
 
    if (gallivm_debug & (GALLIVM_DEBUG_TGSI | GALLIVM_DEBUG_IR)) {
-      tgsi_dump(llvm->draw->vs.vertex_shader->state.tokens, 0);
+      if (llvm->draw->vs.vertex_shader->state.type == PIPE_SHADER_IR_TGSI)
+         tgsi_dump(llvm->draw->vs.vertex_shader->state.tokens, 0);
+      else
+         nir_print_shader(llvm->draw->vs.vertex_shader->state.ir.nir, stderr);
       draw_llvm_dump_variant_key(&variant->key);
    }
 
@@ -712,10 +716,17 @@ generate_vs(struct draw_llvm_variant *variant,
    params.ssbo_sizes_ptr = num_ssbos_ptr;
    params.image = draw_image;
 
-   lp_build_tgsi_soa(variant->gallivm,
-                     tokens,
-                     &params,
-                     outputs);
+   if (llvm->draw->vs.vertex_shader->state.ir.nir &&
+       llvm->draw->vs.vertex_shader->state.type == PIPE_SHADER_IR_NIR)
+      lp_build_nir_soa(variant->gallivm,
+                       llvm->draw->vs.vertex_shader->state.ir.nir,
+                       &params,
+                       outputs);
+   else
+      lp_build_tgsi_soa(variant->gallivm,
+                        tokens,
+                        &params,
+                        outputs);
 
    {
       LLVMValueRef out;
@@ -2495,7 +2506,10 @@ draw_gs_llvm_generate(struct draw_llvm *llvm,
    }
 
    if (gallivm_debug & (GALLIVM_DEBUG_TGSI | GALLIVM_DEBUG_IR)) {
-      tgsi_dump(tokens, 0);
+      if (llvm->draw->gs.geometry_shader->state.type == PIPE_SHADER_IR_TGSI)
+         tgsi_dump(tokens, 0);
+      else
+         nir_print_shader(llvm->draw->gs.geometry_shader->state.ir.nir, stderr);
       draw_gs_llvm_dump_variant_key(&variant->key);
    }
 
@@ -2515,10 +2529,16 @@ draw_gs_llvm_generate(struct draw_llvm *llvm,
    params.ssbo_sizes_ptr = num_ssbos_ptr;
    params.image = image;
 
-   lp_build_tgsi_soa(variant->gallivm,
-                     tokens,
-                     &params,
-                     outputs);
+   if (llvm->draw->gs.geometry_shader->state.type == PIPE_SHADER_IR_TGSI)
+      lp_build_tgsi_soa(variant->gallivm,
+                        tokens,
+                        &params,
+                        outputs);
+   else
+      lp_build_nir_soa(variant->gallivm,
+                       llvm->draw->gs.geometry_shader->state.ir.nir,
+                       &params,
+                       outputs);
 
    sampler->destroy(sampler);
    image->destroy(image);

src/gallium/auxiliary/draw/draw_vs_exec.c
@@ -230,6 +230,7 @@ draw_create_vs_exec(struct draw_context *draw,
 
    tgsi_scan_shader(state->tokens, &vs->base.info);
 
+   vs->base.state.type = state->type;
    vs->base.state.stream_output = state->stream_output;
    vs->base.draw = draw;
    vs->base.prepare = vs_exec_prepare;

commit f01c0565bb9ad7335395920aebc7489cb76d1b72

draw: free the NIR IR.
Not sure how I missed this, the ownership was a bit blurry,
free the NIR.

Fixes: bf12bc2dd7a2 (draw: add nir info gathering and building support)
Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4563>

src/gallium/auxiliary/draw/draw_gs.c
@@ -41,7 +41,7 @@
 #include "util/u_math.h"
 #include "util/u_memory.h"
 #include "util/u_prim.h"
-
+#include "util/ralloc.h"
 /* fixme: move it from here */
 #define MAX_PRIMITIVES 64
 
@@ -962,6 +962,9 @@ void draw_delete_geometry_shader(struct draw_context *draw,
 
    for (i = 0; i < TGSI_MAX_VERTEX_STREAMS; i++)
       FREE(dgs->stream[i].primitive_lengths);
+
+   if (dgs->state.ir.nir)
+      ralloc_free(dgs->state.ir.nir);
    FREE((void*) dgs->state.tokens);
    FREE(dgs);
 }

src/gallium/auxiliary/draw/draw_vs_llvm.c
@@ -38,6 +38,8 @@
 #include "tgsi/tgsi_parse.h"
 #include "tgsi/tgsi_scan.h"
 #include "nir/nir_to_tgsi_info.h"
+#include "nir.h"
+
 static void
 vs_llvm_prepare(struct draw_vertex_shader *shader,
                 struct draw_context *draw)
@@ -76,6 +78,8 @@ vs_llvm_delete( struct draw_vertex_shader *dvs )
    }
 
    assert(shader->variants_cached == 0);
+   if (dvs->state.ir.nir)
+      ralloc_free(dvs->state.ir.nir);
    FREE((void*) dvs->state.tokens);
    FREE( dvs );
 }
@@ -92,9 +96,10 @@ draw_create_vs_llvm(struct draw_context *draw,
 
    /* due to some bugs in the feedback state tracker we have to check
       for ir.nir & PIPE_SHADER_IR_NIR here. */
-   if (state->ir.nir && state->type == PIPE_SHADER_IR_NIR)
+   if (state->ir.nir && state->type == PIPE_SHADER_IR_NIR) {
+      vs->base.state.ir.nir = state->ir.nir;
       nir_tgsi_scan_shader(state->ir.nir, &vs->base.info, true);
-   else {
+   } else {
       /* we make a private copy of the tokens */
       vs->base.state.tokens = tgsi_dup_tokens(state->tokens);
       if (!vs->base.state.tokens) {
@@ -113,7 +118,6 @@ draw_create_vs_llvm(struct draw_context *draw,
          vs->base.info.file_max[TGSI_FILE_IMAGE]+1);
 
    vs->base.state.type = state->type;
-   vs->base.state.ir.nir = state->ir.nir;
    vs->base.state.stream_output = state->stream_output;
    vs->base.draw = draw;
    vs->base.prepare = vs_llvm_prepare;