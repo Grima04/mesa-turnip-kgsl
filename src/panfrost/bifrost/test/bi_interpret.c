23ffaa16c7e227cd4d82350c223f965c0c8dac8c
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-04-24 19:14:21 -0400

    pan/bit: Interpret BI_SELECT
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4766>

--- src/panfrost/bifrost/test/bi_interpret.c
+++ src/panfrost/bifrost/test/bi_interpret.c
@@ -529,8 +529,20 @@ bit_step(struct bit_state *s, bi_instruction *ins, bool FMA)
                 break;
        }
 
+        case BI_SELECT: {
+                if (ins->dest_type == nir_type_uint16) {
+                        for (unsigned c = 0; c < 2; ++c)
+                                dest.u16[c] = srcs[c].u16[ins->swizzle[c][0]];
+                } else if (ins->dest_type == nir_type_uint8) {
+                        for (unsigned c = 0; c < 4; ++c)
+                                dest.u8[c] = srcs[c].u8[ins->swizzle[c][0]];
+                } else {
+                        unreachable("Unknown type");
+                }
+                break;
+        }
+
         case BI_SHIFT:
-        case BI_SELECT:
         case BI_ROUND:
                 unreachable("Unsupported op");
         
