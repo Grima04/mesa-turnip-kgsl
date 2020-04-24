Grima04/mesa-turnip-kgsl

commit 4ef3f7e3d37ece7b4339870282cb52c5e334a68d

anv: Enable Vulkan 1.2 support
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/intel/vulkan/anv_device.c
@@ -1614,8 +1614,8 @@ anv_get_physical_device_properties_1_2(struct anv_physical_device *pdevice,
             "Mesa " PACKAGE_VERSION MESA_GIT_SHA1);
    p->conformanceVersion = (VkConformanceVersionKHR) {
       .major = 1,
-      .minor = 1,
-      .subminor = 2,
+      .minor = 2,
+      .subminor = 0,
       .patch = 0,
    };
 
@@ -4189,7 +4189,7 @@ void anv_DestroyBuffer(
    vk_free2(&device->alloc, pAllocator, buffer);
 }
 
-VkDeviceAddress anv_GetBufferDeviceAddressKHR(
+VkDeviceAddress anv_GetBufferDeviceAddress(
     VkDevice                                    device,
     const VkBufferDeviceAddressInfoKHR*         pInfo)
 {
@@ -4201,14 +4201,14 @@ VkDeviceAddress anv_GetBufferDeviceAddressKHR(
    return anv_address_physical(buffer->address);
 }
 
-uint64_t anv_GetBufferOpaqueCaptureAddressKHR(
+uint64_t anv_GetBufferOpaqueCaptureAddress(
     VkDevice                                    device,
     const VkBufferDeviceAddressInfoKHR*         pInfo)
 {
    return 0;
 }
 
-uint64_t anv_GetDeviceMemoryOpaqueCaptureAddressKHR(
+uint64_t anv_GetDeviceMemoryOpaqueCaptureAddress(
     VkDevice                                    device,
     const VkDeviceMemoryOpaqueCaptureAddressInfoKHR* pInfo)
 {

src/intel/vulkan/anv_extensions.py
@@ -54,6 +54,7 @@ API_PATCH_VERSION = 131
 API_VERSIONS = [
     ApiVersion('1.0',   True),
     ApiVersion('1.1',   True),
+    ApiVersion('1.2',   True),
 ]
 
 MAX_API_VERSION = None # Computed later

src/intel/vulkan/anv_pass.c
@@ -339,7 +339,7 @@ VkResult anv_CreateRenderPass(
 
    for (uint32_t i = 0; i < pCreateInfo->dependencyCount; i++) {
       /* Convert to a Dependency2KHR */
-      VkSubpassDependency2KHR dep2 = {
+      VkSubpassDependency2 dep2 = {
          .srcSubpass       = pCreateInfo->pDependencies[i].srcSubpass,
          .dstSubpass       = pCreateInfo->pDependencies[i].dstSubpass,
          .srcStageMask     = pCreateInfo->pDependencies[i].srcStageMask,
@@ -392,7 +392,7 @@ num_subpass_attachments2(const VkSubpassDescription2KHR *desc)
           (ds_resolve && ds_resolve->pDepthStencilResolveAttachment);
 }
 
-VkResult anv_CreateRenderPass2KHR(
+VkResult anv_CreateRenderPass2(
     VkDevice                                    _device,
     const VkRenderPassCreateInfo2KHR*           pCreateInfo,
     const VkAllocationCallbacks*                pAllocator,

src/intel/vulkan/anv_queue.c
@@ -2145,7 +2145,7 @@ VkResult anv_GetSemaphoreFdKHR(
    return VK_SUCCESS;
 }
 
-VkResult anv_GetSemaphoreCounterValueKHR(
+VkResult anv_GetSemaphoreCounterValue(
     VkDevice                                    _device,
     VkSemaphore                                 _semaphore,
     uint64_t*                                   pValue)
@@ -2277,7 +2277,7 @@ anv_timelines_wait(struct anv_device *device,
    }
 }
 
-VkResult anv_WaitSemaphoresKHR(
+VkResult anv_WaitSemaphores(
     VkDevice                                    _device,
     const VkSemaphoreWaitInfoKHR*               pWaitInfo,
     uint64_t                                    timeout)
@@ -2329,7 +2329,7 @@ VkResult anv_WaitSemaphoresKHR(
    return result;
 }
 
-VkResult anv_SignalSemaphoreKHR(
+VkResult anv_SignalSemaphore(
     VkDevice                                    _device,
     const VkSemaphoreSignalInfoKHR*             pSignalInfo)
 {

src/intel/vulkan/genX_cmd_buffer.c
@@ -3494,7 +3494,7 @@ emit_draw_count_predicate_with_conditional_render(
 }
 #endif
 
-void genX(CmdDrawIndirectCountKHR)(
+void genX(CmdDrawIndirectCount)(
     VkCommandBuffer                             commandBuffer,
     VkBuffer                                    _buffer,
     VkDeviceSize                                offset,
@@ -3560,7 +3560,7 @@ void genX(CmdDrawIndirectCountKHR)(
    }
 }
 
-void genX(CmdDrawIndexedIndirectCountKHR)(
+void genX(CmdDrawIndexedIndirectCount)(
     VkCommandBuffer                             commandBuffer,
     VkBuffer                                    _buffer,
     VkDeviceSize                                offset,
@@ -5249,7 +5249,7 @@ void genX(CmdBeginRenderPass)(
    cmd_buffer_begin_subpass(cmd_buffer, 0);
 }
 
-void genX(CmdBeginRenderPass2KHR)(
+void genX(CmdBeginRenderPass2)(
     VkCommandBuffer                             commandBuffer,
     const VkRenderPassBeginInfo*                pRenderPassBeginInfo,
     const VkSubpassBeginInfoKHR*                pSubpassBeginInfo)
@@ -5274,7 +5274,7 @@ void genX(CmdNextSubpass)(
    cmd_buffer_begin_subpass(cmd_buffer, prev_subpass + 1);
 }
 
-void genX(CmdNextSubpass2KHR)(
+void genX(CmdNextSubpass2)(
     VkCommandBuffer                             commandBuffer,
     const VkSubpassBeginInfoKHR*                pSubpassBeginInfo,
     const VkSubpassEndInfoKHR*                  pSubpassEndInfo)
@@ -5306,7 +5306,7 @@ void genX(CmdEndRenderPass)(
    cmd_buffer->state.subpass = NULL;
 }
 
-void genX(CmdEndRenderPass2KHR)(
+void genX(CmdEndRenderPass2)(
     VkCommandBuffer                             commandBuffer,
     const VkSubpassEndInfoKHR*                  pSubpassEndInfo)
 {

src/intel/vulkan/genX_query.c
@@ -544,7 +544,7 @@ void genX(CmdResetQueryPool)(
    }
 }
 
-void genX(ResetQueryPoolEXT)(
+void genX(ResetQueryPool)(
     VkDevice                                    _device,
     VkQueryPool                                 queryPool,
     uint32_t                                    firstQuery,

src/intel/vulkan/genX_state.c
@@ -446,9 +446,9 @@ VkResult genX(CreateSampler)(
          break;
       }
 #if GEN_GEN >= 9
-      case VK_STRUCTURE_TYPE_SAMPLER_REDUCTION_MODE_CREATE_INFO_EXT: {
-         VkSamplerReductionModeCreateInfoEXT *sampler_reduction =
-            (VkSamplerReductionModeCreateInfoEXT *) ext;
+      case VK_STRUCTURE_TYPE_SAMPLER_REDUCTION_MODE_CREATE_INFO: {
+         VkSamplerReductionModeCreateInfo *sampler_reduction =
+            (VkSamplerReductionModeCreateInfo *) ext;
          sampler_reduction_mode =
             vk_to_gen_sampler_reduction_mode[sampler_reduction->reductionMode];
          enable_sampler_reduction = true;

commit 9c009da208b77496011f149fd1e289656da0f226

anv: Drop an assert
Ever since Vulkan 1.2, this feature has been in core so enabling the
extension is no longer required.

Fixes: 4ef3f7e3d37e "anv: Enable Vulkan 1.2 support"
Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4694>

src/intel/vulkan/anv_device.c
@@ -4299,7 +4299,6 @@ VkResult anv_CreateFramebuffer(
       }
       framebuffer->attachment_count = pCreateInfo->attachmentCount;
    } else {
-      assert(device->enabled_extensions.KHR_imageless_framebuffer);
       framebuffer = vk_alloc2(&device->alloc, pAllocator, size, 8,
                               VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
       if (framebuffer == NULL)