e731740388ca2822ad6a9a09f49be2d05d0a5c38
Christian Gmeiner <christian.gmeiner@gmail.com>
2020-04-24 12:47:04 +0200

    etnaviv: drop default state for FE_HALTI5_ID_CONFIG
    It gets emitted when needed - see emit_halti5_only_state(..).
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Reviewed-by: Jonathan Marek <jonathan@marek.ca>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4729>

--- src/gallium/drivers/etnaviv/etnaviv_context.c
+++ src/gallium/drivers/etnaviv/etnaviv_context.c
@@ -423,7 +423,6 @@ etna_reset_gpu_state(struct etna_context *ctx)
    if (screen->specs.halti >= 5) { /* Only on HALTI5+ */
       etna_set_state(stream, VIVS_NTE_DESCRIPTOR_UNK14C40, 0x00000001);
       etna_set_state(stream, VIVS_FE_HALTI5_UNK007D8, 0x00000002);
-      etna_set_state(stream, VIVS_FE_HALTI5_ID_CONFIG, 0x00000000);
       etna_set_state(stream, VIVS_PS_SAMPLER_BASE, 0x00000000);
       etna_set_state(stream, VIVS_VS_SAMPLER_BASE, 0x00000020);
       etna_set_state(stream, VIVS_SH_CONFIG, VIVS_SH_CONFIG_RTNE_ROUNDING);
