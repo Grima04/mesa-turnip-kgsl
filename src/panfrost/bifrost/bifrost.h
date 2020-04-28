527d7303cabb5512e7de2569feaaefc3a3ae4354
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-04-28 10:34:26 -0400

    pan/bi: Structify ADD FCMP16
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4789>

--- src/panfrost/bifrost/bifrost.h
+++ src/panfrost/bifrost/bifrost.h
@@ -434,6 +434,20 @@ struct bifrost_fma_fcmp16 {
         unsigned op   : 7;
 } __attribute__((packed));
 
+struct bifrost_add_fcmp16 {
+        unsigned src0 : 3;
+        unsigned src1 : 3;
+        enum bifrost_fcmp_cond cond : 3;
+
+        unsigned src0_swizzle : 2;
+        unsigned src1_swizzle : 2;
+
+        /* No abs mods */
+        unsigned src0_neg : 1;
+
+        unsigned op   : 6;
+} __attribute__((packed));
+
 /* Two sources for vectorization */
 #define BIFROST_FMA_FLOAT32_TO_16 (0xdd000 >> 3)
 #define BIFROST_ADD_FLOAT32_TO_16 (0x0EC00 >> 3)
