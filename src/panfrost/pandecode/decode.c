Grima04/mesa-turnip-kgsl

commit 6148d1be4bb52039ccda57f25a9d27ecb7aa7541

panfrost: Fix size of bifrost sampler descriptor
Should be 32-bytes, it looks like.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4844>

src/panfrost/pandecode/decode.c
@@ -2441,9 +2441,9 @@ pandecode_samplers(mali_ptr samplers, unsigned sampler_count, int job_no, bool i
                         pandecode_prop("min_lod = FIXED_16(%f)", DECODE_FIXED_16(s->min_lod));
                         pandecode_prop("max_lod = FIXED_16(%f)", DECODE_FIXED_16(s->max_lod));
 
-                        if (s->zero1 || s->zero2 || s->zero3 || s->zero4 || s->zero5) {
+                        if (s->zero1 || s->zero2 || s->zero3 || s->zero4) {
                                 pandecode_msg("XXX: sampler zero tripped\n");
-                                pandecode_prop("zero = 0x%X, 0x%X, 0x%X, 0x%X, 0x%X\n", s->zero1, s->zero2, s->zero3, s->zero4, s->zero5);
+                                pandecode_prop("zero = 0x" PRIx64 ", 0x" PRIx64 ", 0x" PRIx64 ", 0x" PRIx64 "\n", s->zero1, s->zero2, s->zero3, s->zero4);
                         }
 
                         pandecode_indent--;

src/panfrost/include/panfrost-job.h
@@ -1361,11 +1361,10 @@ struct bifrost_sampler_descriptor {
 
         int16_t min_lod;
         int16_t max_lod;
-        int8_t zero2;
-        int8_t zero3;
 
-        uint32_t zero4;
-        uint32_t zero5;
+        uint64_t zero2;
+        uint64_t zero3;
+        uint64_t zero4;
 } __attribute__((packed));
 
 /* viewport0/viewport1 form the arguments to glViewport. viewport1 is

commit 7eb2bc8f52f0e4aaaac1add6236841484dabeecf

pan/decode: Properly print tripped zeroes
The "%" got lost.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Fixes: 6148d1be4bb5 ("panfrost: Fix size of bifrost sampler descriptor")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4832>

src/panfrost/pandecode/decode.c
@@ -2443,7 +2443,7 @@ pandecode_samplers(mali_ptr samplers, unsigned sampler_count, int job_no, bool i
 
                         if (s->zero1 || s->zero2 || s->zero3 || s->zero4) {
                                 pandecode_msg("XXX: sampler zero tripped\n");
-                                pandecode_prop("zero = 0x" PRIx64 ", 0x" PRIx64 ", 0x" PRIx64 ", 0x" PRIx64 "\n", s->zero1, s->zero2, s->zero3, s->zero4);
+                                pandecode_prop("zero = 0x%" PRIx64 ", 0x%" PRIx64 ", 0x%" PRIx64 ", 0x%" PRIx64 "\n", s->zero1, s->zero2, s->zero3, s->zero4);
                         }
 
                         pandecode_indent--;