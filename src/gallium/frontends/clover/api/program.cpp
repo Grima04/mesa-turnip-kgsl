a92afdea580cd75b93566bcea5f763de09319f88
Pierre Moreau <dev@pmoreau.org>
2020-05-05 13:16:42 +0200

    clover: Implement clCreateProgramWithILKHR
    v2: Use the same storage for OpenCL C sources and ILs representations
        (Karol Herbst, Francisco Jerez)
    v3:
    * Remove `program::has_source` and instead add a value to
      `program::il_type` for sources. (Francisco Jerez)
    * Use `std::move()` on sources.
    * Replace `CL_MAKE_VERSION(99999999u, 0u, 0u)` with
      `std::numeric_limits<uint32_t>::max()` (Francisco Jerez)
    
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>
    Signed-off-by: Pierre Moreau <dev@pmoreau.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/2078>

--- src/gallium/frontends/clover/api/program.cpp
+++ src/gallium/frontends/clover/api/program.cpp
@@ -22,8 +22,10 @@
 
 #include "api/util.hpp"
 #include "core/program.hpp"
+#include "spirv/invocation.hpp"
 #include "util/u_debug.h"
 
+#include <limits>
 #include <sstream>
 
 using namespace clover;
@@ -71,6 +73,29 @@ namespace {
             }, objs<allow_empty_tag>(d_devs, num_devs)))
          throw error(CL_INVALID_DEVICE);
    }
+
+   enum program::il_type
+   identify_and_validate_il(const std::string &il,
+                            const cl_version opencl_version,
+                            const context::notify_action &notify) {
+
+      enum program::il_type il_type = program::il_type::none;
+
+#ifdef HAVE_CLOVER_SPIRV
+      if (spirv::is_binary_spirv(il)) {
+         std::string log;
+         if (!spirv::is_valid_spirv(il, opencl_version, log)) {
+            if (notify) {
+               notify(log.c_str());
+            }
+            throw error(CL_INVALID_VALUE);
+         }
+         il_type = program::il_type::spirv;
+      }
+#endif
+
+      return il_type;
+   }
 }
 
 CLOVER_API cl_program
@@ -92,7 +117,7 @@ clCreateProgramWithSource(cl_context d_ctx, cl_uint count,
 
    // ...and create a program object for them.
    ret_error(r_errcode, CL_SUCCESS);
-   return new program(ctx, source);
+   return new program(ctx, std::move(source), program::il_type::source);
 
 } catch (error &e) {
    ret_error(r_errcode, e);
@@ -148,6 +173,40 @@ clCreateProgramWithBinary(cl_context d_ctx, cl_uint n,
    // initialize a program object with them.
    ret_error(r_errcode, CL_SUCCESS);
    return new program(ctx, devs, map(values(), result));
+
+} catch (error &e) {
+   ret_error(r_errcode, e);
+   return NULL;
+}
+
+cl_program
+clover::CreateProgramWithILKHR(cl_context d_ctx, const void *il,
+                               size_t length, cl_int *r_errcode) try {
+   auto &ctx = obj(d_ctx);
+
+   if (!il || !length)
+      throw error(CL_INVALID_VALUE);
+
+   // Compute the highest OpenCL version supported by all devices associated to
+   // the context. That is the version used for validating the SPIR-V binary.
+   cl_version min_opencl_version = std::numeric_limits<uint32_t>::max();
+   for (const device &dev : ctx.devices()) {
+      const cl_version opencl_version = dev.device_version();
+      min_opencl_version = std::min(opencl_version, min_opencl_version);
+   }
+
+   const char *stream = reinterpret_cast<const char *>(il);
+   std::string binary(stream, stream + length);
+   const enum program::il_type il_type = identify_and_validate_il(binary,
+                                                                  min_opencl_version,
+                                                                  ctx.notify);
+
+   if (il_type == program::il_type::none)
+      throw error(CL_INVALID_VALUE);
+
+   // Initialize a program object with it.
+   ret_error(r_errcode, CL_SUCCESS);
+   return new program(ctx, std::move(binary), il_type);
 
 } catch (error &e) {
    ret_error(r_errcode, e);
@@ -210,7 +269,7 @@ clBuildProgram(cl_program d_prog, cl_uint num_devs,
 
    auto notifier = build_notifier(d_prog, pfn_notify, user_data);
 
-   if (prog.has_source) {
+   if (prog.il_type() != program::il_type::none) {
       prog.compile(devs, opts);
       prog.link(devs, opts, { prog });
    } else if (any_of([&](const device &dev){
@@ -248,11 +307,11 @@ clCompileProgram(cl_program d_prog, cl_uint num_devs,
    if (bool(num_headers) != bool(header_names))
       throw error(CL_INVALID_VALUE);
 
-   if (!prog.has_source)
+   if (prog.il_type() == program::il_type::none)
       throw error(CL_INVALID_OPERATION);
 
    for_each([&](const char *name, const program &header) {
-         if (!header.has_source)
+         if (header.il_type() == program::il_type::none)
             throw error(CL_INVALID_OPERATION);
 
          if (!any_of(key_equals(name), headers))
