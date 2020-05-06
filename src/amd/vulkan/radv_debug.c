f105b69464d908ee8b54c0bddb51909ebde4d686
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2020-05-06 09:38:17 +0200

    radv: report correct backend IR in hang reports when ACO is used
    Trivial.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Acked-by: Daniel Schürmann <daniel@schuermann.dev>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4911>

--- src/amd/vulkan/radv_debug.c
+++ src/amd/vulkan/radv_debug.c
@@ -432,7 +432,9 @@ radv_dump_shader(struct radv_pipeline *pipeline,
 		fprintf(f, "NIR:\n%s\n", shader->nir_string);
 	}
 
-	fprintf(f, "LLVM IR:\n%s\n", shader->ir_string);
+	fprintf(f, "%s IR:\n%s\n",
+		pipeline->device->physical_device->use_aco ? "ACO" : "LLVM",
+		shader->ir_string);
 	fprintf(f, "DISASM:\n%s\n", shader->disasm_string);
 
 	radv_shader_dump_stats(pipeline->device, shader, stage, f);
