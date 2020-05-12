06b6c687e2aba075e9fc3812d80c128bd873bbce
Danylo Piliaiev <danylo.piliaiev@globallogic.com>
2020-05-12 12:27:58 +0300

    anv: Fix deadlock in anv_timelines_wait
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/2945
    Fixes: 34f32a6d6648073e2fda3fb78377124fb32bb288
    Signed-off-by: Danylo Piliaiev <danylo.piliaiev@globallogic.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5005>

--- src/intel/vulkan/anv_queue.c
+++ src/intel/vulkan/anv_queue.c
@@ -2246,9 +2246,10 @@ anv_timelines_wait(struct anv_device *device,
                    uint64_t abs_timeout_ns)
 {
    if (!wait_all && n_timelines > 1) {
+      pthread_mutex_lock(&device->mutex);
+
       while (1) {
          VkResult result;
-         pthread_mutex_lock(&device->mutex);
          for (uint32_t i = 0; i < n_timelines; i++) {
             result =
                anv_timeline_wait_locked(device, timelines[i], serials[i], 0);
