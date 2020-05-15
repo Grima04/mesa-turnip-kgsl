38f32372aa94519f86f3358bea1e26e11398d59c
Eric Anholt <eric@anholt.net>
2020-05-15 16:27:01 -0700

    ci: Improve baremetal's logging of the job env var passthrough.
    Trying to read the sh -x script output was rough, just cat the file once
    we're done setting it up.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5089>

--- .gitlab-ci/bare-metal/fastboot.sh
+++ .gitlab-ci/bare-metal/fastboot.sh
@@ -51,6 +51,7 @@ cp -Rp $BM_ROOTFS rootfs
 # Set up the init script that brings up the system.
 cp $BM/init.sh rootfs/init
 
+set +x
 # Pass through relevant env vars from the gitlab job to the baremetal init script
 touch rootfs/set-job-env-vars.sh
 chmod +x rootfs/set-job-env-vars.sh
@@ -78,6 +79,9 @@ for var in \
   val=`echo ${!var} | sed 's|"||g'`
   echo "export $var=\"${val}\"" >> rootfs/set-job-env-vars.sh
 done
+echo "Variables passed through:"
+cat rootfs/set-job-env-vars.sh
+set -x
 
 # Add the Mesa drivers we built, and make a consistent symlink to them.
 mkdir -p rootfs/$CI_PROJECT_DIR
