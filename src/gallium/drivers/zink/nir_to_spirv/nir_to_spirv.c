09ac0350fdc97a746342c63deb4e9b626959d47a
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-05-15 12:05:11 +0200

    zink: implement i2b1
    This shuold really have been implemented before starting to use these,
    but I guess I missed them.
    
    Fixes a crash when starting a game in Warzone 2100.
    
    Fixes: 7f6a491eec0 ("zink: lower b2b to b2i")
    Reviewed-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5053>

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -1020,6 +1020,13 @@ emit_alu(struct ntv_context *ctx, nir_alu_instr *alu)
                                             nir_src_bit_size(alu->src[0].src),
                                             num_components, 0));
       break;
+   case nir_op_i2b1:
+      assert(nir_op_infos[alu->op].num_inputs == 1);
+      result = emit_binop(ctx, SpvOpINotEqual, dest_type, src[0],
+                          get_ivec_constant(ctx,
+                                            nir_src_bit_size(alu->src[0].src),
+                                            num_components, 0));
+      break;
 
 
 #define BINOP(nir_op, spirv_op) \
