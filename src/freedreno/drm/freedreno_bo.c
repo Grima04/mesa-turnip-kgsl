Grima04/mesa-turnip-kgsl

commit c9e8df61dc8737f167767f2bda8a56abcae8ad5e

freedreno: Initialize the bo's iova at creation time.
Avoids repeated conditionals at reloc time checking if we need to go ask
the kernel.

No statistically significant difference on the drawoverhead case I'm
looking at (n=300).

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4957>

src/freedreno/drm/freedreno_bo.c
@@ -72,6 +72,8 @@ static struct fd_bo * bo_from_handle(struct fd_device *dev,
 	bo->dev = fd_device_ref(dev);
 	bo->size = size;
 	bo->handle = handle;
+	bo->iova = bo->funcs->iova(bo);
+
 	p_atomic_set(&bo->refcnt, 1);
 	list_inithead(&bo->list);
 	/* add ourself into the handle table: */
@@ -225,14 +227,7 @@ out_unlock:
 
 uint64_t fd_bo_get_iova(struct fd_bo *bo)
 {
-	if (!bo->iova)
-		bo->iova = bo->funcs->iova(bo);
 	return bo->iova;
-}
-
-void fd_bo_put_iova(struct fd_bo *bo)
-{
-	/* currently a no-op */
 }
 
 struct fd_bo * fd_bo_ref(struct fd_bo *bo)

src/freedreno/drm/freedreno_drmif.h
@@ -158,7 +158,6 @@ struct fd_bo *fd_bo_from_handle(struct fd_device *dev,
 struct fd_bo * fd_bo_from_name(struct fd_device *dev, uint32_t name);
 struct fd_bo * fd_bo_from_dmabuf(struct fd_device *dev, int fd);
 uint64_t fd_bo_get_iova(struct fd_bo *bo);
-void fd_bo_put_iova(struct fd_bo *bo);
 struct fd_bo * fd_bo_ref(struct fd_bo *bo);
 void fd_bo_del(struct fd_bo *bo);
 int fd_bo_get_name(struct fd_bo *bo, uint32_t *name);

src/freedreno/drm/msm_ringbuffer_sp.c
@@ -416,7 +416,7 @@ msm_ringbuffer_sp_emit_reloc(struct fd_ringbuffer *ring,
 		pipe = msm_ring->u.submit->pipe;
 	}
 
-	uint64_t iova = fd_bo_get_iova(reloc->bo) + reloc->offset;
+	uint64_t iova = reloc->bo->iova + reloc->offset;
 	int shift = reloc->shift;
 
 	if (shift < 0)

src/gallium/drivers/freedreno/freedreno_state.c
@@ -588,10 +588,6 @@ fd_set_global_binding(struct pipe_context *pctx,
 
 		for (unsigned i = 0; i < count; i++) {
 			unsigned n = i + first;
-			if (so->buf[n]) {
-				struct fd_resource *rsc = fd_resource(so->buf[n]);
-				fd_bo_put_iova(rsc->bo);
-			}
 			pipe_resource_reference(&so->buf[n], NULL);
 		}
 

commit a6184eae3123840b1ff3d78e24454610e3013013

freedreno/drm: handle ancient kernels
Older kernels did not support `MSM_INFO_GET_IOVA`.  But this is only
required for (a) clover (ie. `fd_set_global_binding()`) and drm paths
that are limited to newer kernels.  So move the location of the assert
to fix new userspace on old kernels.

Fixes: c9e8df61dc8 ("freedreno: Initialize the bo's iova at creation time.")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Tested-by: Ilia Mirkin <imirkin@alum.mit.edu>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5081>

src/freedreno/drm/freedreno_bo.c
@@ -236,6 +236,8 @@ fd_bo_mark_for_dump(struct fd_bo *bo)
 
 uint64_t fd_bo_get_iova(struct fd_bo *bo)
 {
+	/* ancient kernels did not support this */
+	assert(bo->iova != 0);
 	return bo->iova;
 }
 

src/freedreno/drm/msm_bo.c
@@ -112,7 +112,8 @@ static uint64_t msm_bo_iova(struct fd_bo *bo)
 	int ret;
 
 	ret = drmCommandWriteRead(bo->dev->fd, DRM_MSM_GEM_INFO, &req, sizeof(req));
-	debug_assert(ret == 0);
+	if (ret)
+		return 0;
 
 	return req.value;
 }