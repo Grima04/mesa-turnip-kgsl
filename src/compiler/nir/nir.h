b0d1f9a72ff6e1c7164dc22ff5eb3e817f245d13
Jason Ekstrand <jason@jlekstrand.net>
2020-05-21 21:37:33 -0500

    nir: Add a nir_shader_preserve_all_metadata helper
    There are some passes which really work on the shader level and it's
    easier if we have a helper which preserves metadata on the whole shader.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5171>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -3294,6 +3294,8 @@ nir_function_impl *nir_cf_node_get_function(nir_cf_node *node);
 void nir_metadata_require(nir_function_impl *impl, nir_metadata required, ...);
 /** dirties all but the preserved metadata */
 void nir_metadata_preserve(nir_function_impl *impl, nir_metadata preserved);
+/** Preserves all metadata for the given shader */
+void nir_shader_preserve_all_metadata(nir_shader *shader);
 
 /** creates an instruction with default swizzle/writemask/etc. with NULL registers */
 nir_alu_instr *nir_alu_instr_create(nir_shader *shader, nir_op op);
