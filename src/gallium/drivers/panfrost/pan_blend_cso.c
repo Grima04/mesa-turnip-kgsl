4174a13459da6086963b4b43b4e0dcdd680da6db
Vinson Lee <vlee@freedesktop.org>
2020-05-22 18:34:34 -0700

    panfrost: Ensure final.no_colour is initialized.
    Fix warning reported by Coverity Scan.
    
    Uninitialized scalar variable (UNINIT)
    uninit_use: Using uninitialized value final. Field final.no_colour is
    uninitialized.
    
    Fixes: 3e4e849e6a96 ("panfrost: Disable tib read/write when colourmask = 0x0")
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5183>

--- src/gallium/drivers/panfrost/pan_blend_cso.c
+++ src/gallium/drivers/panfrost/pan_blend_cso.c
@@ -265,6 +265,7 @@ panfrost_get_blend_for_context(struct panfrost_context *ctx, unsigned rti, struc
         struct panfrost_blend_shader *shader = panfrost_get_blend_shader(ctx, blend, fmt, rti);
         final.is_shader = true;
         final.no_blending = false;
+        final.no_colour = false;
         final.shader.work_count = shader->work_count;
         final.shader.first_tag = shader->first_tag;
 
