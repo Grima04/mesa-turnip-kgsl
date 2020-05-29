2260ce6d0c5c47ff22eb59e321fc3458aa737019
Tapani Pälli <tapani.palli@intel.com>
2020-05-29 11:57:07 +0300

    anv: add VK_EXT_extended_dynamic_state but leave it disabled
    This is needed to ensure the function prototypes are declared.
    
    v2: tweak commit message (Jason)
    
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5604>

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -130,6 +130,7 @@ EXTENSIONS = [
     Extension('VK_EXT_direct_mode_display',               1, 'VK_USE_PLATFORM_DISPLAY_KHR'),
     Extension('VK_EXT_display_control',                   1, 'VK_USE_PLATFORM_DISPLAY_KHR'),
     Extension('VK_EXT_display_surface_counter',           1, 'VK_USE_PLATFORM_DISPLAY_KHR'),
+    Extension('VK_EXT_extended_dynamic_state',            1, False),
     Extension('VK_EXT_external_memory_dma_buf',           1, True),
     Extension('VK_EXT_external_memory_host',              1, True),
     Extension('VK_EXT_fragment_shader_interlock',         1, 'device->info.gen >= 9'),
