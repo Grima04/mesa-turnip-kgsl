Grima04/mesa-turnip-kgsl

commit d32690b43c91d4aab938da83704e4ebb68fccf6f

gallivm: add coroutine pass manager support
coroutines require a proper pass manager, so add the passes
to the correct places

Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -44,7 +44,9 @@
 #include <llvm-c/Transforms/Utils.h>
 #endif
 #include <llvm-c/BitWriter.h>
-
+#if GALLIVM_HAVE_CORO
+#include <llvm-c/Transforms/Coroutines.h>
+#endif
 
 /* Only MCJIT is available as of LLVM SVN r216982 */
 #if HAVE_LLVM >= 0x0306
@@ -125,6 +127,10 @@ create_pass_manager(struct gallivm_state *gallivm)
    gallivm->passmgr = LLVMCreateFunctionPassManagerForModule(gallivm->module);
    if (!gallivm->passmgr)
       return FALSE;
+
+#if GALLIVM_HAVE_CORO
+   gallivm->cgpassmgr = LLVMCreatePassManager();
+#endif
    /*
     * TODO: some per module pass manager with IPO passes might be helpful -
     * the generated texture functions may benefit from inlining if they are
@@ -143,6 +149,12 @@ create_pass_manager(struct gallivm_state *gallivm)
       LLVMSetDataLayout(gallivm->module, td_str);
       free(td_str);
    }
+
+#if GALLIVM_HAVE_CORO
+   LLVMAddCoroEarlyPass(gallivm->cgpassmgr);
+   LLVMAddCoroSplitPass(gallivm->cgpassmgr);
+   LLVMAddCoroElidePass(gallivm->cgpassmgr);
+#endif
 
    if ((gallivm_perf & GALLIVM_PERF_NO_OPT) == 0) {
       /*
@@ -170,6 +182,9 @@ create_pass_manager(struct gallivm_state *gallivm)
       LLVMAddConstantPropagationPass(gallivm->passmgr);
       LLVMAddInstructionCombiningPass(gallivm->passmgr);
       LLVMAddGVNPass(gallivm->passmgr);
+#if GALLIVM_HAVE_CORO
+      LLVMAddCoroCleanupPass(gallivm->passmgr);
+#endif
    }
    else {
       /* We need at least this pass to prevent the backends to fail in
@@ -192,6 +207,12 @@ gallivm_free_ir(struct gallivm_state *gallivm)
    if (gallivm->passmgr) {
       LLVMDisposePassManager(gallivm->passmgr);
    }
+
+#if GALLIVM_HAVE_CORO
+   if (gallivm->cgpassmgr) {
+      LLVMDisposePassManager(gallivm->cgpassmgr);
+   }
+#endif
 
    if (gallivm->engine) {
       /* This will already destroy any associated module */
@@ -219,6 +240,7 @@ gallivm_free_ir(struct gallivm_state *gallivm)
    gallivm->target = NULL;
    gallivm->module = NULL;
    gallivm->module_name = NULL;
+   gallivm->cgpassmgr = NULL;
    gallivm->passmgr = NULL;
    gallivm->context = NULL;
    gallivm->builder = NULL;
@@ -610,6 +632,9 @@ gallivm_compile_module(struct gallivm_state *gallivm)
    if (gallivm_debug & GALLIVM_DEBUG_PERF)
       time_begin = os_time_get();
 
+#if GALLIVM_HAVE_CORO
+   LLVMRunPassManager(gallivm->cgpassmgr, gallivm->module);
+#endif
    /* Run optimization passes */
    LLVMInitializeFunctionPassManager(gallivm->passmgr);
    func = LLVMGetFirstFunction(gallivm->module);

src/gallium/auxiliary/gallivm/lp_bld.h
@@ -95,6 +95,11 @@ typedef void *LLVMMCJITMemoryManagerRef;
 #define LLVMInsertBasicBlock ILLEGAL_LLVM_FUNCTION
 #define LLVMCreateBuilder ILLEGAL_LLVM_FUNCTION
 
+#if HAVE_LLVM >= 0x0800
+#define GALLIVM_HAVE_CORO 1
+#else
+#define GALLIVM_HAVE_CORO 0
+#endif
 
 /*
  * Before LLVM 3.4 LLVMSetAlignment only supported GlobalValue, not

src/gallium/auxiliary/gallivm/lp_bld_init.h
@@ -46,6 +46,7 @@ struct gallivm_state
    LLVMExecutionEngineRef engine;
    LLVMTargetDataRef target;
    LLVMPassManagerRef passmgr;
+   LLVMPassManagerRef cgpassmgr;
    LLVMContextRef context;
    LLVMBuilderRef builder;
    LLVMMCJITMemoryManagerRef memorymgr;

commit c8c7450fc73b888504174733e905f4a69a72062a

llvmpipe: move coroutines out of noopt case
the virgl CI code was using the noopt path and crashing with a
wierd can't select llvm.coro.subfn.addr error, turns out we have
to call the cleanup pass no matter what.

This enable a lot more virgl gles31 passes, but we have
to disable tessellation shaders as now they executed, they
crash due to missing OES_gpu_shader5, I should try and reenable
them when llvmpipe is further along

Fixes: d32690b43c91d ("gallivm: add coroutine pass manager support")

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Acked-by: Elie Tournier <elie.tournier@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5320>

.gitlab-ci.yml
@@ -838,6 +838,7 @@ virgl-gles31:
     DEQP_RUNNER_OPTIONS: "--timeout 120"
     MESA_GLES_VERSION_OVERRIDE: "3.1"
     MESA_GLSL_VERSION_OVERRIDE: "310"
+    MESA_EXTENSION_OVERRIDE: "-GL_OES_tessellation_shader"
   extends: virgl-gles2
 
 # Rules for tests that should not be present in MRs or the main

src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -172,9 +172,6 @@ create_pass_manager(struct gallivm_state *gallivm)
       LLVMAddConstantPropagationPass(gallivm->passmgr);
       LLVMAddInstructionCombiningPass(gallivm->passmgr);
       LLVMAddGVNPass(gallivm->passmgr);
-#if GALLIVM_HAVE_CORO
-      LLVMAddCoroCleanupPass(gallivm->passmgr);
-#endif
    }
    else {
       /* We need at least this pass to prevent the backends to fail in
@@ -182,6 +179,9 @@ create_pass_manager(struct gallivm_state *gallivm)
        */
       LLVMAddPromoteMemoryToRegisterPass(gallivm->passmgr);
    }
+#if GALLIVM_HAVE_CORO
+   LLVMAddCoroCleanupPass(gallivm->passmgr);
+#endif
 
    return TRUE;
 }