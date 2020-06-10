96d99f2eccb4bea4b9ede38f9b2e472cdc617e99
Jason Ekstrand <jason@jlekstrand.net>
2020-06-10 17:53:18 -0500

    vc4: Only call nir_lower_io on shader_in/out
    Gallium drivers should never see nir_var_uniform because gallium lowers
    regular uniforms to a UBO.  No GL driver should ever see either
    nir_var_mem_shared because that's lowered in GLSL IR.
    
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Connor Abbott <cwabbott0@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5418>

--- src/gallium/drivers/vc4/vc4_program.c
+++ src/gallium/drivers/vc4/vc4_program.c
@@ -2470,8 +2470,8 @@ vc4_shader_state_create(struct pipe_context *pctx,
         if (s->info.stage == MESA_SHADER_VERTEX)
                 NIR_PASS_V(s, nir_lower_point_size, 1.0f, 0.0f);
 
-        NIR_PASS_V(s, nir_lower_io, nir_var_all, type_size,
-                   (nir_lower_io_options)0);
+        NIR_PASS_V(s, nir_lower_io, nir_var_shader_in | nir_var_shader_out,
+                   type_size, (nir_lower_io_options)0);
 
         NIR_PASS_V(s, nir_lower_regs_to_ssa);
         NIR_PASS_V(s, nir_normalize_cubemap_coords);
