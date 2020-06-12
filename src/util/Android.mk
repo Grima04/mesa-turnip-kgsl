a029eafba360d660dc43f8b44f654ce6af31a2c2
Eric Engestrom <eric@engestrom.ch>
2020-06-12 11:17:20 +0200

    driconf: drop 26% spanish translation
    Only 19 of the 72 strings are translated, and there doesn't seem to be
    any effort to keep it updated.
    
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5440>

--- src/util/Android.mk
+++ src/util/Android.mk
@@ -74,7 +74,7 @@ LOCAL_GENERATED_SOURCES += $(MESA_DRI_OPTIONS_H)
 # Generate options.h from gettext translations.
 #
 
-MESA_DRI_OPTIONS_LANGS := es nl fr sv
+MESA_DRI_OPTIONS_LANGS := nl fr sv
 POT := $(intermediates)/xmlpool.pot
 
 $(POT): $(LOCAL_PATH)/xmlpool/t_options.h
