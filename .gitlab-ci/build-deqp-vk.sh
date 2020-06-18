6ee80d8e0cf678d05ce23d61669199e3f9b39a1c
Eric Anholt <eric@anholt.net>
2020-06-18 12:21:44 -0700

    ci: Bump vulkan CTS to 1.2.3.0.
    Looks like it fixes some potentially important VK test bugs.  But also, it
    fixes the GLES31 SSBO layout tests to not be so excessively large, so we
    can run them in a reasonable time now.  Note that a630 fail list is reset,
    since the test list has changed and so we end up with a different subset
    of tests being run.  Interestingly, in the process the semaphore tests are
    now reporting "NotSupported (Exporting and importing semaphore type not
    supported at vktSynchronizationSignalOrderTests.cpp:513)" where they
    weren't before.
    
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5554>

--- .gitlab-ci/build-deqp-vk.sh
+++ .gitlab-ci/build-deqp-vk.sh
@@ -6,13 +6,10 @@ git config --global user.email "mesa@example.com"
 git config --global user.name "Mesa CI"
 git clone \
     https://github.com/KhronosGroup/VK-GL-CTS.git \
-    -b vulkan-cts-1.2.2.1 \
+    -b vulkan-cts-1.2.3.0 \
+    --depth 1 \
     /VK-GL-CTS
 pushd /VK-GL-CTS
-
-# Cherry pick a fix that's not in 1.2.2.1 yet.  Re-add --depth 1 to the clone
-# when an uprev removes this.
-git cherry-pick -x ea6f1ffae14de94bbd9c354ad5a6c3f452f65ac4
 
 # --insecure is due to SSL cert failures hitting sourceforge for zlib and
 # libpng (sigh).  The archives get their checksums checked anyway, and git
