82caff5bc3612bb7f8498f8c194256276d9bfe7c
Marek Olšák <marek.olsak@amd.com>
2020-07-04 17:43:50 -0400

    glsl: fix evaluating float16 constant expression matrices
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5746>

--- src/compiler/glsl/ir_constant_expression.cpp
+++ src/compiler/glsl/ir_constant_expression.cpp
@@ -935,6 +935,12 @@ ir_dereference_array::constant_expression_value(void *mem_ctx,
          ir_constant_data data = { { 0 } };
 
          switch (column_type->base_type) {
+         case GLSL_TYPE_FLOAT16:
+            for (unsigned i = 0; i < column_type->vector_elements; i++)
+               data.f16[i] = array->value.f16[mat_idx + i];
+
+            break;
+
          case GLSL_TYPE_FLOAT:
             for (unsigned i = 0; i < column_type->vector_elements; i++)
                data.f[i] = array->value.f[mat_idx + i];
