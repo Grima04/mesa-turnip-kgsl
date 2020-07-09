77bb19eebd1d59c7fc48b41c0f356114f5eab2ef
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-07-09 16:22:38 -0400

    panfrost: Abort on unsupported blit
    Instead of silently failing.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5858>

--- src/gallium/drivers/panfrost/pan_blit.c
+++ src/gallium/drivers/panfrost/pan_blit.c
@@ -66,17 +66,9 @@ panfrost_u_blitter_blit(struct pipe_context *pipe,
                         const struct pipe_blit_info *info)
 {
         struct panfrost_context *ctx = pan_context(pipe);
-        struct panfrost_device *dev = pan_device(pipe->screen);
 
-        if (!util_blitter_is_blit_supported(ctx->blitter, info)) {
-                if (dev->debug & PAN_DBG_MSGS) {
-                        fprintf(stderr, "blit unsupported %s -> %s\n",
-                                        util_format_short_name(info->src.resource->format),
-                                        util_format_short_name(info->dst.resource->format));
-                }
-
-                return false;
-        }
+        if (!util_blitter_is_blit_supported(ctx->blitter, info))
+                unreachable("Unsupported blit\n");
 
         /* TODO: Scissor */
 
