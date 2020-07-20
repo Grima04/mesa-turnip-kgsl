4f060549be47863d49ef6c8c6f5ca9016c13fb7b
Rob Clark <robdclark@chromium.org>
2020-07-20 11:14:18 -0700

    freedreno/ir3: lower local_index using local_id
    Somehow this works ok with the full compiler stack, but not in
    ir3_cmdline.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6189>

--- src/freedreno/ir3/ir3_nir.c
+++ src/freedreno/ir3/ir3_nir.c
@@ -71,6 +71,7 @@ static const nir_shader_compiler_options options = {
 		.lower_to_scalar = true,
 		.has_imul24 = true,
 		.lower_wpos_pntc = true,
+		.lower_cs_local_index_from_id = true,
 
 		/* Only needed for the spirv_to_nir() pass done in ir3_cmdline.c
 		 * but that should be harmless for GL since 64b is not
@@ -121,6 +122,7 @@ static const nir_shader_compiler_options options_a6xx = {
 		.has_imul24 = true,
 		.max_unroll_iterations = 32,
 		.lower_wpos_pntc = true,
+		.lower_cs_local_index_from_id = true,
 
 		/* Only needed for the spirv_to_nir() pass done in ir3_cmdline.c
 		 * but that should be harmless for GL since 64b is not
