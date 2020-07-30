6fc52739bb49bd2ac94e909dcfe05caea49aa762
Christian Gmeiner <christian.gmeiner@gmail.com>
2020-07-30 21:25:12 +0200

    etnaviv: fix nir validation problem
    Fixes the following validation problem:
      error: nir_intrinsic_align_offset(instr) < nir_intrinsic_align_mul(instr)
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Tested-by: Lukas F. Hartmann <lukas@mntmn.com>
    Acked-by: Lucas Stach <l.stach@pengutronix.de>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6127>

--- src/gallium/drivers/etnaviv/etnaviv_nir.c
+++ src/gallium/drivers/etnaviv/etnaviv_nir.c
@@ -88,6 +88,7 @@ etna_lower_io(nir_shader *shader, struct etna_shader_variant *v)
                   nir_intrinsic_instr *load_ubo =
                      nir_intrinsic_instr_create(b.shader, nir_intrinsic_load_ubo);
                   load_ubo->num_components = intr->num_components;
+                  nir_intrinsic_set_align(load_ubo, intr->dest.ssa.bit_size / 8, 0);
                   nir_ssa_dest_init(&load_ubo->instr, &load_ubo->dest,
                                     load_ubo->num_components, 32, NULL);
 
