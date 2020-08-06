099ce1aacc3205f0ceeadfc65a12af6778a2f250
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-08-06 15:24:52 -0400

    zink: GLSL 420
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8620>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -198,7 +198,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_GLSL_FEATURE_LEVEL_COMPATIBILITY:
       return 130;
    case PIPE_CAP_GLSL_FEATURE_LEVEL:
-      return 410;
+      return 420;
 
 #if 0 /* TODO: Enable me */
    case PIPE_CAP_COMPUTE:
