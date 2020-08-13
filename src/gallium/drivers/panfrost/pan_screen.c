e03622e50fcebbcc32b2fd403b1a729c73cb49d5
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-08-13 15:19:07 -0400

    panfrost: Set STRIDE_4BYTE_ALIGNED_ONLY
    It isn't clear to me why, but this breaks in some cases (such as some
    dEQP cases where the blob does a translate too).
    
    This reverts commit 9f2997dad061ee38e06f654f524dffbffc5b2a4b.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Cc: mesa-stable
    Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6326>

--- src/gallium/drivers/panfrost/pan_screen.c
+++ src/gallium/drivers/panfrost/pan_screen.c
@@ -191,6 +191,10 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
         case PIPE_CAP_CONDITIONAL_RENDER:
                 return is_gl3;
 
+        /* TODO: Where does this req come from in practice? */
+        case PIPE_CAP_VERTEX_BUFFER_STRIDE_4BYTE_ALIGNED_ONLY:
+                return 1;
+
         case PIPE_CAP_MAX_TEXTURE_2D_SIZE:
                 return 4096;
         case PIPE_CAP_MAX_TEXTURE_3D_LEVELS:
