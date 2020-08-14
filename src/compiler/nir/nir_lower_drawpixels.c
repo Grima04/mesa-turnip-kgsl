844167d10094099e2fdc1fa47ea22f800da305ad
Marek Olšák <marek.olsak@amd.com>
2020-08-14 01:54:19 -0400

    nir: properly identify texcoords for lowered IO in nir_lower_drawpixels
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6442>

--- src/compiler/nir/nir_lower_drawpixels.c
+++ src/compiler/nir/nir_lower_drawpixels.c
@@ -264,16 +264,8 @@ lower_drawpixels_block(lower_drawpixels_state *state, nir_block *block)
 
          case nir_intrinsic_load_interpolated_input:
          case nir_intrinsic_load_input: {
-            /* The intrinsic doesn't carry the variable. We need to find it
-             * manually.
-             */
-            nir_foreach_shader_in_variable(var, state->b.shader) {
-               if ((var->data.driver_location == nir_intrinsic_base(intr)) &&
-                   (nir_intrinsic_component(intr) >= var->data.location_frac  &&
-                    nir_intrinsic_component(intr) <
-                     (var->data.location_frac + glsl_get_components(var->type))))
-                  lower_texcoord(state, intr);
-            }
+            if (nir_intrinsic_io_semantics(intr).location == VARYING_SLOT_TEX0)
+               lower_texcoord(state, intr);
             break;
          }
          default:
