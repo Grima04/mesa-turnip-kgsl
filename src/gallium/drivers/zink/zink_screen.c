3d7d55c2cf4f0b77f4e9e7d9a77025b51ccf8653
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-08-20 08:54:53 -0400

    zink: enable PIPE_CAP_POLYGON_OFFSET_CLAMP
    this is already handled
    
    Acked-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9072>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -182,6 +182,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_TEXTURE_BARRIER:
    case PIPE_CAP_TGSI_VOTE:
    case PIPE_CAP_DRAW_PARAMETERS:
+   case PIPE_CAP_POLYGON_OFFSET_CLAMP:
       return 1;
 
    case PIPE_CAP_ROBUST_BUFFER_ACCESS_BEHAVIOR:
