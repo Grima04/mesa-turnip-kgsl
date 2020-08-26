eb02b311234b71b155496681f294954f3de61823
Eric Anholt <eric@anholt.net>
2020-08-26 16:57:44 -0700

    ci: Bump vulkan CTS version to 1.2.3.2, and keep the GL CTS around.
    The version bump gets us various testcase fixes, mostly to test
    requirements).  While we're rebuilding the container, copy GL CTS stuff
    from build-deqp-gl.sh -- we had already included the glcts binary in our
    image, but we had unnecessary other binaries and were missing the mustpass
    files (container size stays the same overall).  Also pull in all the GLES
    mustpass lists, not just the main ones -- Rob wants them to increase our
    coverage to match what Android CTS covers.
    
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6526>

--- .gitlab-ci/build-deqp-vk.sh
+++ .gitlab-ci/build-deqp-vk.sh
@@ -6,7 +6,7 @@ git config --global user.email "mesa@example.com"
 git config --global user.name "Mesa CI"
 git clone \
     https://github.com/KhronosGroup/VK-GL-CTS.git \
-    -b vulkan-cts-1.2.3.0 \
+    -b vulkan-cts-1.2.3.2 \
     --depth 1 \
     /VK-GL-CTS
 pushd /VK-GL-CTS
@@ -35,11 +35,12 @@ mkdir /deqp/mustpass
 cp /VK-GL-CTS/external/vulkancts/mustpass/master/vk-default.txt \
    /deqp/mustpass/vk-master.txt
 
-for gles in gles2 gles3 gles31; do
-    cp \
-        /deqp/external/openglcts/modules/gl_cts/data/mustpass/gles/aosp_mustpass/3.2.6.x/$gles-master.txt \
-        /deqp/mustpass/$gles-master.txt
-done
+cp \
+    /deqp/external/openglcts/modules/gl_cts/data/mustpass/gles/aosp_mustpass/3.2.6.x/*.txt \
+    /deqp/mustpass/.
+cp \
+    /deqp/external/openglcts/modules/gl_cts/data/mustpass/gl/khronos_mustpass/4.6.1.x/*-master.txt \
+    /deqp/mustpass/.
 
 # Save *some* executor utils, but otherwise strip things down
 # to reduct deqp build size:
@@ -48,12 +49,15 @@ cp /deqp/executor/testlog-to-* /deqp/executor.save
 rm -rf /deqp/executor
 mv /deqp/executor.save /deqp/executor
 
+rm -rf /deqp/external/openglcts/modules/gl_cts/data/mustpass
+rm -rf /deqp/external/openglcts/modules/cts-runner
 rm -rf /deqp/modules/internal
 rm -rf /deqp/execserver
 rm -rf /deqp/modules/egl
 rm -rf /deqp/framework
 find -iname '*cmake*' -o -name '*ninja*' -o -name '*.o' -o -name '*.a' | xargs rm -rf
 ${STRIP_CMD:-strip} external/vulkancts/modules/vulkan/deqp-vk
+${STRIP_CMD:-strip} external/openglcts/modules/glcts
 ${STRIP_CMD:-strip} modules/*/deqp-*
 du -sh *
 rm -rf /VK-GL-CTS
