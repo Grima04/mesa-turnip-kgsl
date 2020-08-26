Grima04/mesa-turnip-kgsl

commit e5cc66dfad0a811338ef088b9b4be17cadc01dea

etnaviv: Rework locking
Replace the per-screen locking of flushing with per-context one and
add per-context lock around command stream buffer accesses, to prevent
cross-context flushing from corrupting these command stream buffers.

Signed-off-by: Marek Vasut <marex@denx.de>

src/gallium/drivers/etnaviv/etnaviv_context.c
@@ -63,7 +63,7 @@ etna_context_destroy(struct pipe_context *pctx)
    struct etna_context *ctx = etna_context(pctx);
    struct etna_screen *screen = ctx->screen;
 
-   mtx_lock(&screen->lock);
+   mtx_lock(&ctx->lock);
    if (ctx->used_resources_read) {
 
       /*
@@ -94,7 +94,7 @@ etna_context_destroy(struct pipe_context *pctx)
       _mesa_set_destroy(ctx->used_resources_write, NULL);
 
    }
-   mtx_unlock(&screen->lock);
+   mtx_unlock(&ctx->lock);
 
    if (ctx->dummy_rt)
       etna_bo_del(ctx->dummy_rt);
@@ -117,6 +117,8 @@ etna_context_destroy(struct pipe_context *pctx)
 
    if (ctx->in_fence_fd != -1)
       close(ctx->in_fence_fd);
+
+   mtx_destroy(&ctx->lock);
 
    FREE(pctx);
 }
@@ -265,6 +267,8 @@ etna_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info)
    if (!etna_state_update(ctx))
       return;
 
+   mtx_lock(&ctx->lock);
+
    /*
     * Figure out the buffers/features we need:
     */
@@ -339,6 +343,7 @@ etna_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info)
        * draw op has caused the hang. */
       etna_stall(ctx->stream, SYNC_RECIPIENT_FE, SYNC_RECIPIENT_PE);
    }
+   mtx_unlock(&ctx->lock);
 
    if (DBG_ENABLED(ETNA_DBG_FLUSH_ALL))
       pctx->flush(pctx, NULL, 0);
@@ -414,7 +419,7 @@ etna_flush(struct pipe_context *pctx, struct pipe_fence_handle **fence,
    struct etna_screen *screen = ctx->screen;
    int out_fence_fd = -1;
 
-   mtx_lock(&screen->lock);
+   mtx_lock(&ctx->lock);
 
    list_for_each_entry(struct etna_hw_query, hq, &ctx->active_hw_queries, node)
       etna_hw_query_suspend(hq, ctx);
@@ -452,9 +457,8 @@ etna_flush(struct pipe_context *pctx, struct pipe_fence_handle **fence,
    }
    _mesa_set_clear(ctx->used_resources_write, NULL);
 
-   mtx_unlock(&screen->lock);
-
    etna_reset_gpu_state(ctx);
+   mtx_unlock(&ctx->lock);
 }
 
 static void
@@ -511,6 +515,8 @@ etna_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
                                           _mesa_key_pointer_equal);
    if (!ctx->used_resources_write)
       goto fail;
+
+   mtx_init(&ctx->lock, mtx_recursive);
 
    /* context ctxate setup */
    ctx->specs = screen->specs;

src/gallium/drivers/etnaviv/etnaviv_resource.c
@@ -645,7 +645,7 @@ etna_resource_used(struct etna_context *ctx, struct pipe_resource *prsc,
    if (!prsc)
       return;
 
-   mtx_lock(&screen->lock);
+   mtx_lock(&ctx->lock);
 
    rsc = etna_resource(prsc);
 
@@ -682,7 +682,7 @@ etna_resource_used(struct etna_context *ctx, struct pipe_resource *prsc,
       _mesa_set_add(rsc->pending_ctx, ctx);
    }
 
-   mtx_unlock(&screen->lock);
+   mtx_unlock(&ctx->lock);
 }
 
 bool

src/gallium/drivers/etnaviv/etnaviv_transfer.c
@@ -355,7 +355,7 @@ etna_transfer_map(struct pipe_context *pctx, struct pipe_resource *prsc,
        * current GPU usage (reads must wait for GPU writes, writes must have
        * exclusive access to the buffer).
        */
-      mtx_lock(&screen->lock);
+      mtx_lock(&ctx->lock);
 
       if ((trans->rsc && (etna_resource(trans->rsc)->status & ETNA_PENDING_WRITE)) ||
           (!trans->rsc &&
@@ -369,7 +369,7 @@ etna_transfer_map(struct pipe_context *pctx, struct pipe_resource *prsc,
          }
       }
 
-      mtx_unlock(&screen->lock);
+      mtx_unlock(&ctx->lock);
 
       if (usage & PIPE_TRANSFER_READ)
          prep_flags |= DRM_ETNA_PREP_READ;

src/gallium/drivers/etnaviv/etnaviv_blt.c
@@ -324,6 +324,7 @@ etna_clear_blt(struct pipe_context *pctx, unsigned buffers,
            const union pipe_color_union *color, double depth, unsigned stencil)
 {
    struct etna_context *ctx = etna_context(pctx);
+   mtx_lock(&ctx->lock);
 
    etna_set_state(ctx->stream, VIVS_GL_FLUSH_CACHE, 0x00000c23);
    etna_set_state(ctx->stream, VIVS_TS_FLUSH_CACHE, VIVS_TS_FLUSH_CACHE_FLUSH);
@@ -344,8 +345,8 @@ etna_clear_blt(struct pipe_context *pctx, unsigned buffers,
       etna_set_state(ctx->stream, VIVS_GL_FLUSH_CACHE, 0x00000c23);
    else
       etna_set_state(ctx->stream, VIVS_GL_FLUSH_CACHE, 0x00000002);
+   mtx_unlock(&ctx->lock);
 }
-
 
 static bool
 etna_try_blt_blit(struct pipe_context *pctx,
@@ -416,6 +417,7 @@ etna_try_blt_blit(struct pipe_context *pctx,
          return true;
    }
 
+   mtx_lock(&ctx->lock);
    /* Kick off BLT here */
    if (src == dst && src_lev->ts_compress_fmt < 0) {
       /* Resolve-in-place */
@@ -510,6 +512,7 @@ etna_try_blt_blit(struct pipe_context *pctx,
 
    dst->seqno++;
    dst_lev->ts_valid = false;
+   mtx_unlock(&ctx->lock);
 
    return true;
 }

src/gallium/drivers/etnaviv/etnaviv_context.h
@@ -194,6 +194,8 @@ struct etna_context {
    /* set of resources used by currently-unsubmitted renders */
    struct set *used_resources_read;
    struct set *used_resources_write;
+
+   mtx_t lock;
 };
 
 static inline struct etna_context *

src/gallium/drivers/etnaviv/etnaviv_rs.c
@@ -392,6 +392,7 @@ etna_clear_rs(struct pipe_context *pctx, unsigned buffers,
            const union pipe_color_union *color, double depth, unsigned stencil)
 {
    struct etna_context *ctx = etna_context(pctx);
+   mtx_lock(&ctx->lock);
 
    /* Flush color and depth cache before clearing anything.
     * This is especially important when coming from another surface, as
@@ -437,6 +438,7 @@ etna_clear_rs(struct pipe_context *pctx, unsigned buffers,
       etna_blit_clear_zs_rs(pctx, ctx->framebuffer_s.zsbuf, buffers, depth, stencil);
 
    etna_stall(ctx->stream, SYNC_RECIPIENT_RA, SYNC_RECIPIENT_PE);
+   mtx_unlock(&ctx->lock);
 }
 
 static bool
@@ -647,6 +649,8 @@ etna_try_rs_blit(struct pipe_context *pctx,
        width & (w_align - 1) || height & (h_align - 1))
       goto manual;
 
+   mtx_lock(&ctx->lock);
+
    /* Always flush color and depth cache together before resolving. This works
     * around artifacts that appear in some cases when scanning out a texture
     * directly after it has been rendered to, such as rendering an animated web
@@ -736,6 +740,7 @@ etna_try_rs_blit(struct pipe_context *pctx,
    dst->seqno++;
    dst_lev->ts_valid = false;
    ctx->dirty |= ETNA_DIRTY_DERIVE_TS;
+   mtx_unlock(&ctx->lock);
 
    return true;
 

src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -84,8 +84,6 @@ etna_screen_destroy(struct pipe_screen *pscreen)
 {
    struct etna_screen *screen = etna_screen(pscreen);
 
-   mtx_destroy(&screen->lock);
-
    if (screen->perfmon)
       etna_perfmon_del(screen->perfmon);
 
@@ -955,8 +953,6 @@ etna_screen_create(struct etna_device *dev, struct etna_gpu *gpu,
 
    if (screen->drm_version >= ETNA_DRM_VERSION_PERFMON)
       etna_pm_query_setup(screen);
-
-   mtx_init(&screen->lock, mtx_recursive);
 
    return pscreen;
 

src/gallium/drivers/etnaviv/etnaviv_screen.h
@@ -85,8 +85,6 @@ struct etna_screen {
 
    uint32_t drm_version;
 
-   mtx_t lock;
-
    nir_shader_compiler_options options;
 };
 

commit 60975ebe58ddb05beebbdd3850dc9d866aee5420

etnaviv: Add lock around pending_ctx
The content of rsc->pending_ctx could be changed from multiple contexts
and thus from multiple threads. The per-context lock is not sufficient
to protect this list. Add per-resource lock to protect this list.

Fixes: e5cc66dfad0 ("etnaviv: Rework locking")
Signed-off-by: Marek Vasut <marex@denx.de>
Cc: <mesa-stable@lists.freedesktop.org>
Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6454>

src/gallium/drivers/etnaviv/etnaviv_context.c
@@ -93,6 +93,7 @@ etna_context_destroy(struct pipe_context *pctx)
    struct etna_context *ctx = etna_context(pctx);
 
    mtx_lock(&ctx->lock);
+
    if (ctx->used_resources_read) {
 
       /*
@@ -103,7 +104,9 @@ etna_context_destroy(struct pipe_context *pctx)
       set_foreach(ctx->used_resources_read, entry) {
          struct etna_resource *rsc = (struct etna_resource *)entry->key;
 
+         mtx_lock(&rsc->lock);
          _mesa_set_remove_key(rsc->pending_ctx, ctx);
+         mtx_unlock(&rsc->lock);
       }
       _mesa_set_destroy(ctx->used_resources_read, NULL);
 
@@ -118,7 +121,9 @@ etna_context_destroy(struct pipe_context *pctx)
       set_foreach(ctx->used_resources_write, entry) {
          struct etna_resource *rsc = (struct etna_resource *)entry->key;
 
+         mtx_lock(&rsc->lock);
          _mesa_set_remove_key(rsc->pending_ctx, ctx);
+         mtx_unlock(&rsc->lock);
       }
       _mesa_set_destroy(ctx->used_resources_write, NULL);
 
@@ -484,11 +489,15 @@ etna_flush(struct pipe_context *pctx, struct pipe_fence_handle **fence,
       struct etna_resource *rsc = (struct etna_resource *)entry->key;
       struct pipe_resource *referenced = &rsc->base;
 
+      mtx_lock(&rsc->lock);
+
       _mesa_set_remove_key(rsc->pending_ctx, ctx);
 
       /* if resource has no pending ctx's reset its status */
       if (_mesa_set_next_entry(rsc->pending_ctx, NULL) == NULL)
          rsc->status &= ~ETNA_PENDING_READ;
+
+      mtx_unlock(&rsc->lock);
 
       pipe_resource_reference(&referenced, NULL);
    }
@@ -498,11 +507,13 @@ etna_flush(struct pipe_context *pctx, struct pipe_fence_handle **fence,
       struct etna_resource *rsc = (struct etna_resource *)entry->key;
       struct pipe_resource *referenced = &rsc->base;
 
+      mtx_lock(&rsc->lock);
       _mesa_set_remove_key(rsc->pending_ctx, ctx);
 
       /* if resource has no pending ctx's reset its status */
       if (_mesa_set_next_entry(rsc->pending_ctx, NULL) == NULL)
          rsc->status &= ~ETNA_PENDING_WRITE;
+      mtx_unlock(&rsc->lock);
 
       pipe_resource_reference(&referenced, NULL);
    }

src/gallium/drivers/etnaviv/etnaviv_resource.c
@@ -321,6 +321,7 @@ etna_resource_alloc(struct pipe_screen *pscreen, unsigned layout,
       memset(map, 0, size);
    }
 
+   mtx_init(&rsc->lock, mtx_recursive);
    rsc->pending_ctx = _mesa_set_create(NULL, _mesa_hash_pointer,
                                        _mesa_key_pointer_equal);
    if (!rsc->pending_ctx)
@@ -463,8 +464,10 @@ etna_resource_destroy(struct pipe_screen *pscreen, struct pipe_resource *prsc)
 {
    struct etna_resource *rsc = etna_resource(prsc);
 
+   mtx_lock(&rsc->lock);
    assert(!_mesa_set_next_entry(rsc->pending_ctx, NULL));
    _mesa_set_destroy(rsc->pending_ctx, NULL);
+   mtx_unlock(&rsc->lock);
 
    if (rsc->bo)
       etna_bo_del(rsc->bo);
@@ -482,6 +485,8 @@ etna_resource_destroy(struct pipe_screen *pscreen, struct pipe_resource *prsc)
 
    for (unsigned i = 0; i < ETNA_NUM_LOD; i++)
       FREE(rsc->levels[i].patch_offsets);
+
+   mtx_destroy(&rsc->lock);
 
    FREE(rsc);
 }
@@ -559,6 +564,7 @@ etna_resource_from_handle(struct pipe_screen *pscreen,
       goto fail;
    }
 
+   mtx_init(&rsc->lock, mtx_recursive);
    rsc->pending_ctx = _mesa_set_create(NULL, _mesa_hash_pointer,
                                        _mesa_key_pointer_equal);
    if (!rsc->pending_ctx)
@@ -616,18 +622,41 @@ etna_resource_used(struct etna_context *ctx, struct pipe_resource *prsc,
    mtx_lock(&ctx->lock);
 
    rsc = etna_resource(prsc);
+again:
+   mtx_lock(&rsc->lock);
 
    set_foreach(rsc->pending_ctx, entry) {
       struct etna_context *extctx = (struct etna_context *)entry->key;
       struct pipe_context *pctx = &extctx->base;
+      bool need_flush = false;
+
+      if (mtx_trylock(&extctx->lock) != thrd_success) {
+         /*
+	  * The other context could be locked in etna_flush() and
+	  * stuck waiting for the resource lock, so release the
+	  * resource lock here, let etna_flush() finish, and try
+	  * again.
+	  */
+         mtx_unlock(&rsc->lock);
+         thrd_yield();
+         goto again;
+      }
 
       set_foreach(extctx->used_resources_read, entry2) {
          struct etna_resource *rsc2 = (struct etna_resource *)entry2->key;
          if (ctx == extctx || rsc2 != rsc)
             continue;
 
-         if (status & ETNA_PENDING_WRITE)
-            pctx->flush(pctx, NULL, 0);
+         if (status & ETNA_PENDING_WRITE) {
+            need_flush = true;
+            break;
+         }
+      }
+
+      if (need_flush) {
+         pctx->flush(pctx, NULL, 0);
+         mtx_unlock(&extctx->lock);
+	 continue;
       }
 
       set_foreach(extctx->used_resources_write, entry2) {
@@ -635,8 +664,14 @@ etna_resource_used(struct etna_context *ctx, struct pipe_resource *prsc,
          if (ctx == extctx || rsc2 != rsc)
             continue;
 
-         pctx->flush(pctx, NULL, 0);
+         need_flush = true;
+         break;
       }
+
+      if (need_flush)
+         pctx->flush(pctx, NULL, 0);
+
+      mtx_unlock(&extctx->lock);
    }
 
    rsc->status = status;
@@ -648,6 +683,7 @@ etna_resource_used(struct etna_context *ctx, struct pipe_resource *prsc,
       _mesa_set_add(rsc->pending_ctx, ctx);
    }
 
+   mtx_unlock(&rsc->lock);
    mtx_unlock(&ctx->lock);
 }
 

src/gallium/drivers/etnaviv/etnaviv_resource.h
@@ -96,6 +96,7 @@ struct etna_resource {
 
    enum etna_resource_status status;
 
+   mtx_t lock; /* Lock to protect pending_ctx */
    struct set *pending_ctx;
 };
 

src/gallium/drivers/etnaviv/etnaviv_transfer.c
@@ -391,12 +391,14 @@ etna_transfer_map(struct pipe_context *pctx, struct pipe_resource *prsc,
           (!trans->rsc &&
            (((usage & PIPE_TRANSFER_READ) && (rsc->status & ETNA_PENDING_WRITE)) ||
            ((usage & PIPE_TRANSFER_WRITE) && rsc->status)))) {
+         mtx_lock(&rsc->lock);
          set_foreach(rsc->pending_ctx, entry) {
             struct etna_context *pend_ctx = (struct etna_context *)entry->key;
             struct pipe_context *pend_pctx = &pend_ctx->base;
 
             pend_pctx->flush(pend_pctx, NULL, 0);
          }
+         mtx_unlock(&rsc->lock);
       }
 
       mtx_unlock(&ctx->lock);