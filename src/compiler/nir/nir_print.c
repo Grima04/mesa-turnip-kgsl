Grima04/mesa-turnip-kgsl

commit 2b1ef5df4eacae2a1c4028655c8b1f35412e0a3b

nir: print IO semantics (v2)
v2: print GS streams readably

Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6442>

src/compiler/nir/nir_print.c
@@ -808,7 +808,9 @@ print_intrinsic_instr(nir_intrinsic_instr *instr, print_state *state)
       [NIR_INTRINSIC_MEMORY_MODES] = "mem_modes",
       [NIR_INTRINSIC_MEMORY_SCOPE] = "mem_scope",
       [NIR_INTRINSIC_EXECUTION_SCOPE] = "exec_scope",
+      [NIR_INTRINSIC_IO_SEMANTICS] = "io_semantics",
    };
+
    for (unsigned idx = 1; idx < NIR_INTRINSIC_NUM_INDEX_FLAGS; idx++) {
       if (!info->index_map[idx])
          continue;
@@ -922,6 +924,32 @@ print_intrinsic_instr(nir_intrinsic_instr *instr, print_state *state)
          }
          break;
       }
+
+      case NIR_INTRINSIC_IO_SEMANTICS:
+         fprintf(fp, " location=%u slots=%u",
+                 nir_intrinsic_io_semantics(instr).location,
+                 nir_intrinsic_io_semantics(instr).num_slots);
+         if (state->shader->info.stage == MESA_SHADER_FRAGMENT &&
+             instr->intrinsic == nir_intrinsic_store_output &&
+             nir_intrinsic_io_semantics(instr).dual_source_blend_index) {
+            fprintf(fp, " dualsrc=1");
+         }
+         if (state->shader->info.stage == MESA_SHADER_FRAGMENT &&
+             instr->intrinsic == nir_intrinsic_load_output &&
+             nir_intrinsic_io_semantics(instr).fb_fetch_output) {
+            fprintf(fp, " fbfetch=1");
+         }
+         if (state->shader->info.stage == MESA_SHADER_GEOMETRY &&
+             instr->intrinsic == nir_intrinsic_store_output) {
+            unsigned gs_streams = nir_intrinsic_io_semantics(instr).gs_streams;
+            fprintf(fp, " gs_streams(");
+            for (unsigned i = 0; i < 4; i++) {
+               fprintf(fp, "%s%c=%u", i ? " " : "", "xyzw"[i],
+                       (gs_streams >> (i * 2)) & 0x3);
+            }
+            fprintf(fp, ")");
+         }
+         break;
 
       default: {
          unsigned off = info->index_map[idx] - 1;

commit a3a8322dcd7aaede8dedff131c7d73bdbe3f06f9

nir: Fix printing of individual instructions with io semantics.
The state->shader is missing when used outside of nir_print_shader, just
drop these details in that case.  We can fix nir_print_instr() to look up
the shader, but let's also make sure that an instr detached from a shader
(such as one you're constructing but haven't yet inserted) still works.

Fixes: 2b1ef5df4eac ("nir: print IO semantics (v2)")
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6496>

src/compiler/nir/nir_print.c
@@ -929,25 +929,27 @@ print_intrinsic_instr(nir_intrinsic_instr *instr, print_state *state)
          fprintf(fp, " location=%u slots=%u",
                  nir_intrinsic_io_semantics(instr).location,
                  nir_intrinsic_io_semantics(instr).num_slots);
-         if (state->shader->info.stage == MESA_SHADER_FRAGMENT &&
-             instr->intrinsic == nir_intrinsic_store_output &&
-             nir_intrinsic_io_semantics(instr).dual_source_blend_index) {
-            fprintf(fp, " dualsrc=1");
-         }
-         if (state->shader->info.stage == MESA_SHADER_FRAGMENT &&
-             instr->intrinsic == nir_intrinsic_load_output &&
-             nir_intrinsic_io_semantics(instr).fb_fetch_output) {
-            fprintf(fp, " fbfetch=1");
-         }
-         if (state->shader->info.stage == MESA_SHADER_GEOMETRY &&
-             instr->intrinsic == nir_intrinsic_store_output) {
-            unsigned gs_streams = nir_intrinsic_io_semantics(instr).gs_streams;
-            fprintf(fp, " gs_streams(");
-            for (unsigned i = 0; i < 4; i++) {
-               fprintf(fp, "%s%c=%u", i ? " " : "", "xyzw"[i],
-                       (gs_streams >> (i * 2)) & 0x3);
+         if (state->shader) {
+            if (state->shader->info.stage == MESA_SHADER_FRAGMENT &&
+                instr->intrinsic == nir_intrinsic_store_output &&
+                nir_intrinsic_io_semantics(instr).dual_source_blend_index) {
+               fprintf(fp, " dualsrc=1");
             }
-            fprintf(fp, ")");
+            if (state->shader->info.stage == MESA_SHADER_FRAGMENT &&
+                instr->intrinsic == nir_intrinsic_load_output &&
+                nir_intrinsic_io_semantics(instr).fb_fetch_output) {
+               fprintf(fp, " fbfetch=1");
+            }
+            if (state->shader->info.stage == MESA_SHADER_GEOMETRY &&
+                instr->intrinsic == nir_intrinsic_store_output) {
+               unsigned gs_streams = nir_intrinsic_io_semantics(instr).gs_streams;
+               fprintf(fp, " gs_streams(");
+               for (unsigned i = 0; i < 4; i++) {
+                  fprintf(fp, "%s%c=%u", i ? " " : "", "xyzw"[i],
+                          (gs_streams >> (i * 2)) & 0x3);
+               }
+               fprintf(fp, ")");
+            }
          }
          break;
 