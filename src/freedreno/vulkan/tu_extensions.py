713386af2037254d6dbd49c56dfb8399d5870c90
Eduardo Lima Mitev <elima@igalia.com>
2020-09-13 16:56:13 +0200

    turnip: Enable support for KHR_incremental_present
    All bits should already be provided by wsi/common.
    
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6704>

--- src/freedreno/vulkan/tu_extensions.py
+++ src/freedreno/vulkan/tu_extensions.py
@@ -98,6 +98,7 @@ EXTENSIONS = [
     Extension('VK_EXT_shader_viewport_index_layer',       1, True),
     Extension('VK_EXT_extended_dynamic_state',            1, True),
     Extension('VK_KHR_push_descriptor',                   1, True),
+    Extension('VK_KHR_incremental_present',               1, 'TU_HAS_SURFACE'),
 ]
 
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
