bd60e31c837b9c8ed48f6b4db0b93b14ab46135e
Eric Anholt <eric@anholt.net>
2020-09-14 13:13:47 -0700

    freedreno/ir3: Make sure we run the opt loop after lowering UBOs to vec4.
    The lowering pass may introduce vector bcsels that we need to scalarize
    back out.  It's unusual to have UBOs and not get any lowered to push
    constants, so the flag was usually set anyway.
    
    Fixes: 2b2524099379 ("freedreno/ir3: Replace our custom vec4 UBO intrinsic
    with the shared lowering.")
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6612>

--- src/freedreno/ir3/ir3_nir.c
+++ src/freedreno/ir3/ir3_nir.c
@@ -471,7 +471,7 @@ ir3_nir_lower_variant(struct ir3_shader_variant *so, nir_shader *s)
 	 * be left as load_ubo
 	 */
 	if (so->shader->compiler->gpu_id >= 600)
-		OPT_V(s, nir_lower_ubo_vec4);
+		progress |= OPT(s, nir_lower_ubo_vec4);
 
 	OPT_V(s, ir3_nir_lower_io_offsets, so->shader->compiler->gpu_id);
 
