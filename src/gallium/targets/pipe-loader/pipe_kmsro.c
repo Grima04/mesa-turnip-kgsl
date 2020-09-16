9ec28b8d226558aa2748dbf813b125f8938a09cc
Eric Anholt <eric@anholt.net>
2020-09-16 08:54:12 -0700

    gallium/drm: Deduplicate screen creation for the dynamic (clover) pipe loader.
    We can just reuse drm_helper.h, which has either the real code or the stub
    for all pipe_screens based on the GALLIUM_* driver defines, and the
    dynamic pipe loader's .c build will only define one GALLIUM_* driver
    define.  The remaining stubs should get GCed by the linker.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6751>

--- src/gallium/targets/pipe-loader/pipe_kmsro.c
+++ src/gallium/targets/pipe-loader/pipe_kmsro.c
@@ -3,19 +3,5 @@
 #include "frontend/drm_driver.h"
 #include "kmsro/drm/kmsro_drm_public.h"
 
-static struct pipe_screen *
-create_screen(int fd, const struct pipe_screen_config *config)
-{
-   struct pipe_screen *screen;
-
-   screen = kmsro_drm_screen_create(fd, config);
-   if (!screen)
-      return NULL;
-
-   screen = debug_screen_wrap(screen);
-
-   return screen;
-}
-
 PUBLIC
-DRM_DRIVER_DESCRIPTOR("kmsro", NULL, create_screen)
+DRM_DRIVER_DESCRIPTOR("kmsro", NULL, pipe_kmsro_create_screen)
