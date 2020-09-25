155b1b1792183e6a4a35fc81927a447995c1fecb
Marek Olšák <marek.olsak@amd.com>
2020-09-25 15:15:53 -0400

    st/mesa: enable GL name reuse for queries based on the driconf option
    Fixes: a56849ddda6
    
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7031>

--- src/mesa/state_tracker/st_context.c
+++ src/mesa/state_tracker/st_context.c
@@ -825,6 +825,13 @@ st_create_context_priv(struct gl_context *ctx, struct pipe_context *pipe,
       _mesa_HashEnableNameReuse(ctx->Shared->MemoryObjects);
       _mesa_HashEnableNameReuse(ctx->Shared->SemaphoreObjects);
    }
+   /* SPECviewperf13/sw-04 crashes since a56849ddda6 if Mesa is build with
+    * -O3 on gcc 7.5, which doesn't happen with ForceGLNamesReuse, which is
+    * the default setting for SPECviewperf because it simulates glGen behavior
+    * of closed source drivers.
+    */
+   if (ctx->Const.ForceGLNamesReuse)
+      _mesa_HashEnableNameReuse(ctx->Query.QueryObjects);
 
    _mesa_override_extensions(ctx);
    _mesa_compute_version(ctx);
