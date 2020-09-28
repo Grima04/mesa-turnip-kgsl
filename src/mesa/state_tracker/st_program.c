1126b5cd2f14776b1554591e5cc238e05882b5ef
Marek Olšák <marek.olsak@amd.com>
2020-09-28 13:12:14 -0400

    Revert "st/mesa: don't pass NIR to draw module if IO is lowered"
    This reverts commit 493fdcf44647ee471e934de3e63d193c6a3b6ff0.
    
    The lowered IO support has been added to Draw recently:
        https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6698
    
    Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6950>

--- src/mesa/state_tracker/st_program.c
+++ src/mesa/state_tracker/st_program.c
@@ -534,8 +534,7 @@ st_translate_vertex_program(struct st_context *st,
          /* For st_draw_feedback, we need to generate TGSI too if draw doesn't
           * use LLVM.
           */
-         /* TODO: Draw can't handle lowered IO. */
-         if (draw_has_llvm() && !stp->Base.info.io_lowered) {
+         if (draw_has_llvm()) {
             st_prepare_vertex_program(stp);
             return true;
          }
@@ -715,9 +714,7 @@ st_create_vp_variant(struct st_context *st,
    state.stream_output = stvp->state.stream_output;
 
    if (stvp->state.type == PIPE_SHADER_IR_NIR &&
-       (!key->is_draw_shader ||
-        /* TODO: Draw can't handle lowered IO. */
-        (draw_has_llvm() && !stvp->Base.info.io_lowered))) {
+       (!key->is_draw_shader || draw_has_llvm())) {
       bool finalize = false;
 
       state.type = PIPE_SHADER_IR_NIR;
