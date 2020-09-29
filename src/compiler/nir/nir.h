2f5d18403a4d51a2cd927c141884361850bad41d
Eric Anholt <eric@anholt.net>
2020-09-29 15:44:57 -0700

    nir: Replace nir_ssa_def->live_index with nir_instr->index.
    live_index had two things going on: 0 meant the instr was an undef and
    always dead, and otherwise ssa defs had increasing numbers by instruction
    order.  We already have a field in the instruction for storing instruction
    order, and ssa defs don't need that number to be contiguous (if you want a
    compact per-ssa-def number, use ssa->index after reindexing).
    
    We don't use ssa->index for this, because reindexing those would change
    nir_print, and that would be rude to people trying to track what's
    happening in optimization passes.
    
    This openend up a hole in nir_ssa_def, so we move nir_ssa_def->index
    toward the end to shrink the struct from 64 bytes to 56.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3395>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -786,12 +786,6 @@ typedef struct nir_ssa_def {
    /** for debugging only, can be NULL */
    const char* name;
 
-   /** generic SSA definition index. */
-   unsigned index;
-
-   /** Ordered SSA definition index used by nir_liveness. */
-   unsigned live_index;
-
    /** Instruction which produces this SSA value. */
    nir_instr *parent_instr;
 
@@ -800,6 +794,9 @@ typedef struct nir_ssa_def {
 
    /** set of nir_ifs where this register is used as a condition */
    struct list_head if_uses;
+
+   /** generic SSA definition index. */
+   unsigned index;
 
    uint8_t num_components;
 
@@ -2841,7 +2838,6 @@ typedef enum {
     *
     * This includes:
     *
-    *   - nir_ssa_def::live_index
     *   - nir_block::live_in
     *   - nir_block::live_out
     *
