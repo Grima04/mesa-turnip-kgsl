Grima04/mesa-turnip-kgsl

commit 73c5f451916de2d14b304d762b65d5db354b6cfa

r600/sfn: Fix interpolate at sample
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6879>

src/gallium/drivers/r600/sfn/sfn_shader_fragment.cpp
@@ -48,6 +48,7 @@ FragmentShaderFromNir::FragmentShaderFromNir(const nir_shader& nir,
    m_front_face_loaded(false),
    m_depth_exports(0),
    m_enable_centroid_interpolators(false),
+   m_enable_sample_interpolators(false),
    m_apply_sample_mask(key.ps.apply_sample_id_mask),
    m_dual_source_blend(key.ps.dual_source_blend)
 {
@@ -132,6 +133,9 @@ bool FragmentShaderFromNir::scan_sysvalue_access(nir_instr *instr)
          /* This is not a sysvalue, should go elsewhere */
          m_enable_centroid_interpolators = true;
          break;
+      case nir_intrinsic_interp_deref_at_sample:
+         m_enable_sample_interpolators = true;
+         break;
       default:
          ;
       }
@@ -161,6 +165,11 @@ bool FragmentShaderFromNir::do_allocate_reserved_registers()
    if (m_enable_centroid_interpolators) {
       m_interpolator[2].enabled = true; /* perspective */
       m_interpolator[5].enabled = true; /* linear */
+   }
+
+   if (m_enable_sample_interpolators) {
+      m_interpolator[0].enabled = true; /* perspective */
+      m_interpolator[3].enabled = true; /* linear */
    }
 
    // sort the varying inputs
@@ -439,16 +448,29 @@ bool FragmentShaderFromNir::emit_interp_deref_at_sample(nir_intrinsic_instr* ins
    assert(var);
 
    auto& io = m_shaderio.input(var->data.driver_location, var->data.location_frac);
-   auto interpolator = m_interpolator[io.ij_index()];
-   PValue dummy(new GPRValue(interpolator.i->sel(), 7));
+
+   int ij_index = io.ij_index() >= 3 ? 3 : 0;
+   auto interpolator = m_interpolator[ij_index];
+   assert(interpolator.enabled);
+   PValue dummy(new GPRValue(interpolator.i->sel(), 0));
 
    GPRVector src({interpolator.j, interpolator.i, dummy, dummy});
 
    auto tex = new TexInstruction(TexInstruction::get_gradient_h, grad, src, 0, 0, PValue());
+   tex->set_flag(TexInstruction::grad_fine);
+   tex->set_flag(TexInstruction::x_unnormalized);
+   tex->set_flag(TexInstruction::y_unnormalized);
+   tex->set_flag(TexInstruction::z_unnormalized);
+   tex->set_flag(TexInstruction::w_unnormalized);
    tex->set_dest_swizzle({0,1,7,7});
    emit_instruction(tex);
 
    tex = new TexInstruction(TexInstruction::get_gradient_v, grad, src, 0, 0, PValue());
+   tex->set_flag(TexInstruction::x_unnormalized);
+   tex->set_flag(TexInstruction::y_unnormalized);
+   tex->set_flag(TexInstruction::z_unnormalized);
+   tex->set_flag(TexInstruction::w_unnormalized);
+   tex->set_flag(TexInstruction::grad_fine);
    tex->set_dest_swizzle({7,7,0,1});
    emit_instruction(tex);
 

src/gallium/drivers/r600/sfn/sfn_shader_fragment.h
@@ -96,6 +96,7 @@ private:
 
    std::map<unsigned, PValue> m_input_cache;
    bool m_enable_centroid_interpolators;
+   bool m_enable_sample_interpolators;
    unsigned m_apply_sample_mask;
    bool m_dual_source_blend;
 };

commit 7683e2756195c01e8786323d9b9329726d0510b2

r600/sfn: Fix enabling the right interpolator for inerpolate_at_sample
Fixes: 73c5f451916
   r600/sfn: Fix interpolate at sample

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6925>

src/gallium/drivers/r600/sfn/sfn_shader_fragment.cpp
@@ -170,10 +170,8 @@ bool FragmentShaderFromNir::do_allocate_reserved_registers()
       m_interpolator[5].enabled = true; /* linear */
    }
 
-   if (m_enable_sample_interpolators) {
-      m_interpolator[0].enabled = true; /* perspective */
-      m_interpolator[3].enabled = true; /* linear */
-   }
+   if (m_enable_sample_interpolators)
+      m_interpolator[1].enabled = true; /* perspective */
 
    // sort the varying inputs
    m_shaderio.sort_varying_inputs();
@@ -467,8 +465,7 @@ bool FragmentShaderFromNir::emit_interp_deref_at_sample(nir_intrinsic_instr* ins
 
    auto& io = m_shaderio.input(var->data.driver_location, var->data.location_frac);
 
-   int ij_index = io.ij_index() >= 3 ? 3 : 0;
-   auto interpolator = m_interpolator[ij_index];
+   auto interpolator = m_interpolator[1];
    assert(interpolator.enabled);
    PValue dummy(new GPRValue(interpolator.i->sel(), 0));
 
@@ -607,7 +604,9 @@ bool FragmentShaderFromNir::do_emit_load_deref(const nir_variable *in_var, nir_i
    auto dst = vec_from_nir(instr->dest, 4);
 
    sfn_log << SfnLog::io << "Set input[" << in_var->data.driver_location
-           << "].gpr=" << dst.sel() << "\n";
+           << "].gpr=" << dst.sel()
+           << " interp=" << io.ij_index()
+           << "\n";
 
    io.set_gpr(dst.sel());
 