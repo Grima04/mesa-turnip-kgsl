3943bcec9c81d4848cb2463697c57a86e23e2528
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-10-06 21:31:18 -0400

    panfrost: Fix Bifrost filter selection
    Point sampling is jargon for nearest, apparently. Fixes
    
    dEQP-GLES2.functional.texture.filtering.2d.nearest_nearest_clamp_rgba8888_pot
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7081>

--- src/gallium/drivers/panfrost/pan_cmdstream.c
+++ src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -221,8 +221,8 @@ void panfrost_sampler_desc_init_bifrost(const struct pipe_sampler_state *cso,
                                         struct mali_bifrost_sampler_packed *hw)
 {
         pan_pack(hw, BIFROST_SAMPLER, cfg) {
-                cfg.point_sample_magnify = cso->mag_img_filter == PIPE_TEX_FILTER_LINEAR;
-                cfg.point_sample_minify = cso->min_img_filter == PIPE_TEX_FILTER_LINEAR;
+                cfg.point_sample_magnify = cso->mag_img_filter == PIPE_TEX_FILTER_NEAREST;
+                cfg.point_sample_minify = cso->min_img_filter == PIPE_TEX_FILTER_NEAREST;
                 cfg.mipmap_mode = pan_pipe_to_mipmode(cso->min_mip_filter);
                 cfg.normalized_coordinates = cso->normalized_coords;
 
