cf3b638f47b80034233a2fbf24cecccc55701447
Rhys Perry <pendingchaos02@gmail.com>
2020-10-07 20:33:50 +0100

    radv: remove RDR2 discard workaround
    The game appears to use HLSL, so this workaround now lives in
    SPIR-V -> NIR.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7062>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -582,9 +582,6 @@ radv_handle_per_app_options(struct radv_instance *instance,
 		} else if (!strcmp(name, "DOOMEternal")) {
 			/* Zero VRAM for Doom Eternal to fix rendering issues. */
 			instance->debug_flags |= RADV_DEBUG_ZERO_VRAM;
-		} else if (!strcmp(name, "Red Dead Redemption 2")) {
-			/* Work around a RDR2 game bug */
-			instance->debug_flags |= RADV_DEBUG_DISCARD_TO_DEMOTE;
 		}
 	}
 
