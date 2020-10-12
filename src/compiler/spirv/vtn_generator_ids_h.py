1070bba19e26640bc1dbfd04180e97217761404d
Rhys Perry <pendingchaos02@gmail.com>
2020-10-12 14:44:36 +0100

    android: fix SPIR-V -> NIR build
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Mauro Rossi <issor.oruam@gmail.com>
    Fixes: 18f9fc919e1 ('spirv: add and use a generator id enum')
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7097>

--- src/compiler/spirv/vtn_generator_ids_h.py
+++ src/compiler/spirv/vtn_generator_ids_h.py
@@ -1,6 +1,6 @@
 COPYRIGHT = """\
 /*
- * Copyright © 2020 Valve Corporation
+ * Copyright (C) 2020 Valve Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
