504142ff754e521aa88a6e95c63683a71323878a
Connor Abbott <cwabbott0@gmail.com>
2020-10-27 13:43:56 +0100

    ir3: Fix STP/LDP assembly
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7386>

--- src/freedreno/ir3/ir3.c
+++ src/freedreno/ir3/ir3.c
@@ -836,7 +836,8 @@ static int emit_cat6(struct ir3_instruction *instr, void *ptr,
 
 		return 0;
 	} else if (instr->cat6.src_offset || (instr->opc == OPC_LDG) ||
-			(instr->opc == OPC_LDL) || (instr->opc == OPC_LDLW)) {
+			(instr->opc == OPC_LDL) || (instr->opc == OPC_LDLW) ||
+			(instr->opc == OPC_LDP)) {
 		struct ir3_register *src3 = instr->regs[3];
 		instr_cat6a_t *cat6a = ptr;
 
@@ -874,7 +875,8 @@ static int emit_cat6(struct ir3_instruction *instr, void *ptr,
 	}
 
 	if (instr->cat6.dst_offset || (instr->opc == OPC_STG) ||
-			(instr->opc == OPC_STL) || (instr->opc == OPC_STLW)) {
+			(instr->opc == OPC_STL) || (instr->opc == OPC_STLW) ||
+			(instr->opc == OPC_STP)) {
 		instr_cat6c_t *cat6c = ptr;
 		cat6->dst_off = true;
 		cat6c->dst = reg(dst, info, instr->repeat, IR3_REG_R | IR3_REG_HALF);
