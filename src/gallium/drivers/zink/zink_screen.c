62d6ec083b96b1176aa18265115c69e46e1d97a7
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-10-27 14:34:16 -0400

    zink: enable PIPE_CAP_INVALIDATE_BUFFER
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9546>

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -235,6 +235,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE:
    case PIPE_CAP_GL_SPIRV:
    case PIPE_CAP_CLEAR_SCISSORED:
+   case PIPE_CAP_INVALIDATE_BUFFER:
       return 1;
 
    case PIPE_CAP_ROBUST_BUFFER_ACCESS_BEHAVIOR:
