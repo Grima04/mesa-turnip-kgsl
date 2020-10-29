0fe5490724e3803dd397d9b8a2d29fdf7d5da886
Alejandro Piñeiro <apinheiro@igalia.com>
2020-10-29 11:19:18 +0100

    v3d/format: use XYZ1 swizzle for three-component formats
    R11G11B10_FLOAT and R9G9B9E5_FLOAT are three-component formats, so we
    shouldn't use 1 for the alpha component.
    
    We don't know about any test/app getting fixed with this change, but
    it is the equivalent to v3dv commit
    e07c5467633431377ce4c9890f96d58cc7a77fdc. Vulkan CTS has some tests
    that used that format and failed if not using XYZ1.
    
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Reviewed-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7365>

--- src/gallium/drivers/v3d/v3dx_format_table.c
+++ src/gallium/drivers/v3d/v3dx_format_table.c
@@ -141,8 +141,8 @@ static const struct v3d_format format_table[] = {
         FORMAT(A32_SINT,          R32I,         R32I,        SWIZ_000X, 32, 1),
         FORMAT(A32_UINT,          R32UI,        R32UI,       SWIZ_000X, 32, 1),
 
-        FORMAT(R11G11B10_FLOAT,   R11F_G11F_B10F, R11F_G11F_B10F, SWIZ_XYZW, 16, 0),
-        FORMAT(R9G9B9E5_FLOAT,    NO,           RGB9_E5,     SWIZ_XYZW, 16, 0),
+        FORMAT(R11G11B10_FLOAT,   R11F_G11F_B10F, R11F_G11F_B10F, SWIZ_XYZ1, 16, 0),
+        FORMAT(R9G9B9E5_FLOAT,    NO,           RGB9_E5,     SWIZ_XYZ1, 16, 0),
 
 #if V3D_VERSION >= 40
         FORMAT(S8_UINT_Z24_UNORM, D24S8,        DEPTH24_X8,  SWIZ_XXXX, 32, 1),
