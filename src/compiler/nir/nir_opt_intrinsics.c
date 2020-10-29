d9c0f3627d78c33926c358f362818f9fe917c8a6
Jason Ekstrand <jason@jlekstrand.net>
2020-10-29 10:15:46 -0500

    nir/opt_intrinsics: Report progress for the gl_SampleMask optimization
    Fixes: d3ce8a7f6b93 "nir: optimize gl_SampleMaskIn to gl_HelperInvocation..."
    Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7366>

--- src/compiler/nir/nir_opt_intrinsics.c
+++ src/compiler/nir/nir_opt_intrinsics.c
@@ -86,6 +86,7 @@ opt_intrinsics_impl(nir_function_impl *impl,
                      nir_ssa_def_rewrite_uses(&alu->dest.dest.ssa,
                                               nir_src_for_ssa(new_expr));
                      nir_instr_remove(&alu->instr);
+                     progress = true;
                      continue;
                   }
                }
