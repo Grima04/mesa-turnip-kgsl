Grima04/mesa-turnip-kgsl

commit 95b9fc4146656493ad2544bd75ee14c01feb9163

zink: implement pipe_device_reset_callback
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6763>

src/gallium/drivers/zink/zink_batch.c
@@ -99,8 +99,8 @@ zink_end_batch(struct zink_context *ctx, struct zink_batch *batch)
    si.pCommandBuffers = &batch->cmdbuf;
 
    if (vkQueueSubmit(ctx->queue, 1, &si, batch->fence->fence) != VK_SUCCESS) {
-      debug_printf("vkQueueSubmit failed\n");
-      abort();
+      debug_printf("ZINK: vkQueueSubmit() failed\n");
+      ctx->is_device_lost = true;
    }
 }
 

src/gallium/drivers/zink/zink_context.c
@@ -75,6 +75,39 @@ zink_context_destroy(struct pipe_context *pctx)
    FREE(ctx);
 }
 
+static enum pipe_reset_status
+zink_get_device_reset_status(struct pipe_context *pctx)
+{
+   struct zink_context *ctx = zink_context(pctx);
+
+   enum pipe_reset_status status = PIPE_NO_RESET;
+
+   if (ctx->is_device_lost) {
+      // Since we don't know what really happened to the hardware, just
+      // assume that we are in the wrong
+      status = PIPE_GUILTY_CONTEXT_RESET;
+
+      debug_printf("ZINK: device lost detected!\n");
+
+      if (ctx->reset.reset)
+         ctx->reset.reset(ctx->reset.data, status);
+   }
+
+   return status;
+}
+
+static void
+zink_set_device_reset_callback(struct pipe_context *pctx,
+                               const struct pipe_device_reset_callback *cb)
+{
+   struct zink_context *ctx = zink_context(pctx);
+
+   if (cb)
+      ctx->reset = *cb;
+   else
+      memset(&ctx->reset, 0, sizeof(ctx->reset));
+}
+
 static VkSamplerMipmapMode
 sampler_mipmap_mode(enum pipe_tex_mipfilter filter)
 {
@@ -1166,6 +1199,8 @@ zink_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
    ctx->base.priv = priv;
 
    ctx->base.destroy = zink_context_destroy;
+   ctx->base.get_device_reset_status = zink_get_device_reset_status;
+   ctx->base.set_device_reset_callback = zink_set_device_reset_callback;
 
    zink_context_state_init(&ctx->base);
 

src/gallium/drivers/zink/zink_context.h
@@ -81,8 +81,11 @@ struct zink_context {
    struct slab_child_pool transfer_pool;
    struct blitter_context *blitter;
 
+   struct pipe_device_reset_callback reset;
+
    VkCommandPool cmdpool;
    struct zink_batch batches[4];
+   bool is_device_lost;
    unsigned curr_batch;
 
    VkQueue queue;

commit 0e3f2e632154d003cc582468dfcb758d9e8cf100

zink: call the reset callback not only during a status check
Fixes: 95b9fc41 ("zink: implement pipe_device_reset_callback")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7404>

src/gallium/drivers/zink/zink_batch.c
@@ -101,6 +101,10 @@ zink_end_batch(struct zink_context *ctx, struct zink_batch *batch)
    if (vkQueueSubmit(ctx->queue, 1, &si, batch->fence->fence) != VK_SUCCESS) {
       debug_printf("ZINK: vkQueueSubmit() failed\n");
       ctx->is_device_lost = true;
+
+      if (ctx->reset.reset) {
+         ctx->reset.reset(ctx->reset.data, PIPE_GUILTY_CONTEXT_RESET);
+      }
    }
 }
 