14bbc24b60cdd5eb17b3f2798fddf483edd92b00
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-11-09 08:10:13 -0500

    pan/mdg: Add missing Collabora copyright notices
    On a few of the older files.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Acked-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7502>

--- src/panfrost/midgard/compiler.h
+++ src/panfrost/midgard/compiler.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2019-2020 Collabora, Ltd.
  * Copyright (C) 2019 Alyssa Rosenzweig <alyssa@rosenzweig.io>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
