6e6dab479907fe79ed24e69be841dd3ec7bd479e
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-11-10 17:32:15 +0100

    nir: handle float atomics in copy propagation pass
    Without this patch, copy propagation pass can optimize out
    buffer loads out of compare & swap loop, which then leads
    to infinite loop.
    
    Triggered by a change to atomicCompSwap float test in piglit.
    
    Fixes: 8424cd8fbd1 ("nir: Account for atomics in copy propagation.")
    Suggested-by: Jason Ekstrand <jason@jlekstrand.net>
    Signed-off-by: Marcin Ślusarz <marcin.slusarz@intel.com>
    Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7538>

--- src/compiler/nir/nir_opt_copy_prop_vars.c
+++ src/compiler/nir/nir_opt_copy_prop_vars.c
@@ -218,15 +218,19 @@ gather_vars_written(struct copy_prop_var_state *state,
             break;
 
          case nir_intrinsic_deref_atomic_add:
+         case nir_intrinsic_deref_atomic_fadd:
          case nir_intrinsic_deref_atomic_imin:
          case nir_intrinsic_deref_atomic_umin:
+         case nir_intrinsic_deref_atomic_fmin:
          case nir_intrinsic_deref_atomic_imax:
          case nir_intrinsic_deref_atomic_umax:
+         case nir_intrinsic_deref_atomic_fmax:
          case nir_intrinsic_deref_atomic_and:
          case nir_intrinsic_deref_atomic_or:
          case nir_intrinsic_deref_atomic_xor:
          case nir_intrinsic_deref_atomic_exchange:
          case nir_intrinsic_deref_atomic_comp_swap:
+         case nir_intrinsic_deref_atomic_fcomp_swap:
          case nir_intrinsic_store_deref:
          case nir_intrinsic_copy_deref:
          case nir_intrinsic_memcpy_deref: {
@@ -1118,15 +1122,19 @@ copy_prop_vars_block(struct copy_prop_var_state *state,
 
       case nir_intrinsic_memcpy_deref:
       case nir_intrinsic_deref_atomic_add:
+      case nir_intrinsic_deref_atomic_fadd:
       case nir_intrinsic_deref_atomic_imin:
       case nir_intrinsic_deref_atomic_umin:
+      case nir_intrinsic_deref_atomic_fmin:
       case nir_intrinsic_deref_atomic_imax:
       case nir_intrinsic_deref_atomic_umax:
+      case nir_intrinsic_deref_atomic_fmax:
       case nir_intrinsic_deref_atomic_and:
       case nir_intrinsic_deref_atomic_or:
       case nir_intrinsic_deref_atomic_xor:
       case nir_intrinsic_deref_atomic_exchange:
       case nir_intrinsic_deref_atomic_comp_swap:
+      case nir_intrinsic_deref_atomic_fcomp_swap:
          if (debug) dump_instr(instr);
 
          nir_deref_instr *dst = nir_src_as_deref(intrin->src[0]);
