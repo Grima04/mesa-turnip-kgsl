af09db6e588e694c62ad2c2530b6570ef9765c07
Pierre Moreau <dev@pmoreau.org>
2020-11-12 12:01:14 +0100

    clover: Advertise cl_khr_il_program
    v2:
    * Change the code to avoid the macro (Karol Herbst, Francisco Jerez);
    * Update the code to add a `cl_name_version`.
    
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>
    Signed-off-by: Pierre Moreau <dev@pmoreau.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/2078>

--- src/gallium/frontends/clover/core/device.cpp
+++ src/gallium/frontends/clover/core/device.cpp
@@ -367,6 +367,9 @@ device::supported_extensions() const {
       vec.push_back( cl_name_version{ CL_MAKE_VERSION(1, 0, 0), "cl_khr_fp16" } );
    if (svm_support())
       vec.push_back( cl_name_version{ CL_MAKE_VERSION(1, 0, 0), "cl_arm_shared_virtual_memory" } );
+   if (!clover::spirv::supported_versions().empty() &&
+       supports_ir(PIPE_SHADER_IR_NIR_SERIALIZED))
+      vec.push_back( cl_name_version{ CL_MAKE_VERSION(1, 0, 0), "cl_khr_il_program" } );
    return vec;
 }
 
