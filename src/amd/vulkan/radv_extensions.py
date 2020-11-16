58e52326254e6967018354bf678fc7fa7c0eee53
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2020-11-16 12:52:58 +0100

    radv: Enable DRM format modifiers on GFX9+.
    Reviewed-By: Chad Versace <chad@kiwitree.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7667>

--- src/amd/vulkan/radv_extensions.py
+++ src/amd/vulkan/radv_extensions.py
@@ -139,7 +139,7 @@ EXTENSIONS = [
     Extension('VK_EXT_external_memory_host',              1, 'device->rad_info.has_userptr'),
     Extension('VK_EXT_global_priority',                   2, 'device->rad_info.has_ctx_priority'),
     Extension('VK_EXT_host_query_reset',                  1, True),
-    Extension('VK_EXT_image_drm_format_modifier',         1, False),
+    Extension('VK_EXT_image_drm_format_modifier',         1, 'device->rad_info.chip_class >= GFX9'),
     Extension('VK_EXT_image_robustness',                  1, True),
     Extension('VK_EXT_index_type_uint8',                  1, 'device->rad_info.chip_class >= GFX8'),
     Extension('VK_EXT_inline_uniform_block',              1, True),
