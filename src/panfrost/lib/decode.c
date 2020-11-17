05880bc28510dcd829fae293c605823f07077af2
Boris Brezillon <boris.brezillon@collabora.com>
2020-11-17 19:18:42 +0100

    panfrost: Fix decoding of texture payloads
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7653>

--- src/panfrost/lib/decode.c
+++ src/panfrost/lib/decode.c
@@ -743,10 +743,10 @@ pandecode_texture_payload(mali_ptr payload,
                 if (manual_stride && (i & 1)) {
                         /* signed 32-bit snuck in as a 64-bit pointer */
                         uint64_t stride_set = pointers_and_strides[i];
-                        uint32_t clamped_stride = stride_set;
-                        int32_t stride = clamped_stride;
-                        assert(stride_set == clamped_stride);
-                        pandecode_log("(mali_ptr) %d /* stride */, \n", stride);
+                        int32_t line_stride = stride_set;
+                        int32_t surface_stride = stride_set >> 32;
+                        pandecode_log("(mali_ptr) %d /* surface stride */ %d /* line stride */, \n",
+                                      surface_stride, line_stride);
                 } else {
                         char *a = pointer_as_memory_reference(pointers_and_strides[i]);
                         pandecode_log("%s, \n", a);
