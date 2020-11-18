0626e3a950d5dbe5d86244670b54b67a2af6014c
Eric Anholt <eric@anholt.net>
2020-11-17 16:37:41 -0800

    gallium: Fix leak of the merged driconf options.
    Fixes: 8a05d6ffc65d ("driconf: Make the driver's declarations be structs instead of XML.")
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7695>

--- src/gallium/auxiliary/pipe-loader/pipe_loader.c
+++ src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -120,6 +120,7 @@ pipe_loader_load_options(struct pipe_loader_device *dev)
    driParseOptionInfo(&dev->option_info, merged_driconf, merged_count);
    driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
                        dev->driver_name, NULL, NULL, 0, NULL, 0);
+   free((void *)merged_driconf);
 }
 
 char *
