bcb2981e145f0c2a54a637e20b6c55eaf316c04f
Rob Clark <robdclark@chromium.org>
2020-11-23 14:30:32 -0800

    mesa/st: Use do_once for one-time init
    Also, since there is a second call-path into st_init_extensions() from
    get_version(), add an extra st_debug_init() call.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Kristian H. Kristensen <hoegsberg@google.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7644>

--- src/mesa/state_tracker/st_debug.c
+++ src/mesa/state_tracker/st_debug.c
@@ -68,7 +68,9 @@ DEBUG_GET_ONCE_FLAGS_OPTION(st_debug, "ST_DEBUG", st_debug_flags, 0)
 void
 st_debug_init(void)
 {
-   ST_DEBUG = debug_get_option_st_debug();
+   do_once {
+      ST_DEBUG = debug_get_option_st_debug();
+   }
 }
 
 
--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -1040,6 +1040,8 @@ void st_init_extensions(struct pipe_screen *screen,
         } },
    };
 
+   st_debug_init();
+
    /*
     * Extensions that are supported by all Gallium drivers:
     */
