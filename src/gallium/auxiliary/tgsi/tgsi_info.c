f57f040f13cdf8ba7bafc46ce398d12e2a7ef63c
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-11-24 23:33:06 +0100

    Revert "tgsi: Fix helgrind complaint about one-time init"
    This reverts commit d91fe7d1c65179e64a6ca294135ac1bad2b16fb9.
    
    Acked-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7760>

--- src/gallium/auxiliary/tgsi/tgsi_info.c
+++ src/gallium/auxiliary/tgsi/tgsi_info.c
@@ -53,12 +53,15 @@ static const struct tgsi_opcode_info opcode_info[TGSI_OPCODE_LAST] =
 const struct tgsi_opcode_info *
 tgsi_get_opcode_info(enum tgsi_opcode opcode)
 {
+   static boolean firsttime = 1;
+
    ASSERT_BITFIELD_SIZE(struct tgsi_opcode_info, opcode, TGSI_OPCODE_LAST - 1);
    ASSERT_BITFIELD_SIZE(struct tgsi_opcode_info, output_mode,
                         TGSI_OUTPUT_OTHER);
 
-   do_once {
+   if (firsttime) {
       unsigned i;
+      firsttime = 0;
       for (i = 0; i < ARRAY_SIZE(opcode_info); i++)
          assert(opcode_info[i].opcode == i);
    }
