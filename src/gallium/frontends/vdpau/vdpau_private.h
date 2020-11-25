cd1ac36ddde6eaf758d1cbb7a501369f5d60d330
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2020-11-25 15:32:36 +0100

    vdpau: fix -Wabsolute-value warning
    vdpau specifies that top-left is x0/y0, bottom-right is x1/y1 and that x0/y0 are
    inclusive while x1/y1 are exclusive.
    
    This commit remove the abs() usage and instead verifies that the VdpRects passed
    by the user matche the documentation. When they don't they're treated as empty
    rectangles.
    
    Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7846>

--- src/gallium/frontends/vdpau/vdpau_private.h
+++ src/gallium/frontends/vdpau/vdpau_private.h
@@ -349,10 +349,16 @@ RectToPipeBox(const VdpRect *rect, struct pipe_resource *res)
    box.depth = 1;
 
    if (rect) {
-      box.x = MIN2(rect->x0, rect->x1);
-      box.y = MIN2(rect->y0, rect->y1);
-      box.width = abs(rect->x1 - rect->x0);
-      box.height = abs(rect->y1 - rect->y0);
+      if (rect->x1 > rect->x0 &&
+          rect->y1 > rect->y0) {
+         box.x = rect->x0;
+         box.y = rect->y0;
+         box.width = rect->x1 - box.x;
+         box.height = rect->y1 - box.y;
+      } else {
+         box.width = 0;
+         box.height = 0;
+      }
    }
 
    return box;
