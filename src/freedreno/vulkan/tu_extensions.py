271c18f48ec3f7396150686a4f15c756818cf335
Connor Abbott <cwabbott0@gmail.com>
2020-12-04 17:26:31 +0100

    tu: Expose VK_KHR_relaxed_block_layout
    This was absorbed into Vulkan 1.1, but we forgot to expose it
    separately. It's a subset of what's allowed by
    VK_EXT_scalar_block_layout.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8695>

--- src/freedreno/vulkan/tu_extensions.py
+++ src/freedreno/vulkan/tu_extensions.py
@@ -115,6 +115,7 @@ EXTENSIONS = [
     Extension('VK_KHR_16bit_storage',                     1, 'device->gpu_id >= 650'),
     Extension('VK_EXT_scalar_block_layout',               1, True),
     Extension('VK_KHR_spirv_1_4',                         1, True),
+    Extension('VK_KHR_relaxed_block_layout',              1, True),
 ]
 
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
