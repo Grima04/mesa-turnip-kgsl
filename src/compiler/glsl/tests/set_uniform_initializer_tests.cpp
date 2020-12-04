39dbf47bfe75231329fa29eb21465d915ba06bc4
Eric Anholt <eric@anholt.net>
2020-12-04 13:19:32 -0800

    glsl/uniform_initializer_tests: Fix memory leak
    Needed for meson test with asan enabled.
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7936>

--- src/compiler/glsl/tests/set_uniform_initializer_tests.cpp
+++ src/compiler/glsl/tests/set_uniform_initializer_tests.cpp
@@ -86,6 +86,9 @@ set_uniform_initializer::TearDown()
    ralloc_free(this->mem_ctx);
    this->mem_ctx = NULL;
 
+   if (this->prog->UniformHash)
+      string_to_uint_map_dtor(this->prog->UniformHash);
+
    ralloc_free(this->prog);
    this->prog = NULL;
 
