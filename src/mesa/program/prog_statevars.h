23729a783996545ff2e56605f7c025a36ac8ba35
Marek Olšák <marek.olsak@amd.com>
2020-12-12 13:20:20 -0500

    mesa: sort and tightly pack STATE_* enums to generate better switch code
    The enums are sorted by their occurence in fetch_state.
    
    Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8183>

--- src/mesa/program/prog_statevars.h
+++ src/mesa/program/prog_statevars.h
@@ -53,7 +53,7 @@ struct gl_program_parameter_list;
 typedef enum gl_state_index_ {
    STATE_NOT_STATE_VAR = 0,
 
-   STATE_MATERIAL = 100,  /* start at 100 so small ints are seen as ints */
+   STATE_MATERIAL,
 
    STATE_LIGHT,         /* One gl_light attribute. */
    STATE_LIGHT_ARRAY, /* Multiple gl_light attributes loaded at once. */
@@ -62,6 +62,7 @@ typedef enum gl_state_index_ {
    STATE_LIGHTPROD,
 
    STATE_TEXGEN,
+   STATE_TEXENV_COLOR,
 
    STATE_FOG_COLOR,
    STATE_FOG_PARAMS,
@@ -96,6 +97,49 @@ typedef enum gl_state_index_ {
    STATE_PROGRAM_MATRIX_TRANSPOSE,
    STATE_PROGRAM_MATRIX_INVTRANS,
 
+   STATE_NUM_SAMPLES,    /* An integer, not a float like the other state vars */
+
+   STATE_DEPTH_RANGE,
+
+   STATE_FRAGMENT_PROGRAM_ENV,
+   STATE_FRAGMENT_PROGRAM_ENV_ARRAY,
+   STATE_FRAGMENT_PROGRAM_LOCAL,
+   STATE_FRAGMENT_PROGRAM_LOCAL_ARRAY,
+   STATE_VERTEX_PROGRAM_ENV,
+   STATE_VERTEX_PROGRAM_ENV_ARRAY,
+   STATE_VERTEX_PROGRAM_LOCAL,
+   STATE_VERTEX_PROGRAM_LOCAL_ARRAY,
+
+   STATE_NORMAL_SCALE_EYESPACE,
+   STATE_CURRENT_ATTRIB,        /* ctx->Current vertex attrib value */
+   STATE_CURRENT_ATTRIB_MAYBE_VP_CLAMPED,        /* ctx->Current vertex attrib value after passthrough vertex processing */
+   STATE_NORMAL_SCALE,
+   STATE_FOG_PARAMS_OPTIMIZED,  /* for faster fog calc */
+   STATE_POINT_SIZE_CLAMPED,    /* includes implementation dependent size clamp */
+   STATE_LIGHT_SPOT_DIR_NORMALIZED,   /* pre-normalized spot dir */
+   STATE_LIGHT_POSITION,              /* object vs eye space */
+   STATE_LIGHT_POSITION_NORMALIZED,   /* object vs eye space */
+   STATE_LIGHT_HALF_VECTOR,           /* object vs eye space */
+   STATE_PT_SCALE,              /**< Pixel transfer RGBA scale */
+   STATE_PT_BIAS,               /**< Pixel transfer RGBA bias */
+   STATE_FB_SIZE,               /**< (width-1, height-1, 0, 0) */
+   STATE_FB_WPOS_Y_TRANSFORM,   /**< (1, 0, -1, height) if a FBO is bound, (-1, height, 1, 0) otherwise */
+   STATE_FB_PNTC_Y_TRANSFORM,   /**< (1, 0, 0, 0) if point origin is upper left, (-1, 1, 0, 0) otherwise */
+   STATE_TCS_PATCH_VERTICES_IN, /**< gl_PatchVerticesIn for TCS (integer) */
+   STATE_TES_PATCH_VERTICES_IN, /**< gl_PatchVerticesIn for TES (integer) */
+   /**
+    * A single enum gl_blend_support_qualifier value representing the
+    * currently active advanced blending equation, or zero if disabled.
+    */
+   STATE_ADVANCED_BLENDING_MODE,
+   STATE_ALPHA_REF,        /* alpha-test reference value */
+   STATE_CLIP_INTERNAL,    /* similar to STATE_CLIPPLANE, but in clip-space */
+
+   STATE_INTERNAL_DRIVER,	/* first available state index for drivers (must be last) */
+
+
+   /** All enums below don't occur in state[0]. **/
+
    /* These 8 enums must be in the same order as the gl_light union members,
     * which should also match the order of gl_LightSource members.
     */
@@ -122,48 +166,6 @@ typedef enum gl_state_index_ {
    STATE_TEXGEN_OBJECT_T,
    STATE_TEXGEN_OBJECT_R,
    STATE_TEXGEN_OBJECT_Q,
-
-   STATE_TEXENV_COLOR,
-
-   STATE_NUM_SAMPLES,    /* An integer, not a float like the other state vars */
-
-   STATE_DEPTH_RANGE,
-
-   STATE_VERTEX_PROGRAM_ENV,
-   STATE_VERTEX_PROGRAM_ENV_ARRAY,
-   STATE_VERTEX_PROGRAM_LOCAL,
-   STATE_VERTEX_PROGRAM_LOCAL_ARRAY,
-   STATE_FRAGMENT_PROGRAM_ENV,
-   STATE_FRAGMENT_PROGRAM_ENV_ARRAY,
-   STATE_FRAGMENT_PROGRAM_LOCAL,
-   STATE_FRAGMENT_PROGRAM_LOCAL_ARRAY,
-
-   STATE_CURRENT_ATTRIB,        /* ctx->Current vertex attrib value */
-   STATE_CURRENT_ATTRIB_MAYBE_VP_CLAMPED,        /* ctx->Current vertex attrib value after passthrough vertex processing */
-   STATE_NORMAL_SCALE,
-   STATE_NORMAL_SCALE_EYESPACE,
-   STATE_FOG_PARAMS_OPTIMIZED,  /* for faster fog calc */
-   STATE_POINT_SIZE_CLAMPED,    /* includes implementation dependent size clamp */
-   STATE_LIGHT_SPOT_DIR_NORMALIZED,   /* pre-normalized spot dir */
-   STATE_LIGHT_POSITION,              /* object vs eye space */
-   STATE_LIGHT_POSITION_NORMALIZED,   /* object vs eye space */
-   STATE_LIGHT_HALF_VECTOR,           /* object vs eye space */
-   STATE_PT_SCALE,              /**< Pixel transfer RGBA scale */
-   STATE_PT_BIAS,               /**< Pixel transfer RGBA bias */
-   STATE_FB_SIZE,               /**< (width-1, height-1, 0, 0) */
-   STATE_FB_WPOS_Y_TRANSFORM,   /**< (1, 0, -1, height) if a FBO is bound, (-1, height, 1, 0) otherwise */
-   STATE_FB_PNTC_Y_TRANSFORM,   /**< (1, 0, 0, 0) if point origin is upper left, (-1, 1, 0, 0) otherwise */
-   STATE_TCS_PATCH_VERTICES_IN, /**< gl_PatchVerticesIn for TCS (integer) */
-   STATE_TES_PATCH_VERTICES_IN, /**< gl_PatchVerticesIn for TES (integer) */
-   /**
-    * A single enum gl_blend_support_qualifier value representing the
-    * currently active advanced blending equation, or zero if disabled.
-    */
-   STATE_ADVANCED_BLENDING_MODE,
-   STATE_ALPHA_REF,        /* alpha-test reference value */
-   STATE_CLIP_INTERNAL,    /* similar to STATE_CLIPPLANE, but in clip-space */
-
-   STATE_INTERNAL_DRIVER	/* first available state index for drivers (must be last) */
 } gl_state_index;
 
 
