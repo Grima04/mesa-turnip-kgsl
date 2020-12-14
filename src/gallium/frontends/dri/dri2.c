d21cc94201ab1e6cb54ff33a7a835248fe336777
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2020-12-14 21:32:55 +0200

    gallium/dri2: Don't forget protected content flag
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: bd182777c8f2c4 ("egl: implement EGL_EXT_protected_surface support")
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8094>

--- src/gallium/frontends/dri/dri2.c
+++ src/gallium/frontends/dri/dri2.c
@@ -758,6 +758,8 @@ dri2_create_image_from_winsys(__DRIscreen *_screen,
    if (pscreen->is_format_supported(pscreen, map->pipe_format, screen->target, 0, 0,
                                     PIPE_BIND_SAMPLER_VIEW))
       tex_usage |= PIPE_BIND_SAMPLER_VIEW;
+   if (is_protected_content)
+      tex_usage |= PIPE_BIND_PROTECTED;
 
    /* For NV12, see if we have support for sampling r8_b8g8 */
    if (!tex_usage && map->pipe_format == PIPE_FORMAT_NV12 &&
