e4cdbeab814bdc2b468ec2375a74fc961a423213
Eric Anholt <eric@anholt.net>
2020-12-17 12:30:16 -0800

    freedreno/a6xx: Flush depth at the end of bypass rendering, too.
    We apparently don't have anything else making sure that it's flushed in
    between use as a render target and use as a texture source, so bypass-mode
    depth texture sampling could get stale data.
    
    Fixes consistent (as far as I could see) failures in FD_MESA_DEBUG=nogmem
    on:
    
    dEQP-GLES31.functional.texture.multisample.samples_*.use_texture_depth_2d
    dEQP-GLES31.functional.stencil_texturing.render.depth24_stencil8_draw
    
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8146>

--- src/gallium/drivers/freedreno/a6xx/fd6_gmem.c
+++ src/gallium/drivers/freedreno/a6xx/fd6_gmem.c
@@ -1435,6 +1435,7 @@ fd6_emit_sysmem_fini(struct fd_batch *batch)
 	fd6_emit_lrz_flush(ring);
 
 	fd6_event_write(batch, ring, PC_CCU_FLUSH_COLOR_TS, true);
+	fd6_event_write(batch, ring, PC_CCU_FLUSH_DEPTH_TS, true);
 }
 
 void
