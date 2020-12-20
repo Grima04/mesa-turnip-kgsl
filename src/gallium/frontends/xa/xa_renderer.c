2a0195f4b9e535945a2df9c16469d923a696126a
Marek Olšák <marek.olsak@amd.com>
2020-12-20 00:43:39 -0500

    st/xa: stop using cso_set_sampler_views
    It will be removed.
    
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8180>

--- src/gallium/frontends/xa/xa_renderer.c
+++ src/gallium/frontends/xa/xa_renderer.c
@@ -442,7 +442,7 @@ renderer_copy_prepare(struct xa_context *r,
 	u_sampler_view_default_template(&templ,
 					src_texture, src_texture->format);
 	src_view = pipe->create_sampler_view(pipe, src_texture, &templ);
-	cso_set_sampler_views(r->cso, PIPE_SHADER_FRAGMENT, 1, &src_view);
+	pipe->set_sampler_views(pipe, PIPE_SHADER_FRAGMENT, 0, 1, &src_view);
 	pipe_sampler_view_reference(&src_view, NULL);
     }
 
