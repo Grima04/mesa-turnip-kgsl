01dfd65a2d407dd95ac472e7e80e736bc7a8fc57
Dave Airlie <airlied@redhat.com>
2020-12-29 12:43:11 +1000

    nir: port fp16 casting code from dxil
    This moves the dxil pass to common code and makes dxil
    use the new code.
    
    Acked-by: Adam Jackson <ajax@redhat.com>
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9643>

--- src/microsoft/clc/clc_compiler.c
+++ src/microsoft/clc/clc_compiler.c
@@ -1361,7 +1361,7 @@ clc_to_dxil(struct clc_context *ctx,
    NIR_PASS_V(nir, dxil_nir_lower_loads_stores_to_dxil);
    NIR_PASS_V(nir, dxil_nir_opt_alu_deref_srcs);
    NIR_PASS_V(nir, dxil_nir_lower_atomics_to_dxil);
-   NIR_PASS_V(nir, dxil_nir_lower_fp16_casts);
+   NIR_PASS_V(nir, nir_lower_fp16_casts);
    NIR_PASS_V(nir, nir_lower_convert_alu_types, NULL);
 
    // Convert pack to pack_split
