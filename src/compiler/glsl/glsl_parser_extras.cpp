yuq/mesa-lima

commit c7e3d31b0b5f22299a6bd72655502ce8427b40bf

glsl: fix compat shaders in GLSL 1.40
The compatibility and core tokens were not added until GLSL 1.50,
for GLSL 1.40 just assume all shaders built with a compat profile
are compat shaders.

Fixes rendering issues in Dawn of War II on radeonsi which has
enabled OpenGL 3.1 compat support.

Fixes: a0c8b49284ef "mesa: enable OpenGL 3.1 with ARB_compatibility"

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=105807

src/compiler/glsl/glsl_parser_extras.cpp
@@ -429,6 +429,8 @@ _mesa_glsl_parse_state::process_version_directive(YYLTYPE *locp, int version,
       this->language_version = version;
 
    this->compat_shader = compat_token_present ||
+                         (this->ctx->API == API_OPENGL_COMPAT &&
+                          this->language_version == 140) ||
                          (!this->es_shader && this->language_version < 140);
 
    bool supported = false;

Grima04/mesa-turnip-kgsl

commit 6c8cc9be12dc5d6c0d2386d6addb69d8f2fb5399

glsl: default to compat shaders in compat profile
If the shader does not specify "core" or "compatibility" in shaders
above 1.40 we were defaulting these shaders to core shaders when
in a compat profile. Instead default to compat shaders.

This brings us inline with the behaviour of the binary drivers and
fixes a crash on start-up for the game Foundation.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3594

Fixes: c7e3d31b0b5f ("glsl: fix compat shaders in GLSL 1.40")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6993>

src/compiler/glsl/glsl_parser_extras.cpp
@@ -396,14 +396,13 @@ _mesa_glsl_parse_state::process_version_directive(YYLTYPE *locp, int version,
 {
    bool es_token_present = false;
    bool compat_token_present = false;
+   bool core_token_present = false;
    if (ident) {
       if (strcmp(ident, "es") == 0) {
          es_token_present = true;
       } else if (version >= 150) {
          if (strcmp(ident, "core") == 0) {
-            /* Accept the token.  There's no need to record that this is
-             * a core profile shader since that's the only profile we support.
-             */
+            core_token_present = true;
          } else if (strcmp(ident, "compatibility") == 0) {
             compat_token_present = true;
 
@@ -444,7 +443,8 @@ _mesa_glsl_parse_state::process_version_directive(YYLTYPE *locp, int version,
 
    this->compat_shader = compat_token_present ||
                          (this->ctx->API == API_OPENGL_COMPAT &&
-                          this->language_version == 140) ||
+                          this->language_version >= 140 &&
+                          !core_token_present) ||
                          (!this->es_shader && this->language_version < 140);
 
    bool supported = false;