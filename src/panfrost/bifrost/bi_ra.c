5eb39dd95f8b9eec1fd648f7797edf43988db402
Icecream95 <ixn@disroot.org>
2021-01-14 14:44:48 +1300

    pan/bi: Iterate from zero when setting RA interference
    It is now valid for a node to have an index of zero.
    
    Fixes: 39aa8c4a5ac ("pan/bi: Switch to new IR")
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8358>

--- src/panfrost/bifrost/bi_ra.c
+++ src/panfrost/bifrost/bi_ra.c
@@ -48,7 +48,7 @@ bi_compute_interference(bi_context *ctx, struct lcra_state *l)
                                 if (bi_get_node(ins->dest[d]) >= node_count)
                                         continue;
 
-                                for (unsigned i = 1; i < node_count; ++i) {
+                                for (unsigned i = 0; i < node_count; ++i) {
                                         if (live[i])
                                                 lcra_add_node_interference(l, bi_get_node(ins->dest[d]), bi_writemask(ins), i, live[i]);
                                 }
@@ -57,7 +57,7 @@ bi_compute_interference(bi_context *ctx, struct lcra_state *l)
                         if (!ctx->is_blend && ins->op == BI_OPCODE_BLEND) {
                                 /* Add blend shader interference: blend shaders might
                                  * clobber r0-r15. */
-                                for (unsigned i = 1; i < node_count; ++i) {
+                                for (unsigned i = 0; i < node_count; ++i) {
                                         if (!live[i])
                                                 continue;
 
