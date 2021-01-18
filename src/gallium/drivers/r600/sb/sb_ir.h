ff382c6634692cdbba61f684a7d612ebf9345263
Gert Wollny <gert.wollny@collabora.com>
2021-01-18 18:04:16 +0100

    r600/sb: Add support for INTERP_X and INTERP_Z ops
    v2: Fix parsing the 2-slot instructions
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8563>

--- src/gallium/drivers/r600/sb/sb_ir.h
+++ src/gallium/drivers/r600/sb/sb_ir.h
@@ -713,7 +713,8 @@ enum node_flags {
 	NF_SCHEDULE_EARLY = (1 << 9),
 
 	// for ALU_PUSH_BEFORE - when set, replace with PUSH + ALU
-	NF_ALU_STACK_WORKAROUND = (1 << 10)
+	NF_ALU_STACK_WORKAROUND = (1 << 10),
+	NF_ALU_2SLOT = (1 << 11),
 };
 
 inline node_flags operator |(node_flags l, node_flags r) {
@@ -1021,8 +1022,9 @@ public:
 	virtual bool fold_dispatch(expr_handler *ex);
 
 	unsigned forced_bank_swizzle() {
-		return ((bc.op_ptr->flags & AF_INTERP) && (bc.slot_flags == AF_4V)) ?
-				VEC_210 : 0;
+		return ((bc.op_ptr->flags & AF_INTERP) &&
+			((bc.slot_flags == AF_4V) ||
+			 (bc.slot_flags == AF_2V))) ? VEC_210 : 0;
 	}
 
 	// return param index + 1 if instruction references interpolation param,
