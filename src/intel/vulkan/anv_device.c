e2cd83fbc57f6571be145b7f743f6e728e6c9792
Jason Ekstrand <jason.ekstrand@intel.com>
2021-01-22 16:51:44 -0600

    anv: Fix an old parameter name in GetDeviceQueue
    I don't know if this is a typo or an artifact of ancient versions of the
    Vulkan API.  In any case, it's wrong.
    
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8667>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -3237,7 +3237,7 @@ VkResult anv_EnumerateDeviceLayerProperties(
 
 void anv_GetDeviceQueue(
     VkDevice                                    _device,
-    uint32_t                                    queueNodeIndex,
+    uint32_t                                    queueFamilyIndex,
     uint32_t                                    queueIndex,
     VkQueue*                                    pQueue)
 {
@@ -3245,7 +3245,7 @@ void anv_GetDeviceQueue(
       .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_INFO_2,
       .pNext = NULL,
       .flags = 0,
-      .queueFamilyIndex = queueNodeIndex,
+      .queueFamilyIndex = queueFamilyIndex,
       .queueIndex = queueIndex,
    };
 
