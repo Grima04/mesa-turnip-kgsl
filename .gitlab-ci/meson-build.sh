e25a3e21f83390342a1de5f13d41caa0ea076dfa
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-01-27 09:25:28 -0500

    ci: disable glcpp tests for now
    these are too flaky to continue running for now
    
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8738>

--- .gitlab-ci/meson-build.sh
+++ .gitlab-ci/meson-build.sh
@@ -48,6 +48,7 @@ meson _build --native-file=native.file \
       -D libdir=lib \
       -D buildtype=${BUILDTYPE:-debug} \
       -D build-tests=true \
+      -D enable-glcpp-tests=false \
       -D libunwind=${UNWIND} \
       ${DRI_LOADERS} \
       -D dri-drivers=${DRI_DRIVERS:-[]} \
