f21e13301a54c1604e888e12421c43e66055391f
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2021-01-27 11:24:43 -0500

    panfrost: Bump advertised ESSL feature level
    Needed to run more dEQP tests.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8774>

--- src/gallium/drivers/panfrost/pan_screen.c
+++ src/gallium/drivers/panfrost/pan_screen.c
@@ -174,7 +174,7 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
         case PIPE_CAP_GLSL_FEATURE_LEVEL_COMPATIBILITY:
                 return is_gl3 ? 330 : 140;
         case PIPE_CAP_ESSL_FEATURE_LEVEL:
-                return 300;
+                return (is_deqp && pan_is_bifrost(dev)) ? 320 : 310;
 
         case PIPE_CAP_CONSTANT_BUFFER_OFFSET_ALIGNMENT:
                 return 16;
