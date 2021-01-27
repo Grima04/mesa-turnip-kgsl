4a2f12898528174da0e16c4f5c950bd9b2496fb3
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2021-01-27 13:02:01 -0500

    pan/mdg: Lower ufind_msb, poorly
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8774>

--- src/panfrost/midgard/midgard_nir_algebraic.py
+++ src/panfrost/midgard/midgard_nir_algebraic.py
@@ -71,6 +71,12 @@ algebraic_late = [
     (('fmul', a, 2.0), ('fadd', a, a))
 ]
 
+# Size conversion is redundant to Midgard but needed for NIR, and writing this
+# lowering in MIR would be painful without a competent builder, so eat the
+# extra instruction
+for sz in ('8', '16', '32'):
+    converted = ('u2u32', a) if sz != '32' else a
+    algebraic_late += [(('ufind_msb', 'a@' + sz), ('isub', 31, ('uclz', converted)))]
 
 # Midgard is able to type convert down by only one "step" per instruction; if
 # NIR wants more than one step, we need to break up into multiple instructions.
