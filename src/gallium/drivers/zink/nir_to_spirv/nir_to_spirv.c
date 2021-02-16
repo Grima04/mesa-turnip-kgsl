2b6d59bd8ca15ec885a04423366bbc7f198e5e92
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-02-15 20:25:02 -0500

    zink: support nir_intrinsic_group_memory_barrier
    needed for cts
    
    Reviewed-by: Hoe Hao Cheng <haochengho12907@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9073>

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -2691,6 +2691,12 @@ emit_intrinsic(struct ntv_context *ctx, nir_intrinsic_instr *intr)
                                         SpvMemorySemanticsMakeVisibleMask  | SpvMemorySemanticsAcquireReleaseMask);
       break;
 
+   case nir_intrinsic_group_memory_barrier:
+      spirv_builder_emit_memory_barrier(&ctx->builder, SpvScopeWorkgroup,
+                                        SpvMemorySemanticsWorkgroupMemoryMask |
+                                        SpvMemorySemanticsAcquireReleaseMask);
+      break;
+
    case nir_intrinsic_memory_barrier_shared:
       spirv_builder_emit_memory_barrier(&ctx->builder, SpvScopeWorkgroup,
                                         SpvMemorySemanticsWorkgroupMemoryMask |
