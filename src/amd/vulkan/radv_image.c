2169c4f76385fd81e99224f5437a925500173bbe
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-02-24 11:22:10 +0100

    radv: re-enable TC-compat HTILE for MSAA D32S8 images on GFX9+
    Should help MSAA games. Note that it's broken on GFX8 because
    the tiling doesn't match.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3868
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9284>

--- src/amd/vulkan/radv_image.c
+++ src/amd/vulkan/radv_image.c
@@ -90,13 +90,13 @@ radv_use_tc_compat_htile_for_image(struct radv_device *device,
 				    VK_IMAGE_USAGE_TRANSFER_SRC_BIT)))
 		return false;
 
-	/* FIXME: for some reason TC compat with 2/4/8 samples breaks
-	 * some cts tests - disable for now.
-	 */
-	if (pCreateInfo->samples >= 2 && format == VK_FORMAT_D32_SFLOAT_S8_UINT)
-		return false;
-
 	if (device->physical_device->rad_info.chip_class < GFX9) {
+		/* TC-compat HTILE for MSAA depth/stencil images is broken
+		 * on GFX8 because the tiling doesn't match.
+		 */
+		if (pCreateInfo->samples >= 2 && format == VK_FORMAT_D32_SFLOAT_S8_UINT)
+			return false;
+
 		/* GFX9+ supports compression for both 32-bit and 16-bit depth
 		 * surfaces, while GFX8 only supports 32-bit natively. Though,
 		 * the driver allows TC-compat HTILE for 16-bit depth surfaces
