5f1b3544729178715a1ed0714bd1029737089824
Rhys Perry <pendingchaos02@gmail.com>
2021-02-25 15:37:17 +0000

    aco: calculate all p_as_uniform and v_readfirstlane_b32 sources in WQM
    We should avoid a situation where a v_readfirstlane_b32 is in WQM but it's
    source is calculated in Exact.
    
    Fixes hang when running Assassin's Creed: Valhalla benchmark.
    
    fossil-db (GFX10.3):
    Totals from 1021 (0.70% of 146267) affected shaders:
    CodeSize: 7835228 -> 7842992 (+0.10%); split: -0.00%, +0.10%
    Instrs: 1519208 -> 1521149 (+0.13%); split: -0.00%, +0.13%
    SClause: 78921 -> 78920 (-0.00%)
    Copies: 44456 -> 45421 (+2.17%); split: -0.05%, +2.22%
    Branches: 12987 -> 13933 (+7.28%)
    PreSGPRs: 47599 -> 47813 (+0.45%)
    Cycles: 10037540 -> 10045304 (+0.08%); split: -0.00%, +0.08%
    VMEM: 538381 -> 538777 (+0.07%); split: +0.11%, -0.03%
    SMEM: 84553 -> 84554 (+0.00%); split: +0.01%, -0.01%
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9288>

--- src/amd/compiler/aco_insert_exec_mask.cpp
+++ src/amd/compiler/aco_insert_exec_mask.cpp
@@ -143,7 +143,8 @@ void get_block_needs(wqm_ctx &ctx, exec_ctx &exec_ctx, Block* block)
       aco_ptr<Instruction>& instr = block->instructions[i];
 
       WQMState needs = needs_exact(instr) ? Exact : Unspecified;
-      bool propagate_wqm = instr->opcode == aco_opcode::p_wqm;
+      bool propagate_wqm = instr->opcode == aco_opcode::p_wqm ||
+                           instr->opcode == aco_opcode::p_as_uniform;
       bool preserve_wqm = instr->opcode == aco_opcode::p_discard_if;
       bool pred_by_exec = needs_exec_mask(instr.get());
       for (const Definition& definition : instr->definitions) {
