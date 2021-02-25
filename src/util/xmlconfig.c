Grima04/mesa-turnip-kgsl

commit c83400e6736152439e1ed1e45cbad78e85ca8827

xmlconfig: Reshuffle to keep attr processing
For the static-table alternative to WITH_XMLCONFIG, we are going to want
to re-use the element attribute processing, to avoid duplicating things
like engine name regexp matching and version range matching.  This just
shuffles things around a bit so we can re-use useful parts in the next
patch.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9179>

src/util/xmlconfig.c
@@ -42,8 +42,8 @@
 #include <errno.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <regex.h>
 #endif
+#include <regex.h>
 #include <fcntl.h>
 #include <math.h>
 #include "strndup.h"
@@ -520,8 +520,6 @@ driGetOptionsXml(const driOptionDescription *configOptions, unsigned numOptions)
    return output;
 }
 
-#if WITH_XMLCONFIG
-
 /**
  * Print message to \c stderr if the \c LIBGL_DEBUG environment variable
  * is set.
@@ -545,6 +543,12 @@ __driUtilMessage(const char *f, ...)
       fprintf(stderr, "\n");
    }
 }
+
+/* We don't have real line/column # info in static-config case: */
+#if !WITH_XML_CONFIG
+#  define XML_GetCurrentLineNumber(p) -1
+#  define XML_GetCurrentColumnNumber(p) -1
+#endif
 
 /** \brief Output a warning message. */
 #define XML_WARNING1(msg) do {                                          \
@@ -574,7 +578,9 @@ __driUtilMessage(const char *f, ...)
 /** \brief Parser context for configuration files. */
 struct OptConfData {
    const char *name;
+#if WITH_XMLCONFIG
    XML_Parser parser;
+#endif
    driOptionCache *cache;
    int screenNum;
    const char *driverName, *execName;
@@ -590,33 +596,6 @@ struct OptConfData {
    uint32_t inApp;
    uint32_t inOption;
 };
-
-/** \brief Elements in configuration files. */
-enum OptConfElem {
-   OC_APPLICATION = 0, OC_DEVICE, OC_DRICONF, OC_ENGINE, OC_OPTION, OC_COUNT
-};
-static const char *OptConfElems[] = {
-   [OC_APPLICATION]  = "application",
-   [OC_DEVICE] = "device",
-   [OC_DRICONF] = "driconf",
-   [OC_ENGINE]  = "engine",
-   [OC_OPTION] = "option",
-};
-
-static int compare(const void *a, const void *b) {
-   return strcmp(*(char *const*)a, *(char *const*)b);
-}
-/** \brief Binary search in a string array. */
-static uint32_t
-bsearchStr(const char *name, const char *elems[], uint32_t count)
-{
-   const char **found;
-   found = bsearch(&name, elems, count, sizeof(char *), compare);
-   if (found)
-      return found - elems;
-   else
-      return count;
-}
 
 /** \brief Parse a list of ranges of type info->type. */
 static unsigned char
@@ -818,6 +797,35 @@ parseOptConfAttr(struct OptConfData *data, const char **attr)
       } else if (!parseValue(&cache->values[opt], cache->info[opt].type, value))
          XML_WARNING("illegal option value: %s.", value);
    }
+}
+
+#if WITH_XMLCONFIG
+
+/** \brief Elements in configuration files. */
+enum OptConfElem {
+   OC_APPLICATION = 0, OC_DEVICE, OC_DRICONF, OC_ENGINE, OC_OPTION, OC_COUNT
+};
+static const char *OptConfElems[] = {
+   [OC_APPLICATION]  = "application",
+   [OC_DEVICE] = "device",
+   [OC_DRICONF] = "driconf",
+   [OC_ENGINE]  = "engine",
+   [OC_OPTION] = "option",
+};
+
+static int compare(const void *a, const void *b) {
+   return strcmp(*(char *const*)a, *(char *const*)b);
+}
+/** \brief Binary search in a string array. */
+static uint32_t
+bsearchStr(const char *name, const char *elems[], uint32_t count)
+{
+   const char **found;
+   found = bsearch(&name, elems, count, sizeof(char *), compare);
+   if (found)
+      return found - elems;
+   else
+      return count;
 }
 
 /** \brief Handler for start element events. */
@@ -1061,9 +1069,6 @@ driParseConfigFiles(driOptionCache *cache, const driOptionCache *info,
                     const char *engineName, uint32_t engineVersion)
 {
    initOptionCache(cache, info);
-
-#if WITH_XMLCONFIG
-   char *home;
    struct OptConfData userData;
 
    userData.cache = cache;
@@ -1075,6 +1080,9 @@ driParseConfigFiles(driOptionCache *cache, const driOptionCache *info,
    userData.engineName = engineName ? engineName : "";
    userData.engineVersion = engineVersion;
    userData.execName = execname ?: util_get_process_name();
+
+#if WITH_XMLCONFIG
+   char *home;
 
    parseConfigDir(&userData, datadir);
    parseOneConfigFile(&userData, SYSCONFDIR "/drirc");

commit 5a2b930014ce2ef930b26216bfece43e9155c470

meson/xmlconfig: win32 regex fallback
xmlconfig now uses regex fonctions even without xml support

Fixes: c83400e6
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9261>

meson.build
@@ -1424,6 +1424,15 @@ endif
 # it's not linux and wont
 dep_m = cc.find_library('m', required : false)
 
+if host_machine.system() == 'windows'
+  dep_regex = meson.get_compiler('c').find_library('regex', required : false)
+  if not dep_regex.found()
+    dep_regex = declare_dependency(compile_args : ['-DNO_REGEX'])
+  endif
+else
+  dep_regex = null_dep
+endif
+
 if with_platform_haiku
   dep_network = cc.find_library('network')
 endif

src/util/meson.build
@@ -221,6 +221,7 @@ xmlconfig_deps = []
 if not (with_platform_android or with_platform_windows)
   xmlconfig_deps += dep_expat
 endif
+xmlconfig_deps += dep_regex
 
 _libxmlconfig = static_library(
   'xmlconfig',

src/util/xmlconfig.c
@@ -43,7 +43,17 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #endif
+#ifdef NO_REGEX
+typedef int regex_t;
+#define REG_EXTENDED 0
+#define REG_NOSUB 0
+#define REG_NOMATCH 1
+inline int regcomp(regex_t *r, const char *s, int f) { return 0; }
+inline int regexec(regex_t *r, const char *s, int n, void *p, int f) { return REG_NOMATCH; }
+inline void regfree(regex_t* r) {}
+#else
 #include <regex.h>
+#endif
 #include <fcntl.h>
 #include <math.h>
 #include "strndup.h"
@@ -1153,7 +1163,7 @@ driParseConfigFiles(driOptionCache *cache, const driOptionCache *info,
    userData.applicationVersion = applicationVersion;
    userData.engineName = engineName ? engineName : "";
    userData.engineVersion = engineVersion;
-   userData.execName = execname ?: util_get_process_name();
+   userData.execName = execname ? execname : util_get_process_name();
 
 #if WITH_XMLCONFIG
    char *home;