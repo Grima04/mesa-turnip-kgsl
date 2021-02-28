0360ecac73f9afee9875b3da080a9ed7cde9bb38
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2021-02-28 03:09:48 +0100

    radv: Enable linear sampling for depth textures.
    Turns out there are CTS tests.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4258
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9319>

--- src/amd/vulkan/radv_formats.c
+++ src/amd/vulkan/radv_formats.c
@@ -718,6 +718,9 @@ radv_physical_device_get_format_properties(struct radv_physical_device *physical
 			if (radv_is_filter_minmax_format_supported(format))
 				 tiled |= VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_MINMAX_BIT;
 
+			if (vk_format_is_depth(format))
+				tiled |= VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT;
+
 			/* Don't support blitting surfaces with depth/stencil. */
 			if (vk_format_is_depth(format) && vk_format_is_stencil(format))
 				tiled &= ~VK_FORMAT_FEATURE_BLIT_DST_BIT;
