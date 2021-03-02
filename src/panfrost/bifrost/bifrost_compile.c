93cdb072a82334c30acdb66936f09ecfaa0b5484
Alyssa Rosenzweig <alyssa@collabora.com>
2021-03-02 20:22:26 +0000

    pan/bi: Only run DCE once
    No more progress loop needed. I'm skeptical we really want a dataflow
    approach long-term, though, this is annoyingly expensive.
    
    No shader-db changes.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9421>

--- src/panfrost/bifrost/bifrost_compile.c
+++ src/panfrost/bifrost/bifrost_compile.c
@@ -3036,14 +3036,7 @@ bifrost_compile_shader_nir(nir_shader *nir,
         /* Runs before copy prop */
         bi_opt_push_ubo(ctx);
         bi_opt_copy_prop(ctx);
-
-        bool progress = false;
-
-        do {
-                progress = false;
-
-                progress |= bi_opt_dead_code_eliminate(ctx, false);
-        } while(progress);
+        bi_opt_dead_code_eliminate(ctx, false);
 
         bi_foreach_block(ctx, _block) {
                 bi_block *block = (bi_block *) _block;
