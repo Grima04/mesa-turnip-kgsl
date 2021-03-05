b10b55f3d3398c4a2916a86ae63f418796c89a6c
Dave Airlie <airlied@redhat.com>
2021-03-05 10:08:31 +1000

    draw: refactor out the instances drawing code
    This can be reused nice for multiview if refactored out
    
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9399>

--- src/gallium/auxiliary/draw/draw_pt.c
+++ src/gallium/auxiliary/draw/draw_pt.c
@@ -463,6 +463,40 @@ resolve_draw_info(const struct pipe_draw_info *raw_info,
    }
 }
 
+/*
+ * Loop over all instances and execute draws for them.
+ */
+static void
+draw_instances(struct draw_context *draw,
+               const struct pipe_draw_info *info,
+               const struct pipe_draw_start_count *draws,
+               int count)
+{
+   unsigned instance;
+
+   draw->start_instance = info->start_instance;
+
+   for (instance = 0; instance < info->instance_count; instance++) {
+      unsigned instance_idx = instance + info->start_instance;
+      draw->instance_id = instance;
+      /* check for overflow */
+      if (instance_idx < instance ||
+          instance_idx < draw->start_instance) {
+         /* if we overflown just set the instance id to the max */
+         draw->instance_id = 0xffffffff;
+      }
+
+      draw_new_instance(draw);
+
+      if (info->primitive_restart) {
+         draw_pt_arrays_restart(draw, info, &draws[0]);
+      }
+      else {
+         draw_pt_arrays(draw, info->mode, draws[0].start, count);
+      }
+   }
+}
+
 /**
  * Draw vertex arrays.
  * This is the main entrypoint into the drawing module.  If drawing an indexed
@@ -476,7 +510,6 @@ draw_vbo(struct draw_context *draw,
          const struct pipe_draw_start_count *draws,
          unsigned num_draws)
 {
-   unsigned instance;
    unsigned index_limit;
    unsigned count;
    unsigned fpstate = util_fpstate_get();
@@ -571,26 +604,7 @@ draw_vbo(struct draw_context *draw,
     * the min_index/max_index hints given by gallium frontends.
     */
 
-   for (instance = 0; instance < info->instance_count; instance++) {
-      unsigned instance_idx = instance + info->start_instance;
-      draw->start_instance = info->start_instance;
-      draw->instance_id = instance;
-      /* check for overflow */
-      if (instance_idx < instance ||
-          instance_idx < draw->start_instance) {
-         /* if we overflown just set the instance id to the max */
-         draw->instance_id = 0xffffffff;
-      }
-
-      draw_new_instance(draw);
-
-      if (info->primitive_restart) {
-         draw_pt_arrays_restart(draw, info, &draws[0]);
-      }
-      else {
-         draw_pt_arrays(draw, info->mode, draws[0].start, count);
-      }
-   }
+   draw_instances(draw, info, draws, count);
 
    /* If requested emit the pipeline statistics for this run */
    if (draw->collect_statistics) {
