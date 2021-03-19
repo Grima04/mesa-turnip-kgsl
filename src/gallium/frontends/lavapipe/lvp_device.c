34525bb088da43b3ec492ca0f439064ee077a167
Dave Airlie <airlied@redhat.com>
2021-03-19 12:05:01 +1000

    lavapipe: enable subgroups features
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9645>

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -770,9 +770,9 @@ lvp_get_physical_device_properties_1_1(struct lvp_physical_device *pdevice,
    p->deviceLUIDValid = false;
    p->deviceNodeMask = 0;
 
-   p->subgroupSize = 0;
-   p->subgroupSupportedStages = 0;
-   p->subgroupSupportedOperations = 0;
+   p->subgroupSize = 8;
+   p->subgroupSupportedStages = VK_SHADER_STAGE_FRAGMENT_BIT | VK_SHADER_STAGE_COMPUTE_BIT;
+   p->subgroupSupportedOperations = VK_SUBGROUP_FEATURE_BASIC_BIT | VK_SUBGROUP_FEATURE_VOTE_BIT | VK_SUBGROUP_FEATURE_ARITHMETIC_BIT | VK_SUBGROUP_FEATURE_BALLOT_BIT;
    p->subgroupQuadOperationsInAllStages = false;
 
    p->pointClippingBehavior = VK_POINT_CLIPPING_BEHAVIOR_ALL_CLIP_PLANES;
