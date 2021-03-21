befd9fbbba1985b38a4a2a43cb3be08d268e9956
Rob Clark <robdclark@chromium.org>
2021-03-21 08:18:26 -0700

    freedreno/a6xx: Fix typo
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9535>

--- src/gallium/drivers/freedreno/a6xx/fd6_emit.c
+++ src/gallium/drivers/freedreno/a6xx/fd6_emit.c
@@ -890,7 +890,7 @@ fd6_emit_streamout(struct fd_ringbuffer *ring, struct fd6_emit *emit)
 {
 	struct fd_context *ctx = emit->ctx;
 	const struct fd6_program_state *prog = fd6_emit_get_prog(emit);
-	struct ir3_stream_output_info *info = prog->stream_output;;
+	struct ir3_stream_output_info *info = prog->stream_output;
 	struct fd_streamout_stateobj *so = &ctx->streamout;
 
 	emit->streamout_mask = 0;
