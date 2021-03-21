c9137bd11843b8eadc045d3208f3020c723d9bbe
Rob Clark <robdclark@chromium.org>
2021-03-21 10:09:57 -0700

    tegra: Use util_draw_multi() helper
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9742>

--- src/gallium/drivers/tegra/tegra_context.c
+++ src/gallium/drivers/tegra/tegra_context.c
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 
 #include "util/u_debug.h"
+#include "util/u_draw.h"
 #include "util/u_inlines.h"
 #include "util/u_upload_mgr.h"
 
@@ -52,13 +53,7 @@ tegra_draw_vbo(struct pipe_context *pcontext,
                unsigned num_draws)
 {
    if (num_draws > 1) {
-      struct pipe_draw_info tmp_info = *pinfo;
-
-      for (unsigned i = 0; i < num_draws; i++) {
-         tegra_draw_vbo(pcontext, &tmp_info, pindirect, &draws[i], 1);
-         if (tmp_info.increment_draw_id)
-            tmp_info.drawid++;
-      }
+      util_draw_multi(pcontext, pinfo, pindirect, draws, num_draws);
       return;
    }
 
