dee89af505d505827a1ad4b08d3bfad3659a000e
Eric Anholt <eric@anholt.net>
2021-03-24 10:16:26 -0700

    ci: Uprev piglit to 6a4be9e9946d ("piglit: NOTE! Default branch is now main")
    Along with other new tests, brings in the perf improvement for
    gl-1.3-texture-env so we can stop skipping it.
    
    Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9806>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -8,7 +8,7 @@ fi
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout b0bbeb876a506e0ee689dd7e17cee374c8284058
+git checkout 6a4be9e9946df310d9402f995f371c7deb8c27ba
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
