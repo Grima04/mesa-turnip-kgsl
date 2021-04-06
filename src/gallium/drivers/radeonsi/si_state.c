9fb1f9303b5e082f1aa6133c76a71cbf075bfd11
Rhys Perry <pendingchaos02@gmail.com>
2021-04-06 09:52:26 +0100

    Revert "radeonsi: set TRUNC_COORD=0 for Total War: WARHAMMER to fix it"
    This reverts commit cd12fcff96cc7db794047e2a045142ea861254a2.
    
    The terrain looks fine now that TRUNC_COORD=0 for textureGather().
    
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10036>

--- src/gallium/drivers/radeonsi/si_state.c
+++ src/gallium/drivers/radeonsi/si_state.c
@@ -4455,8 +4455,7 @@ static void *si_create_sampler_state(struct pipe_context *ctx,
    struct si_sampler_state *rstate = CALLOC_STRUCT(si_sampler_state);
    unsigned max_aniso = sscreen->force_aniso >= 0 ? sscreen->force_aniso : state->max_anisotropy;
    unsigned max_aniso_ratio = si_tex_aniso_filter(max_aniso);
-   bool trunc_coord = !sscreen->options.no_trunc_coord &&
-                      state->min_img_filter == PIPE_TEX_FILTER_NEAREST &&
+   bool trunc_coord = state->min_img_filter == PIPE_TEX_FILTER_NEAREST &&
                       state->mag_img_filter == PIPE_TEX_FILTER_NEAREST &&
                       state->compare_mode == PIPE_TEX_COMPARE_NONE;
    union pipe_color_union clamped_border_color;
