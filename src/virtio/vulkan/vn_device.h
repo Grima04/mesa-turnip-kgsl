8b6ed71cf1c3500938c6a830b5e8f2473e6bbb1c
Chia-I Wu <olvaffe@gmail.com>
2021-04-07 16:29:38 -0700

    venus: split out vn_pipeline.[ch]
    Move VkShaderModule and VkPipeline{,Layout,Cache} functions to the new
    files.
    
    Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Reviewed-by: Ryan Neph <ryanneph@google.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10117>

--- src/virtio/vulkan/vn_device.h
+++ src/virtio/vulkan/vn_device.h
@@ -355,38 +355,6 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(vn_query_pool,
                                VkQueryPool,
                                VK_OBJECT_TYPE_QUERY_POOL)
 
-struct vn_shader_module {
-   struct vn_object_base base;
-};
-VK_DEFINE_NONDISP_HANDLE_CASTS(vn_shader_module,
-                               base.base,
-                               VkShaderModule,
-                               VK_OBJECT_TYPE_SHADER_MODULE)
-
-struct vn_pipeline_layout {
-   struct vn_object_base base;
-};
-VK_DEFINE_NONDISP_HANDLE_CASTS(vn_pipeline_layout,
-                               base.base,
-                               VkPipelineLayout,
-                               VK_OBJECT_TYPE_PIPELINE_LAYOUT)
-
-struct vn_pipeline_cache {
-   struct vn_object_base base;
-};
-VK_DEFINE_NONDISP_HANDLE_CASTS(vn_pipeline_cache,
-                               base.base,
-                               VkPipelineCache,
-                               VK_OBJECT_TYPE_PIPELINE_CACHE)
-
-struct vn_pipeline {
-   struct vn_object_base base;
-};
-VK_DEFINE_NONDISP_HANDLE_CASTS(vn_pipeline,
-                               base.base,
-                               VkPipeline,
-                               VK_OBJECT_TYPE_PIPELINE)
-
 VkResult
 vn_instance_submit_roundtrip(struct vn_instance *instance,
                              uint32_t *roundtrip_seqno);
