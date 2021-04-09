d075711b0e8e94fab17e0320f74eff241be52972
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-04-09 13:26:30 +0200

    radv: do not use the whole HTILE buffer for depth when VRS is used
    The stencil data needs to be included for storing the VRS rates
    into the HTILE buffer.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10187>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1958,7 +1958,7 @@ static inline bool
 radv_image_tile_stencil_disabled(const struct radv_device *device, const struct radv_image *image)
 {
    if (device->physical_device->rad_info.chip_class >= GFX9) {
-      return !vk_format_has_stencil(image->vk_format);
+      return !vk_format_has_stencil(image->vk_format) && !radv_image_has_vrs_htile(device, image);
    } else {
       /* Due to a hw bug, TILE_STENCIL_DISABLE must be set to 0 for
        * the TC-compat ZRANGE issue even if no stencil is used.
