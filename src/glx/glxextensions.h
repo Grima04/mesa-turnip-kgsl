7ae2ba5da9a014bd1dbde842d4c24e16d24e3554
Adam Jackson <ajax@redhat.com>
2021-04-13 18:58:54 -0400

    glx: Remove some ancient backwards-compatibility typedefs
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10223>

--- src/glx/glxextensions.h
+++ src/glx/glxextensions.h
@@ -279,12 +279,6 @@ extern GLboolean __glExtensionBitIsEnabled(struct glx_context *gc,
 extern void
 __glXEnableDirectExtension(struct glx_screen *psc, const char *name);
 
-/* Source-level backwards compatibility with old drivers. They won't
- * find the respective functions, though. 
- */
-typedef void (*PFNGLXENABLEEXTENSIONPROC) (const char *name,
-                                           GLboolean force_client);
-typedef void (*PFNGLXDISABLEEXTENSIONPROC) (const char *name);
 
 /* GLX_ALIAS should be used for functions with a non-void return type.
    GLX_ALIAS_VOID is for functions with a void return type. */
