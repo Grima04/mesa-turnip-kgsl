580a1ba7065698ef86b6222b2bcbf89147b1e4e9
Eric Anholt <eric@anholt.net>
2021-04-19 14:49:08 -0700

    mesa: Move per-pixel Z pack functions to swrast.
    Only swrast uses them, and this makes it clearer why we only need partial
    format support for them, avoids linker work deleting them for gallium
    drivers, and means they will automatically go away when we get to finally
    delete swrast.
    
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10336>

--- src/mesa/main/format_pack.h
+++ src/mesa/main/format_pack.h
@@ -34,21 +34,6 @@
 extern "C" {
 #endif
 
-/** Pack a uint32_t Z value to dest address */
-typedef void (*mesa_pack_uint_z_func)(const uint32_t *src, void *dst);
-
-/** Pack a uint8_t stencil value to dest address */
-typedef void (*mesa_pack_ubyte_stencil_func)(const uint8_t *src, void *dst);
-
-
-extern mesa_pack_uint_z_func
-_mesa_get_pack_uint_z_func(mesa_format format);
-
-
-extern mesa_pack_ubyte_stencil_func
-_mesa_get_pack_ubyte_stencil_func(mesa_format format);
-
-
 static inline void
 _mesa_pack_float_rgba_row(mesa_format format, uint32_t n,
                           const float src[][4], void *dst)
