080b05e29e1f04f22a776057631f4061cf7c1824
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-04-21 01:06:41 +0530

    asahi: Add Gallium driver
    Forked from noop, with some code from Panfrost.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Acked-by: Jason Ekstrand <jason@jlekstrand.net>
    Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10582>

--- src/gallium/auxiliary/target-helpers/inline_sw_helper.h
+++ src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -33,6 +33,10 @@
 #include "d3d12/d3d12_public.h"
 #endif
 
+#ifdef GALLIUM_ASAHI
+#include "asahi/agx_public.h"
+#endif
+
 static inline struct pipe_screen *
 sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
 {
@@ -71,6 +75,11 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
       screen = d3d12_create_dxcore_screen(winsys, NULL);
 #endif
 
+#if defined(GALLIUM_ASAHI)
+   if (screen == NULL && strcmp(driver, "asahi") == 0)
+      screen = agx_screen_create(winsys);
+#endif
+
    return screen ? debug_screen_wrap(screen) : NULL;
 }
 
@@ -83,6 +92,9 @@ sw_screen_create(struct sw_winsys *winsys)
       debug_get_option("GALLIUM_DRIVER", ""),
 #if defined(GALLIUM_D3D12)
       only_sw ? "" : "d3d12",
+#endif
+#if defined(GALLIUM_ASAHI)
+      only_sw ? "" : "asahi",
 #endif
 #if defined(GALLIUM_LLVMPIPE)
       "llvmpipe",
