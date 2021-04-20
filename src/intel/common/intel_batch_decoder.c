c073648d80e3fd079da69439303b85a89a00d696
Marcin Ślusarz <marcin.slusarz@intel.com>
2021-04-20 10:42:18 +0200

    intel/batch_decoder: set foreground color of decoded instructions
    It's impossible to see the names of instructions if the terminal's
    color scheme uses black as foreground. Just set it to white - it
    will look good on any color scheme.
    
    Signed-off-by: Marcin Ślusarz <marcin.slusarz@intel.com>
    Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10341>

--- src/intel/common/intel_batch_decoder.c
+++ src/intel/common/intel_batch_decoder.c
@@ -65,7 +65,7 @@ intel_batch_decode_ctx_finish(struct intel_batch_decode_ctx *ctx)
 
 #define CSI "\e["
 #define RED_COLOR    CSI "31m"
-#define BLUE_HEADER  CSI "0;44m"
+#define BLUE_HEADER  CSI "0;44m" CSI "1;37m"
 #define GREEN_HEADER CSI "1;42m"
 #define NORMAL       CSI "0m"
 
