d050be4b268c74b26a82df5b855464e50f0ed8c5
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-04-21 13:19:47 -0400

    zink: grab GetPhysicalDeviceMemoryProperties2 from instance
    Acked-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10511>

--- src/gallium/drivers/zink/zink_screen.h
+++ src/gallium/drivers/zink/zink_screen.h
@@ -109,6 +109,7 @@ struct zink_screen {
    PFN_vkGetPhysicalDeviceProperties2 vk_GetPhysicalDeviceProperties2;
    PFN_vkGetPhysicalDeviceFormatProperties2 vk_GetPhysicalDeviceFormatProperties2;
    PFN_vkGetPhysicalDeviceImageFormatProperties2 vk_GetPhysicalDeviceImageFormatProperties2;
+   PFN_vkGetPhysicalDeviceMemoryProperties2 vk_GetPhysicalDeviceMemoryProperties2;
 
    PFN_vkCmdDrawIndirectCount vk_CmdDrawIndirectCount;
    PFN_vkCmdDrawIndexedIndirectCount vk_CmdDrawIndexedIndirectCount;
