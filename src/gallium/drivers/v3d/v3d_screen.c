9094ad7c6a0cb45fc1f4985878603055d7ddf453
Jose Maria Casanova Crespo <jmcasanova@igalia.com>
2021-04-29 18:37:25 +0200

    v3d: YUV formats at query_dmabuf_modifiers are external_only
    This fixes Issue https://github.com/Igalia/meta-webkit/issues/185
    "Issue Raspberry 4-64 + Mesa VC4 driver + Gstreamer = red Label on video"
    
    Fixes: 95c4f0f91098 "v3d: Enables DRM_FORMAT_MOD_BROADCOM_SAND128 support"
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10524>

--- src/gallium/drivers/v3d/v3d_screen.c
+++ src/gallium/drivers/v3d/v3d_screen.c
@@ -660,10 +660,8 @@ v3d_screen_query_dmabuf_modifiers(struct pipe_screen *pscreen,
         for (i = 0; i < *count; i++) {
                 modifiers[i] = v3d_available_modifiers[i];
                 if (external_only)
-                        external_only[i] =
-                                v3d_available_modifiers[i] ==
-                                DRM_FORMAT_MOD_BROADCOM_SAND128;
-       }
+                        external_only[i] = util_format_is_yuv(format);
+        }
 }
 
 static bool
